ff6f2805e0700dc7165756864b1f7b6546249b75
Quentin Renard <quentin@area17.com>
2017-01-20 18:44:02 +0100

    Update icon helper to work best with the FE Boilerplate

--- src/Helpers/frontend_helpers.php
+++ src/Helpers/frontend_helpers.php
@@ -30,6 +30,6 @@ if (!function_exists('icon')) {
         $title = isset($opts['title']) ? ' title="' . htmlentities($opts['title'], ENT_QUOTES, 'UTF-8') . '" ' : '';
         $role = isset($opts['role']) ? ' role="' . htmlentities($opts['role'], ENT_QUOTES, 'UTF-8') . '" ' : ' role="presentation" ';
         $css_class = isset($opts['css_class']) ? htmlentities($opts['css_class'], ENT_QUOTES, 'UTF-8') : '';
-        return "<svg class=\"Icon--$name $css_class\" $title $role><use xlink:href=\"" . revAsset('sprites.svg') . "#$name\"></use></svg>";
+        return "<svg class=\"icon--$name $css_class\" $title $role><use xlink:href=\"" . revAsset('sprites.svg') . "#icon--$name\"></use></svg>";
     }
 }
