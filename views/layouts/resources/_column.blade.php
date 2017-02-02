3de3cbd335a0ab63d0f34e0050f5959229aafcc6
Quentin Renard <quentin@area17.com>
2017-02-02 15:42:30 +0100

    Add nested column type

--- views/layouts/resources/_column.blade.php
+++ views/layouts/resources/_column.blade.php
@@ -2,6 +2,14 @@
     {{ array_get($item, "{$columnOptions['relationship']}.{$columnOptions['field']}") }}
 @elseif (isset($columnOptions['count']) && $columnOptions['count'])
     {{ $item->{$columnOptions['field']}->count() }}
+@elseif (isset($columnOptions['nested']) && $columnOptions['nested'])
+    <a class="main" href="{{ moduleRoute("{$columnOptions['nested']['parent']}.{$columnOptions['nested']['field']}", $routePrefix, 'index', [$item->id]) }}">
+    @if(($nb = $item->{$columnOptions['nested']['field']}->count()) > 0)
+        {{ $nb }} {{ $nb > 1 ? $columnOptions['title'] : $columnOptions['title'] }}
+    @else
+        Create {{$columnOptions['title']}}
+    @endif
+    </a>
 @else
     @if(isset($columnOptions['present']) && $columnOptions['present'])
         {!! $item->presentAdmin()->{$columnOptions['field']} !!}
