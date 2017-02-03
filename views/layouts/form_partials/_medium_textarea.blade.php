3125e5e0d9d8f9ce713d79845296410f00751c57
Fernando Petrelli <fernando@area17.com>
2017-02-03 16:11:06 -0500

    Differentiate Medium editor and rich text area

--- views/layouts/form_partials/_medium_textarea.blade.php
+++ views/layouts/form_partials/_medium_textarea.blade.php
@@ -1,6 +1,6 @@
 @php
     $required = $required ?? "";
-    $options = $options ?? [];
+    $options = [];
     if (isset($textLimit)) {
         $options['maxlength'] = "{$textLimit}";
     }
@@ -13,7 +13,11 @@
     {!! Form::textarea($field, null,[
         'class' => "textarea-medium-editor string {$required}",
         'id' => $field,
-        'data-behavior' => "markitup"] + $options) !!}
+        'data-behavior' => "medium_editor",
+        'data-medium-editor-js' => "assets/admin/vendor/medium-editor/medium-editor.min.js",
+        'data-medium-editor-css' => "assets/admin/vendor/medium-editor/medium-editor.css, assets/admin/vendor/medium-editor/themes/flat.min.css",
+        'data-medium-editor-options' => ($data_medium_editor_options ?? ''),
+    ] + $options) !!}
     @if (isset($textLimit))
         <span class="hint"><span class="textlimit-remaining">0</span> / {{ $textLimit }} characters maximum</span>
     @endif
--- views/layouts/form_partials/_medium_textarea_locale.blade.php
+++ views/layouts/form_partials/_medium_textarea_locale.blade.php
@@ -1,7 +1,7 @@
 @php
     $required = $required ?? "";
     $rows = $rows ?? 10;
-    $options = $options ?? [];
+    $options = [];
 
     if (isset($textLimit)) {
         $options['maxlength'] = "{$textLimit}";
@@ -27,7 +27,10 @@
             'class' => "textarea-medium-editor string {$required}",
             'id' => $fullField,
             'rows' => $rows,
-            'data-behavior' => "markitup"
+            'data-behavior' => "medium_editor",
+            'data-medium-editor-js' => "assets/admin/vendor/medium-editor/medium-editor.min.js",
+            'data-medium-editor-css' => "assets/admin/vendor/medium-editor/medium-editor.css, assets/admin/vendor/medium-editor/themes/flat.min.css",
+            'data-medium-editor-options' => $data_medium_editor_options ?? '',
         ] + $options) !!}
         @if (isset($textLimit))
             <span class="hint"><span class="textlimit-remaining">0</span> / {{ $textLimit }} characters maximum</span>
