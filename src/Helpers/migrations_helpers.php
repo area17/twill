786fb069044e42c2bd4f3cacbf3d8b1fd293b1ce
Quentin Renard <quentin@area17.com>
2017-02-15 18:43:42 +0100

    Fix translations and slugs migration helpers

--- src/Helpers/migrations_helpers.php
+++ src/Helpers/migrations_helpers.php
@@ -11,22 +11,30 @@ if (!function_exists('createDefaultFields')) {
 }
 
 if (!function_exists('createDefaultTranslationsTableFields')) {
-    function createDefaultTranslationsTableFields($table, $tableNameSingular)
+    function createDefaultTranslationsTableFields($table, $tableNameSingular, $tableNamePlural = null)
     {
+        if (!$tableNamePlural) {
+            $tableNamePlural = str_plural($tableNameSingular);
+        }
+
         $table->increments('id');
         $table->softDeletes();
         $table->timestamps();
         $table->string('locale', 6)->index();
         $table->boolean('active');
         $table->integer("{$tableNameSingular}_id")->unsigned();
-        $table->foreign("{$tableNameSingular}_id", "fk_{$tableNameSingular}_translations_{$tableNameSingular}_id")->references('id')->on($table)->onDelete('CASCADE');
+        $table->foreign("{$tableNameSingular}_id", "fk_{$tableNameSingular}_translations_{$tableNameSingular}_id")->references('id')->on($tableNamePlural)->onDelete('CASCADE');
         $table->unique(["{$tableNameSingular}_id", 'locale']);
     }
 }
 
 if (!function_exists('createDefaultSlugsTableFields')) {
-    function createDefaultSlugsTableFields($table, $tableNameSingular)
+    function createDefaultSlugsTableFields($table, $tableNameSingular, $tableNamePlural = null)
     {
+        if (!$tableNamePlural) {
+            $tableNamePlural = str_plural($tableNameSingular);
+        }
+
         $table->increments('id');
         $table->softDeletes();
         $table->timestamps();
@@ -34,6 +42,6 @@ if (!function_exists('createDefaultSlugsTableFields')) {
         $table->string('locale', 6)->index();
         $table->boolean('active');
         $table->integer("{$tableNameSingular}_id")->unsigned();
-        $table->foreign("{$tableNameSingular}_id", "fk_{$tableNameSingular}_slugs_{$tableNameSingular}_id")->references('id')->on($table)->onDelete('CASCADE')->onUpdate('NO ACTION');
+        $table->foreign("{$tableNameSingular}_id", "fk_{$tableNameSingular}_slugs_{$tableNameSingular}_id")->references('id')->on($tableNamePlural)->onDelete('CASCADE')->onUpdate('NO ACTION');
     }
 }
