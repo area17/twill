0144f611c09dd6f4572aaffc28ec2e014a963296
Quentin Renard <quentin@area17.com>
2017-03-01 14:45:13 +0100

    Add repeater support to hidden form field

--- views/layouts/form_partials/_hidden.blade.php
+++ views/layouts/form_partials/_hidden.blade.php
@@ -1,5 +1,8 @@
 @php
     $value_field = $value_field ?? null;
+    if (isset($repeater) && $repeater) {
+        $field = $moduleName . '[' . $repeaterIndex . '][' . $field . ']';
+    }
 @endphp
 
 {!! Form::hidden($field, $value_field) !!}
