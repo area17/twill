e82c21bf835a76ded3cf37a7155e5fd9966cf4db
Quentin Renard <quentin@area17.com>
2017-03-01 17:22:00 +0100

    Update migration helpers

--- src/Helpers/migrations_helpers.php
+++ src/Helpers/migrations_helpers.php
@@ -45,8 +45,8 @@ if (!function_exists('createDefaultSlugsTableFields')) {
         $table->foreign("{$tableNameSingular}_id", "fk_{$tableNameSingular}_slugs_{$tableNameSingular}_id")->references('id')->on($tableNamePlural)->onDelete('CASCADE')->onUpdate('NO ACTION');
     }
 }
-if (!function_exists('createDefaultTableForBrowser')) {
-    function createDefaultTableForBrowser($table, $table1NameSingular, $table2NameSingular, $table1NamePlural = null, $table2NamePlural = null)
+if (!function_exists('createDefaultRelationshipTableFields')) {
+    function createDefaultRelationshipTableFields($table, $table1NameSingular, $table2NameSingular, $table1NamePlural = null, $table2NamePlural = null)
     {
         if (!$table1NamePlural) {
             $table1NamePlural = str_plural($table1NameSingular);
@@ -59,7 +59,6 @@ if (!function_exists('createDefaultTableForBrowser')) {
         $table->foreign("{$table1NameSingular}_id")->references('id')->on($table1NamePlural)->onDelete('cascade');
         $table->integer("{$table2NameSingular}_id")->unsigned();
         $table->foreign("{$table2NameSingular}_id")->references('id')->on($table2NamePlural)->onDelete('cascade');
-        $table->integer('position')->unsigned()->index();
         $table->index(["{$table2NameSingular}_id", "{$table1NameSingular}_id"]);
     }
 }
