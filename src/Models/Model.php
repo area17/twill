eed6d21bd8d93123785d8377bea9e26ae57327a1
Quentin Renard <quentin@area17.com>
2017-03-24 14:05:21 +0100

    Make parent model absract

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -8,7 +8,7 @@ use Cartalyst\Tags\TaggableTrait;
 use Illuminate\Database\Eloquent\Model as BaseModel;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Model extends BaseModel implements TaggableInterface
+abstract class Model extends BaseModel implements TaggableInterface
 {
     use HasPresenter, SoftDeletes, TaggableTrait;
 
