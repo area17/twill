78f426b92fb5e1a4b5f8ff54fc6f3bf366633b84
Quentin Renard <quentin@area17.com>
2017-04-07 19:00:31 +0200

    Add support for S3_UPLOADER_USE_HTTPS env variable (used in Roto)

--- config/disks.php
+++ config/disks.php
@@ -7,6 +7,6 @@ return [
         'secret' => env('S3_SECRET', env('AWS_SECRET')),
         'region' => env('S3_REGION', env('AWS_REGION', 'us-east-1')),
         'bucket' => env('S3_BUCKET', env('AWS_BUCKET')),
-        'use_https' => env('S3_USE_HTTPS', env('AWS_USE_HTTPS', true)),
+        'use_https' => env('S3_UPLOADER_USE_HTTPS', env('S3_USE_HTTPS', env('AWS_USE_HTTPS', true))),
     ],
 ];
