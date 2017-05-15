a35c919c8ba21f712e96e89b3dd950a99d24c8c4
Quentin Renard <quentin@area17.com>
2017-05-15 13:30:37 +0200

    Hide scrollbars in blocks previews

--- views/layouts/block_frame.blade.php
+++ views/layouts/block_frame.blade.php
@@ -1,4 +1,4 @@
-<iframe class="blockFrame" srcdoc="{{ $blockMarkup }}" style="width: 100%;" onload="resizeIframe(this);"></iframe>
+<iframe class="blockFrame" srcdoc="{{ $blockMarkup }}" style="width: 100%;" onload="resizeIframe(this);" scrolling="no"></iframe>
 <script>
     function resizeIframe(blockFrame) {
         blockFrame.style.height = blockFrame.contentWindow.document.documentElement.offsetHeight + 'px';
