5dc0461d6b1c43ebc9524ab53200e1bff32503a4
Quentin Renard <quentin@area17.com>
2017-08-09 14:36:51 +0200

    Add new setting to block editor config: iframe width (defaults to 66)

--- views/layouts/block_frame.blade.php
+++ views/layouts/block_frame.blade.php
@@ -1,4 +1,4 @@
-<iframe class="blockFrame" srcdoc="{{ $blockMarkup }}" style="width: 66%; margin-left:auto; margin-right:auto; display:block;" onload="resizeIframe(this);" scrolling="no"></iframe>
+<iframe class="blockFrame" srcdoc="{{ $blockMarkup }}" style="width: {{ config('cms-toolkit.block-editor.iframe_width') ?? 66 }}%; margin-left:auto; margin-right:auto; display:block;" onload="resizeIframe(this);" scrolling="no"></iframe>
 <script>
     function resizeIframe(blockFrame) {
         blockFrame.style.height = blockFrame.contentWindow.document.documentElement.offsetHeight + 'px';
