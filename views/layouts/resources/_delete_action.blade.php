df7cf5b28302abdfcd6dd0ec6f6a7e54385b4382
Quentin Renard <quentin@area17.com>
2017-08-17 13:08:54 +0200

    Allow disabling delete in module listing

--- views/layouts/resources/_delete_action.blade.php
+++ views/layouts/resources/_delete_action.blade.php
@@ -1,3 +1,7 @@
+@php
+    $itemIsDeletable = $item->canDelete ?? true;
+@endphp
+
 <td>
-    <a class="icon icon-trash" data-behavior="delete" data-delete-confirm="Do you really want to delete this {{ strtolower($modelName) }} ?" data-delete-reload="true" data-delete-id="{{ $item->id }}" data-delete-url="{{ moduleRoute($moduleName, $routePrefix, 'destroy', array_merge (isset($parent_id) ? [$parent_id] : [], [$item->id])) }}" href="#" rel="nofollow" title="Delete">Delete</a>
+    <a class="icon icon-trash {{ $itemIsDeletable ? '' : 'disabled' }}" data-behavior="delete" data-delete-confirm="Do you really want to delete this {{ strtolower($modelName) }} ?" data-delete-reload="true" data-delete-id="{{ $item->id }}" data-delete-url="{{ moduleRoute($moduleName, $routePrefix, 'destroy', array_merge (isset($parent_id) ? [$parent_id] : [], [$item->id])) }}" href="#" rel="nofollow" title="Delete" @if($itemIsDeletable) disabled="disabled" @endif>Delete</a>
 </td>
