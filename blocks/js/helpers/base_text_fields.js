e5042fd3d872c2866822e1ed583efd67dd8f669f
Quentin Renard <quentin@area17.com>
2017-08-22 01:06:35 +0200

    Add placeholder in block text fields for simple repeater blocks based on numbers

--- blocks/js/helpers/base_text_fields.js
+++ blocks/js/helpers/base_text_fields.js
@@ -26,13 +26,13 @@ a17cms.Helpers.sirTrevorBaseTextFields = function(fields) {
       field.class = (field.class === undefined) ? "a17-input-block" : field.class;
       switch (field.type) {
         case 'input':
-          field.html += "<input type='text' class='" + field.class + "' name='" + field.name + "_{{lang}}' maxlength='" + field.maxlength + "' placeholder='" + field.placeholder + "' autocomplete='false' " + field.attr + " />";
+          field.html += "<input type='text' class='" + field.class + "' name='" + field.name + (field.repeater ? "{{repeaterIndex}}" : "") + "_{{lang}}" + "' maxlength='" + field.maxlength + "' placeholder='" + field.placeholder + "' autocomplete='false' " + field.attr + " />";
           break;
         case 'textarea':
-          field.html += "<textarea class='" + field.class + "' name='" + field.name + "_{{lang}}' maxlength='" + field.maxlength + "' placeholder='" + field.placeholder + "' " + field.attr + " />";
+          field.html += "<textarea class='" + field.class + "' name='" + field.name + (field.repeater ? "{{repeaterIndex}}" : "") + "_{{lang}}" + "' maxlength='" + field.maxlength + "' placeholder='" + field.placeholder + "' " + field.attr + " />";
           break;
         case 'medium_textarea':
-          field.html += "<textarea class='" + field.class + " a17-input-medium-editor textarea-medium-editor' name='" + field.name + "_{{lang}}' rows='20' data-medium-editor-show-button='Display source code' data-medium-editor-hide-button='Hide source code' " + field.attr + " />";
+          field.html += "<textarea class='" + field.class + " a17-input-medium-editor textarea-medium-editor' name='" + field.name + (field.repeater ? "{{repeaterIndex}}" : "") + "_{{lang}}" + "' rows='20' data-medium-editor-show-button='Display source code' data-medium-editor-hide-button='Hide source code' " + field.attr + " />";
           break;
       }
 
