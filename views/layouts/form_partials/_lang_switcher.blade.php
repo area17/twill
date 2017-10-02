3d8984b74b1ea4de2a2ea8cd95750425ab8073e4
Mubashar Iqbal <me@mubs.me>
2017-10-02 11:09:12 -0400

    Translatable supports nested languages, including in the default configuration, the language switcher does not

--- views/layouts/form_partials/_lang_switcher.blade.php
+++ views/layouts/form_partials/_lang_switcher.blade.php
@@ -7,11 +7,13 @@
     <div class="lang-switcher lang-num{{ count($lang_switcher_locales) }}" data-behavior="lang_switcher">
         <div class="lang-label">
             @foreach ($lang_switcher_locales as $locale)
-                <a href="#" data-lang="{{ $locale }}" title="{{ strtoupper($locale) }}" @if(($loop->first && !request()->has('locale')) || request('locale') === $locale) class="selected" @endif style="width:50px;">
-                    <span class="">
-                        {{strtoupper($locale)}}
-                    </span>
-                </a>
+                @if (!is_array($locale)))
+                    <a href="#" data-lang="{{ $locale }}" title="{{ strtoupper($locale) }}" @if(($loop->first && !request()->has('locale')) || request('locale') === $locale) class="selected" @endif style="width:50px;">
+                        <span class="">
+                            {{strtoupper($locale)}}
+                        </span>
+                    </a>
+                @endif
             @endforeach
         </div>
         <div class="lang-selector">
