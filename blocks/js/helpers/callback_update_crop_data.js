018ee3cd3e39e0edfa02838ca8c0bf722185cde5
Quentin Renard <quentin@area17.com>
2017-10-30 01:42:26 +0100

    WIP — Remove old assets

--- blocks/js/helpers/callback_update_crop_data.js
+++
@@ -1,8 +0,0 @@
-a17cms.Helpers.callbackUpdateCropData = function(datas) {
-  var $editor = $("#" + datas["role"]);
-  var $hidden_crop_data = $("[data-crop]", $editor);
-
-  if(datas) {
-    $hidden_crop_data.val(JSON.stringify(datas['data']));
-  }
-};
--- config/blocks.php
+++ config/blocks.php
@@ -7,32 +7,10 @@ return [
     |--------------------------------------------------------------------------
     |
     | This array allows you to provide the package with your configuration
-    | for the Block renderer service.
-    | More to come here...
+    | for the Block editor service.
     |
      */
-    'blocks_js_path' => '/assets/admin/blocks/blocks.js',
-    'blocks_js_rev' => false,
-
-    'blocks_css_path' => 'blocks.css',
-    'blocks_css_rev' => true,
-
     'use_iframes' => false,
     'iframe_wrapper_view' => '',
-
     'show_render_errors' => env('BLOCK_EDITOR_SHOW_ERRORS', false),
-
-    'blocks' => [
-        "blocktitle" => "A17\CmsToolkit\Services\BlockEditor\Blocks\Text",
-        "blocktext" => "A17\CmsToolkit\Services\BlockEditor\Blocks\Text",
-        "blockquote" => "A17\CmsToolkit\Services\BlockEditor\Blocks\Text",
-        "image" => "A17\CmsToolkit\Services\BlockEditor\Blocks\Image",
-        "imagegrid" => "A17\CmsToolkit\Services\BlockEditor\Blocks\Image",
-        "imagetext" => "A17\CmsToolkit\Services\BlockEditor\Blocks\Image",
-        "diaporama" => "A17\CmsToolkit\Services\BlockEditor\Blocks\Image",
-        "blockseparator" => "A17\CmsToolkit\Services\BlockEditor\Blocks\Separator",
-    ],
-    'sitemap_blocks' => [
-        'A17\CmsToolkit\Services\BlockEditor\Blocks\Image',
-    ],
 ];
