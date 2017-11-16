3b4f5acd8e7776e35dbfbdaddb51de485e33595e
Antoine Doury <antoine@area17.com>
2017-11-16 12:45:50 +0100

    Show / Hide env colored bars when scrolling

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,8 +1,10 @@
 // General behaviors
 import navToggle from '@/behaviors/navToggle'
+import showEnvLine from '@/behaviors/showEnvLine'
 
 const A17Init = function () {
   navToggle()
+  showEnvLine()
 }
 
 export default A17Init
