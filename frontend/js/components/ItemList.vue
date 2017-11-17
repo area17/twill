b161917a0d8ba1721a9f9fd7e5998e46790daa37
Antoine Doury <antoine@area17.com>
2017-11-17 11:04:23 +0100

    Browser list style : Style of the thumbnail, spacings + style of the loading item

--- frontend/js/components/ItemList.vue
+++ frontend/js/components/ItemList.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="itemlist">
-    <div class="itemlist__item" v-for="(item, index) in listItemsLoading" :key="item.id">
-      <span class="itemlist__button s--loading">
-        <span class="itemlist__progress" v-if="!item.error"><span class="itemlist__progressBar" :style="loadingProgress(index)"></span></span>
+    <div class="itemlist__row" v-for="(item, index) in listItemsLoading" :key="item.id" >
+      <span class="itemlist__cell itemlist__cell--loading">
+        <span class="itemlist__progress" v-if="!item.error" ><span class="itemlist__progressBar" :style="loadingProgress(index)"></span></span>
         <span class="itemlist__progressError" v-else>Upload Error</span>
       </span>
     </div>
-    <div class="itemlist__item" v-for="(item, index) in listItems" :key="item.id" :class="{ 's--picked': isSelected(item.id) }" @click.prevent="toggleSelection(item.id)">
-      <span class="itemlist__button">
+    <div class="itemlist__row" v-for="(item, index) in listItems" :key="item.id" :class="{ 's--picked': isSelected(item.id) }" @click.prevent="toggleSelection(item.id)">
+      <span class="itemlist__cell itemlist__cell--btn">
         <a17-checkbox name="item_list" :value="item.id" :initialValue="checkedItems" theme="bold"></a17-checkbox>
-        <span v-if="item.hasOwnProperty('thumbnail')"><img :src="item.thumbnail" /></span>
-        {{ item.name }}
       </span>
+      <span class="itemlist__cell itemlist__cell--thumb" v-if="item.hasOwnProperty('thumbnail')"><img :src="item.thumbnail" /></span>
+      <span class="itemlist__cell itemlist__cell--name">{{ item.name }}</span>
       <span v-if="item.hasOwnProperty('size')">{{ item.size }}</span>
     </div>
   </div>
@@ -85,7 +85,7 @@
     padding: 10px;
   }
 
-  .itemlist__item {
+  .itemlist__row {
     display:flex;
     overflow: hidden;
     background:white;
@@ -94,24 +94,73 @@
     border:1px solid $color__border--light;
     margin-bottom: -1px;
     cursor:pointer;
-    padding:20px;
+    padding:20px 15px 20px 0;
 
     &:hover {
       background-color: $color__f--bg;
     }
   }
 
-  .itemlist__item:first-child {
+  .itemlist__row:first-child {
     border-top-left-radius: 2px;
     border-top-right-radius: 2px;
   }
 
-  .itemlist__button > *:first-child {
+  .itemlist__cell {
+    margin-left:15px;
+    white-space: nowrap;
+    overflow:hidden;
+    text-overflow:ellipsis;
+  }
+
+  .itemlist__cell > *:first-child {
     display: inline-block;
     vertical-align: top;
   }
 
-  .itemlist__button {
+  .itemlist__cell--btn {
+    width:15px;
+  }
+
+  .itemlist__cell--name {
     flex-grow: 1;
+  }
+
+  .itemlist__cell--thumb {
+    display:inline-block;
+    max-width:50px;
+
+    img {
+      display:block;
+      max-width:50px;
+      height:auto;
+    }
+  }
+
+  .itemlist__cell--loading {
+    flex-grow: 1;
+    height: 4px;
+  }
+
+  .itemlist__progress {
+    height: 4px;
+    width: 100%;
+    background: $color__border--focus;
+    border-radius: 2px;
+    position: relative;
+  }
+
+  .itemlist__progressBar {
+    position: absolute;
+    top:0;
+    left:0;
+    width: 100%;
+    border-radius: 2px;
+    height:4px;
+    background: $color__action;
+  }
+
+  .itemlist__progressError {
+    color:$color__error;
   }
 </style>
