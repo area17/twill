d19db61bf4fd444d5b30ff817e281006a258e62f
Quentin Renard <quentin@area17.com>
2017-11-19 21:53:34 +0100

    Set correct tab index on login page

--- views/auth/login.blade.php
+++ views/auth/login.blade.php
@@ -12,20 +12,20 @@
 
                     <fieldset class="login__fieldset">
                         <label class="login__label" for="email">Email</label>
-                        <input type="email" name="email" id="email" class="login__input" required autofocus />
+                        <input type="email" name="email" id="email" class="login__input" required autofocus tabindex="1"/>
                     </fieldset>
 
                     <fieldset class="login__fieldset">
                         <label class="login__label" for="password">Password</label>
-                        <a href="{{ route('admin.password.reset.link') }}" class="login__help f--small"><span>Forgot password</span></a>
-                        <input type="password" name="password" id="password" class="login__input" required />
+                        <a href="{{ route('admin.password.reset.link') }}" class="login__help f--small" tabindex="5"><span>Forgot password</span></a>
+                        <input type="password" name="password" id="password" class="login__input" required tabindex="2" />
                     </fieldset>
 
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                    <input class="login__button" type="submit" value="Login">
+                    <input class="login__button" type="submit" value="Login" tabindex="3">
 
-                    <a href="#" class="login__google">
+                    <a href="#" class="login__google" tabindex="4">
                         <span symbol="more-dots" class="icon icon--google-sign-in"><svg><title>Google Icon</title><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#google-sign-in"></use></svg></span>
                         <span>Sign in with Google</span>
                     </a>
