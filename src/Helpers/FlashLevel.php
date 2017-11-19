de14018702f8ae433f2b976faa20fb58158f3e30
Quentin Renard <quentin@area17.com>
2017-11-19 02:47:55 +0100

    Rename success flash level

--- src/Helpers/FlashLevel.php
+++ src/Helpers/FlashLevel.php
@@ -6,7 +6,7 @@ use MyCLabs\Enum\Enum;
 
 class FlashLevel extends Enum
 {
-    const SUCCESS = 'notice';
+    const SUCCESS = 'success';
     const ERROR = 'error';
     const WARNING = 'caution';
     const INFO = 'help';
