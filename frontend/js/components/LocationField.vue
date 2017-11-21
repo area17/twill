d9315fbf3ec50e74c37523851a2efbb2661081b6
Antonin <antonin@area17.com>
2017-11-21 18:24:38 +0100

    LocationField: Add option to show / hide map when showMap is enabled

--- frontend/js/components/LocationField.vue
+++ frontend/js/components/LocationField.vue
@@ -16,10 +16,14 @@
         @blur="onBlur"
         @input="onInput"
       />
+      <div v-if="showMap" class="form__field--showMap">
+        <a href="#" type="button" @click.prevent="toggleMap"><span v-svg symbol="location"></span><span v-html="mapMessage"></span></a>
+      </div>
 
-      <input type="hidden" :name="`${name}__lat`" :value="lat" />
-      <input type="hidden" :name="`${name}__lng`" :value="lng" />
+      <input type="hidden" :name="`${name}__lat`" :value="lat"/>
+      <input type="hidden" :name="`${name}__lng`" :value="lng"/>
     </div>
+    <div class="form__mapContainer" v-if="showMap" v-show="isMapOpen"></div>
   </a17-inputframe>
 </template>
 
@@ -28,6 +32,9 @@
   import FormStoreMixin from '@/mixins/formStore'
   import InputframeMixin from '@/mixins/inputFrame'
   import LocaleMixin from '@/mixins/locale'
+
+  const openMapMessage = 'Show&nbsp;map'
+  const hideMapMessage = 'Hide&nbsp;map'
 
   export default {
     name: 'A17Locationfield',
@@ -44,6 +51,10 @@
       showMap: {
         type: Boolean,
         default: true
+      },
+      openMap: {
+        type: Boolean,
+        default: false
       },
       initialLat: {
         type: Number,
@@ -62,7 +73,9 @@
         address: '',
         lat: parseFloat(this.initialLat),
         lng: parseFloat(this.initialLng),
-        focused: false
+        focused: false,
+        isMapOpen: this.openMap,
+        mapMessage: this.openMap ? hideMapMessage : openMapMessage
       }
     },
     computed: {
@@ -71,7 +84,7 @@
           return this.lat + '|' + this.lng
         },
         set (value) {
-          var coord = value.split('|')
+          const coord = value.split('|')
           this.lat = parseFloat(coord[0])
           this.lng = parseFloat(coord[coord.length - 1])
         }
@@ -145,62 +158,67 @@
       setLatLng: function (latlng) {
         this.lat = latlng.lat()
         this.lng = latlng.lng()
+      },
+      toggleMap: function () {
+        this.isMapOpen = !this.isMapOpen
+        this.mapMessage = this.isMapOpen ? hideMapMessage : openMapMessage
+        if (!this.map && google) this.initMap(google)
+      },
+      initMap: function (google) {
+        const preset = this.lat + this.lng
+
+        const mapOptions = {
+          zoom: preset ? this.zoom : 1,
+          center: new google.maps.LatLng(this.lat, this.lng),
+          mapTypeControl: false,
+          panControl: false,
+          zoomControl: false,
+          streetViewControl: false
+        }
+
+        // Init google API here
+        const mapCanvas = document.createElement('div')
+        mapCanvas.className = 'form__map'
+
+        this.$el.querySelector('.form__mapContainer').appendChild(mapCanvas)
+        this.map = new google.maps.Map(mapCanvas, mapOptions)
+
+        if (preset) {
+          this.addMarker(new google.maps.LatLng(this.lat, this.lng))
+        }
       }
     },
     mounted: function () {
-      let self = this
-
-      const preset = this.lat + this.lng
-
-      const mapOptions = {
-        zoom: preset ? this.zoom : 1,
-        center: new google.maps.LatLng(this.lat, this.lng),
-        mapTypeControl: false,
-        panControl: false,
-        zoomControl: false,
-        streetViewControl: false
-      }
-
+      const self = this
       /* global google */
       if (google) {
-        if (this.showMap) {
-          // Init google API here
-          const mapCanvas = document.createElement('div')
-          mapCanvas.className = 'form__map'
-
-          this.$el.appendChild(mapCanvas)
-
-          this.map = new google.maps.Map(mapCanvas, mapOptions)
-        }
-
         // Create the autocomplete object and associate it with the UI input control.
-        this.autocompletePlace = new google.maps.places.Autocomplete(self.$el.querySelector('input[type="search"]'))
-
+        this.autocompletePlace = new google.maps.places.Autocomplete(this.$el.querySelector('input[type="search"]'))
         // When a place is selected
         google.maps.event.addListener(this.autocompletePlace, 'place_changed', this.onPlaceChanged)
 
-        if (preset) {
-          if (this.showMap) this.addMarker(new google.maps.LatLng(self.lat, self.lng))
+        if (this.address === '') {
+          const geocoder = new google.maps.Geocoder()
+          const location = {lat: this.lat, lng: this.lng}
 
           // reverse geocoding
-          if (this.address === '') {
-            const geocoder = new google.maps.Geocoder()
-            const location = { lat: self.lat, lng: self.lng }
-
-            geocoder.geocode({
-              'location': location
-            }, function (results, status) {
-              if (status === 'OK') {
-                if (results[1]) {
-                  self.address = results[1].formatted_address
-                } else {
-                  console.info('Geocoding - No results found')
-                }
+          geocoder.geocode({
+            'location': location
+          }, function (results, status) {
+            if (status === 'OK') {
+              if (results[1]) {
+                self.address = results[1].formatted_address
               } else {
-                console.warn('Geocoding - Geocoder failed due to: ' + status)
+                console.info('Geocoding - No results found')
               }
-            })
-          }
+            } else {
+              console.warn('Geocoding - Geocoder failed due to: ' + status)
+            }
+          })
+        }
+
+        if (this.showMap && this.isMapOpen) {
+          this.initMap(google)
         }
       }
     },
@@ -209,3 +227,38 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  @import '~styles/setup/_mixins-colors-vars.scss';
+
+  .form__field {
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+
+    input {
+      padding: 0;
+    }
+
+    .form__field--showMap {
+
+      a {
+        @include font-tiny();
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        color: $color__text--light;
+        transition: color 250ms;
+
+        &:hover {
+          color: $color__text--forms;
+        }
+
+        span {
+          margin-right: 5px;
+        }
+      }
+    }
+  }
+
+</style>
