f9b02ce0fa147bdcf2a7affb5ad336eab7e6c5d0
Quentin Renard <quentin@area17.com>
2017-12-04 03:03:10 +0100

    Read block editor available blocks from config

--- config/blocks.php
+++ config/blocks.php
@@ -10,6 +10,62 @@ return [
     | for the Block editor service.
     |
      */
+    'blocks' => [
+        'title' => [
+            'title' => 'Title',
+            'icon' => 'text',
+            'component' => 'a17-block-title',
+        ],
+        'quote' => [
+            'title' => 'Quote',
+            'icon' => 'quote',
+            'component' => 'a17-block-quote',
+        ],
+        'text' => [
+            'title' => 'Body text',
+            'icon' => 'text',
+            'component' => 'a17-block-wysiwyg',
+        ],
+        'image' => [
+            'title' => 'Full width Image',
+            'icon' => 'image',
+            'component' => 'a17-block-image',
+            'attributes' => [
+                'cropContext' => 'cover',
+            ],
+        ],
+        'sonia' => [
+            'title' => 'Sonia',
+            'icon' => 'text',
+            'component' => 'a17-block-sonia',
+        ],
+        'charvet' => [
+            'title' => 'Charvet',
+            'icon' => 'text',
+            'component' => 'a17-block-charvet',
+        ],
+        'grid' => [
+            'title' => 'Grid',
+            'icon' => 'text',
+            'component' => 'a17-block-grid',
+        ],
+        'complex' => [
+            'title' => 'Complex block test',
+            'icon' => 'image',
+            'component' => 'a17-block-test',
+        ],
+        'publications' => [
+            'title' => 'Publication Grid',
+            'icon' => 'text',
+            'component' => 'a17-browserfield',
+            'attributes' => [
+                'max' => 4,
+                'itemLabel' => 'Publications',
+                'endpoint' => 'https://www.mocky.io/v2/59d77e61120000ce04cb1c5b',
+                'modalTitle' => 'Attach publications',
+            ],
+        ],
+    ],
     'use_iframes' => false,
     'iframe_wrapper_view' => '',
     'show_render_errors' => env('BLOCK_EDITOR_SHOW_ERRORS', false),
--- views/partials/form/_block_editor.blade.php
+++ views/partials/form/_block_editor.blade.php
@@ -2,62 +2,12 @@
 
 <a17-content title="Add Content"></a17-content>
 
+@php
+    $availableBlocks = isset($blocks) ? collect($blocks)->map(function ($block) {
+        return config('cms-toolkit.block-editor.blocks.' . $block);
+    })->filter()->toArray() : config('cms-toolkit.block-editor.blocks');
+@endphp
+
 @push('fieldsStore')
-    window.STORE.form.content = [
-        {
-            title: 'Title',
-            icon: 'text',
-            component: 'a17-block-title'
-        },
-        {
-            title: 'Quote',
-            icon: 'quote',
-            component: 'a17-block-quote'
-        },
-        {
-            title: 'Body text',
-            icon: 'text',
-            component: 'a17-block-wysiwyg'
-        },
-        {
-            title: 'Full width Image',
-            icon: 'image',
-            component: 'a17-block-image',
-            attributes: {
-                cropContext: 'cover'
-            }
-        },
-        {
-            title: 'Grid',
-            icon: 'text',
-            component: 'a17-block-grid'
-        },
-        {
-            title: 'Complex block test',
-            icon: 'image',
-            component: 'a17-block-test'
-        },
-        {
-            title: 'Publication Grid',
-            icon: 'text',
-            component: 'a17-browserfield',
-            attributes: {
-                max: 4,
-                itemLabel: 'Publications',
-                endpoint: 'https://www.mocky.io/v2/59d77e61120000ce04cb1c5b',
-                modalTitle: 'Attach publications'
-            }
-        },
-        {
-            title: 'News Grid',
-            icon: 'text',
-            component: 'a17-browserfield',
-            attributes: {
-                max: 4,
-                itemLabel: 'News',
-                endpoint: 'https://www.mocky.io/v2/59d77e61120000ce04cb1c5b',
-                modalTitle: 'News publications'
-            }
-        }
-    ]
+    window.STORE.form.content = {!! json_encode(array_values($availableBlocks)) !!}
 @endpush
