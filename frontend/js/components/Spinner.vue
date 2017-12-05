461dc059e1feee20cb6f527f6348c266b29aced6
Antoine Doury <antoine@area17.com>
2017-12-05 11:35:52 +0100

    A17 spinner - show only loader after a certain time

--- frontend/js/components/Spinner.vue
+++ frontend/js/components/Spinner.vue
@@ -1,12 +1,47 @@
-<template functional>
-  <transition name="fade_spinner">
+<template>
+  <transition name="fade_spinner"  v-on:after-enter="afterEnter" v-on:before-leave="beforeLeave">
     <div class="spinner__container">
-      <div class="spinner">
-        <slot></slot>
+      <div class="spinner" :class="{ 'spinner--visible' : visible }">
+        <svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
+            <g fill="none" fill-rule="evenodd">
+                <g transform="translate(1 1)" stroke-width="2">
+                    <circle stroke-opacity=".5" cx="18" cy="18" r="18"/>
+                    <path d="M36 18c0-9.94-8.06-18-18-18">
+                        <animateTransform
+                            attributeName="transform"
+                            type="rotate"
+                            from="0 18 18"
+                            to="360 18 18"
+                            dur="1s"
+                            repeatCount="indefinite"/>
+                    </path>
+                </g>
+            </g>
+        </svg>
+        <!-- <slot></slot> -->
       </div>
     </div>
   </transition>
 </template>
+
+<script>
+  export default {
+    name: 'A17Spinner',
+    data: function () {
+      return {
+        visible: false
+      }
+    },
+    methods: {
+      afterEnter: function (el) {
+        this.visible = true
+      },
+      beforeLeave: function (el) {
+        this.visible = false
+      }
+    }
+  }
+</script>
 
 <style lang="scss" scoped>
   @import '~styles/setup/_mixins-colors-vars.scss';
@@ -31,6 +66,13 @@
       position: relative;
       text-align: center;
       color:$color__text--light;
+      opacity:0;
+      transition: opacity .25s linear;
+      transition-delay:0.5s;
+
+      &.spinner--visible {
+        opacity:1;
+      }
     }
   }
 </style>
