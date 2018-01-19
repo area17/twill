f85ce9ad171ffba70276133a3e7357fe50c3cabb
Antoine Doury <antoine@area17.com>
2018-01-19 17:21:54 +0100

    Add new getFormData helper to process the form store into something the back-end will understand

--- frontend/js/store/modules/form.js
+++ frontend/js/store/modules/form.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import api from '../api/form'
 import * as types from '../mutation-types'
+import { getFormData } from '@/utils/getFormData.js'
 
 const state = {
   loading: false,
@@ -97,36 +98,12 @@ const actions = {
     commit(types.CLEAR_FORM_ERRORS)
     commit(types.CLEAR_NOTIF, 'error')
 
-    let fields = state.fields.filter((field) => {
-      // we start by filtering out blocks related form fields
-      return !field.name.startsWith('blocks[')
-    }).reduce((fields, field) => {
-      // and we create a new object with field names as keys,
-      // to inline fields in the submitted data
-      fields[field.name] = field.value
-      return fields
-    }, {})
-
     // we can now create our submitted data object out of:
     // - our just created fields object,
     // - publication properties
     // - selected medias and browsers
     // - created blocks and repeaters
-    let data = Object.assign(fields, {
-      parentId: rootState.parents.active,
-      cmsSaveType: saveType,
-      published: rootState.publication.published,
-      public: rootState.publication.visibility === 'public',
-      publish_start_date: rootState.publication.startDate,
-      publish_end_date: rootState.publication.endDate,
-      languages: rootState.language.all,
-      medias: gatherSelected(rootState.mediaLibrary.selected),
-      browsers: gatherSelected(rootState.browser.selected),
-      blocks: rootState.content.blocks.map(block => {
-        return buildBlock(block, state, rootState)
-      }),
-      repeaters: gatherRepeaters(state, rootState)
-    })
+    const data = getFormData(rootState, saveType)
 
     api.save(state.saveUrl, data, function (successResponse) {
       commit(types.UPDATE_FORM_LOADING, false)
@@ -142,91 +119,6 @@ const actions = {
       commit(types.SET_NOTIF, { message: 'Your submission could not be validated, please fix and retry', variant: 'error' })
     })
   }
-}
-
-const isBlockField = (name, id) => {
-  return name.startsWith('blocks[' + id + ']')
-}
-
-const stripOutBlockNamespace = (name, id) => {
-  return name.replace('blocks[' + id + '][', '').slice(0, -1)
-}
-
-/*
-* Gather selected items in a selected object (currently used for medias and browsers)
-* if a block is passed as second argument, we retrieve selected items namespaced by the block id
-* and strip it out from the key to clean things up and make it easier for the backend
-*/
-const gatherSelected = (selected, block = null) => {
-  return Object.assign({}, ...Object.keys(selected).map(key => {
-    if (block) {
-      if (isBlockField(key, block.id)) {
-        return {
-          [stripOutBlockNamespace(key, block.id)]: selected[key]
-        }
-      }
-    } else if (!key.startsWith('blocks[')) {
-      return {
-        [key]: selected[key]
-      }
-    }
-    return null
-  }).filter(x => x))
-}
-
-const buildBlock = (block, state, rootState) => {
-  return {
-    type: block.type,
-    // retrieve all fields for this block and clean up field names
-    content: state.fields.filter((field) => {
-      return isBlockField(field.name, block.id)
-    }).map((field) => {
-      return {
-        name: stripOutBlockNamespace(field.name, block.id),
-        value: field.value
-      }
-    }).reduce((content, field) => {
-      content[field.name] = field.value
-      return content
-    }, {}),
-    medias: gatherSelected(rootState.mediaLibrary.selected, block),
-    browsers: gatherSelected(rootState.browser.selected, block),
-    // gather repeater blocks from the repeater store module
-    blocks: Object.assign({}, ...Object.keys(rootState.repeaters.repeaters).filter(repeaterKey => {
-      return repeaterKey.startsWith('blocks-' + block.id)
-    }).map(repeaterKey => {
-      return {
-        [repeaterKey.replace('blocks-' + block.id + '_', '')]: rootState.repeaters.repeaters[repeaterKey].map(repeaterItem => {
-          return buildBlock(repeaterItem, state, rootState)
-        })
-      }
-    }))
-  }
-}
-
-const gatherRepeaters = (state, rootState) => {
-  return Object.assign({}, ...Object.keys(rootState.repeaters.repeaters).filter(repeaterKey => {
-      // we start by filtering out repeater blocks
-    return !repeaterKey.startsWith('blocks-')
-  }).map(repeater => {
-    return {
-      [repeater]: rootState.repeaters.repeaters[repeater].map(repeaterItem => {
-        // and for each repeater we build a block for each item
-        let repeaterBlock = buildBlock(repeaterItem, state, rootState)
-
-        // we want to inline fields in the repeater object
-        // and we don't need the type of component used
-        let fields = repeaterBlock.content
-        delete repeaterBlock.content
-        delete repeaterBlock.type
-
-        // and lastly we want to keep the id to update existing items
-        repeaterBlock.id = repeaterItem.id
-
-        return Object.assign(repeaterBlock, fields)
-      })
-    }
-  }))
 }
 
 export default {
