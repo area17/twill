40e93a0afd14b2342053548f638d23c5d658b206
Quentin Renard <quentin@area17.com>
2018-02-19 08:02:24 +0100

    Improves errors UX on multilingual modules

--- frontend/js/mixins/inputFrame.js
+++ frontend/js/mixins/inputFrame.js
@@ -1,3 +1,10 @@
+function arrayToSentence (arr) {
+  var length = arr.length
+  return arr.reduce(function (a, b, c) {
+    return a + (c - 1 === length ? ', ' : ' and ') + b
+  })
+}
+
 export default {
   props: {
     label: {
@@ -20,6 +27,31 @@ export default {
   computed: {
     errorKey () {
       return this.hasLocale ? (this.name.replace('[', '.').replace(']', '')) : this.name
+    },
+    errorLocales () {
+      if (!this.hasLocale) return []
+
+      const errorKeyWithoutLocale = this.errorKey.substr(0, this.errorKey.indexOf('.'))
+
+      const locales = []
+
+      Object.keys(this.$store.state.form.errors).forEach((error) => {
+        if (error.substr(0, error.indexOf('.')) === errorKeyWithoutLocale) {
+          locales.push(error.substr(error.indexOf('.') + 1, error.length))
+        }
+      }, [])
+
+      return locales
+    },
+    otherLocalesError () {
+      return this.errorLocales.filter((locale) => {
+        return locale !== this.currentLocale.value
+      }).length
+    },
+    errorMessageLocales () {
+      return arrayToSentence(this.errorLocales.map((locale) => {
+        return this.languages.find(l => l.value === locale).label
+      })) + ' language' + (this.errorLocales.length > 1 ? 's' : '') + ' missing details.'
     },
     errorMessage () {
       let message = this.error ? this.$store.state.form.errors[this.errorKey][0] : ''
