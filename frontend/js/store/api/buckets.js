bb1aa025e290bd2b4d5681f6921b07f015ecd573
Antonin Caudron <antonin@area17.com>
2018-02-21 11:39:33 +0100

    Buckets: refactor API and all global errors handler

--- frontend/js/store/api/buckets.js
+++ frontend/js/store/api/buckets.js
@@ -1,23 +1,33 @@
 import axios from 'axios'
 import { getURLWithoutQuery } from '@/utils/pushState.js'
+import { globalError } from '@/utils/errors'
+
+const component = 'BUCKETS'
 
 export default {
 
-  get: function (params, callback) {
-    axios.get(getURLWithoutQuery(), {params: params}).then(function (resp) {
+  get: function (params, callback, errorCallback) {
+    axios.get(getURLWithoutQuery(), {
+      params: params
+    })
+    .then((resp) => {
       if (callback && typeof callback === 'function') callback(resp.data)
-    }, function (resp) {
-      // error callback
+    })
+    .catch((resp) => {
+      globalError(component, resp)
+      if (errorCallback && typeof errorCallback === 'function') errorCallback(resp)
     })
   },
 
   save (endpoint, params, callback, errorCallback) {
-    axios.post(endpoint, params).then(function (resp) {
-      if (callback && typeof callback === 'function') callback(resp)
-    }, function (resp) {
+    axios.post(endpoint, params)
+      .then((resp) => {
+        if (callback && typeof callback === 'function') callback(resp)
+      })
+      .catch((resp) => {
       // error callback
-      if (errorCallback && typeof errorCallback === 'function') errorCallback(resp)
-      console.warn('save request error.')
-    })
+        globalError(component, resp)
+        if (errorCallback && typeof errorCallback === 'function') errorCallback(resp)
+      })
   }
 }
