2349763a8f8834fd228a513560d638971bb90425
Antoine Doury <antoine@area17.com>
2018-03-13 16:35:24 +0100

    Hidden fields - Simple way to manage hidden fields in form and modal

---
+++ frontend/js/components/HiddenField.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="form__input form__input--hidden">
+    <input type="hidden" :name="name" :id="uniqId" :value="value" />
+  </div>
+</template>
+
+<script>
+  import randKeyMixin from '@/mixins/randKey'
+  import InputMixin from '@/mixins/input'
+  import FormStoreMixin from '@/mixins/formStore'
+
+  export default {
+    name: 'A17HiddenField',
+    mixins: [randKeyMixin, InputMixin, FormStoreMixin],
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      initialValue: {
+        default: ''
+      }
+    },
+    computed: {
+      uniqId: function () {
+        return this.name + '-' + this.randKey
+      }
+    },
+    data: function () {
+      return {
+        value: this.initialValue
+      }
+    },
+    watch: {
+      initialValue: function () {
+        this.value = this.initialValue
+      }
+    },
+    methods: {
+      updateFromStore: function (newValue) { // called from the formStore mixin
+        if (typeof newValue === 'undefined') newValue = ''
+
+        if (this.value !== newValue) {
+          this.value = newValue
+        }
+      }
+    }
+  }
+</script>
---
+++ views/partials/form/_hidden.blade.php
@@ -0,0 +1,23 @@
+@php
+    $value = $value ?? false;
+@endphp
+
+<a17-hiddenfield
+    @include('cms-toolkit::partials.form.utils._field_name')
+    @if ($value) :initial-value="'{{ $value }}'" @endif
+    :has-default-store="true"
+    in-store="value"
+></a17-hiddenfield>
+
+@unless($renderForBlocks || $renderForModal)
+@push('vuexStore')
+
+@if(isset($item->$name))
+    window.STORE.form.fields.push({
+        name: '{{ $name }}',
+        value: {!! json_encode($item->$name) !!}
+    })
+@endif
+
+@endpush
+@endunless
