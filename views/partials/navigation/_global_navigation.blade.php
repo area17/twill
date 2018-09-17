e48560fe2ec32bbc3540fdf50b6b288a6a840150
Quentin Renard <quentin@area17.com>
2018-09-16 23:29:28 -0400

    Clean global navigation when nothing has been defined by the host application

--- views/partials/navigation/_global_navigation.blade.php
+++ views/partials/navigation/_global_navigation.blade.php
@@ -1,18 +1,20 @@
 @if (config()->has('twill-navigation'))
     <nav class="header__nav">
-        <ul class="header__items">
-            @foreach(config('twill-navigation') as $global_navigation_key => $global_navigation_element)
-                @can($global_navigation_element['can'] ?? 'list')
-                    @if(isActiveNavigation($global_navigation_element, $global_navigation_key, $_global_active_navigation))
-                        <li class="header__item s--on">
-                    @else
-                        <li class="header__item">
-                    @endif
-                            <a href="{{ getNavigationUrl($global_navigation_element, $global_navigation_key) }}">{{ $global_navigation_element['title'] }}</a>
-                        </li>
-                @endcan
-            @endforeach
-        </ul>
+        @if(!empty(config('twill-navigation')))
+            <ul class="header__items">
+                @foreach(config('twill-navigation') as $global_navigation_key => $global_navigation_element)
+                    @can($global_navigation_element['can'] ?? 'list')
+                        @if(isActiveNavigation($global_navigation_element, $global_navigation_key, $_global_active_navigation))
+                            <li class="header__item s--on">
+                        @else
+                            <li class="header__item">
+                        @endif
+                                <a href="{{ getNavigationUrl($global_navigation_element, $global_navigation_key) }}">{{ $global_navigation_element['title'] }}</a>
+                            </li>
+                    @endcan
+                @endforeach
+            </ul>
+        @endif
         @if (config('twill.enabled.media-library') || config('twill.enabled.file-library') || config('twill.enabled.site-link'))
             <ul class="header__items">
                 @can('list')
