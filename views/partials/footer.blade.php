89513030f2d6a5b8b439663ea55c9e51f6e1ea55
Yanhao Li <hi@yanhaoli.com>
2018-10-19 10:56:57 -0400

    separate the notication toaster component from footer

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -1,24 +1,4 @@
-@if($errors->any())
-    <div class="notif notif--error">
-        <div class="notif__inner">{{ $errors->first() }}</div>
-    </div>
-@elseif (session('status'))
-    <div class="notif notif--success">
-        <div class="notif__inner">
-            <button type="button" class="notif__close" aria-label="alertClose" onclick="this.parentNode.parentNode.remove()">
-                <span class="icon icon--close_modal">
-                    <svg><title>Close</title><use xlink:href="#close_modal"></use></svg>
-                </span>
-            </button>
-            {{ session('status') }}
-        </div>
-    </div>
-@elseif (session('restoreMessage'))
-    <div class="notif notif--warning">
-        <div class="notif__inner">{{ session('restoreMessage') }}</div>
-    </div>
-@endif
-
+@include('twill::partials.toaster')
 <footer class="footer">
     <div class="container">
         <span class="footer__copyright"><a href="https://twill.io" target="_blank" class="f--light-hover" tabindex="0">Made with Twill</a></span>
