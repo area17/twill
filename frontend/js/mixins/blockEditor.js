473124fba34c61674b610c5e20f2217171ca4a62
Antonin Caudron <antonin@area17.com>
2018-12-11 22:32:59 +0100

    Add blockItem and blockEditor mixins

---
+++ frontend/js/mixins/blockEditor.js
@@ -0,0 +1,63 @@
+import cloneDeep from 'lodash/cloneDeep'
+
+export default {
+  props: {
+    section: {
+      type: String,
+      default: 'default'
+    },
+    blocks: {
+      type: Array,
+      default: () => []
+    },
+    savedBlocksLength: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    addAndEditBlock (add, edit, {block, index}) {
+      window.PREVSTATE = cloneDeep(this.$store.state)
+      console.log('add-and-edit-block', {block, index})
+      add(block, index)
+      edit(index)
+    },
+    saveBlock (fn, index) {
+      if (window.hasOwnProperty('PREVSTATE')) delete window.PREVSTATE
+      this.unselectBlock(fn, index)
+      this.$emit('block:save', index)
+    },
+    cancelBlock (fn, index) {
+      if (window.hasOwnProperty('PREVSTATE')) {
+        console.warn('Store - Restore previous Store state')
+        this.$store.replaceState(window.PREVSTATE)
+      }
+      this.unselectBlock(fn, index)
+      this.$emit('block:cancel', index)
+    },
+    selectBlock (fn, index) {
+      window.PREVSTATE = cloneDeep(this.$store.state)
+      fn()
+      this.$emit('block:select', index)
+    },
+    unselectBlock (fn, index) {
+      fn()
+      if (window.hasOwnProperty('PREVSTATE')) delete window.PREVSTATE
+      this.$emit('block:unselect', index)
+    },
+    moveBlock (index) {
+      this.$emit('block:move', index)
+    },
+    deleteBlock (fn) {
+      if (this.$root.$refs.warningContentEditor) {
+        this.$root.$refs.warningContentEditor.open(() => {
+          fn()
+          this.$emit('block:delete')
+        })
+      } else {
+        fn()
+        this.$emit('block:delete')
+      }
+    }
+  }
+}
