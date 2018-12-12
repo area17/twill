09368995ddcce1e6b3567d11c7953f67a97bc38e
Quentin Renard <quentin@area17.com>
2018-12-12 16:07:03 -0500

    Fix select field value in settings for codebases with multiple languages

--- views/partials/form/utils/_selector_input_store.blade.php
+++ views/partials/form/utils/_selector_input_store.blade.php
@@ -2,7 +2,16 @@ window.STORE.form.fields.push({
     name: '{{ $name }}',
     value: @if(isset($item) && is_numeric($item->$name)) {{ $item->$name }}
            @elseif(isset($item->$name)) {!! json_encode($item->$name) !!}
-           @elseif(isset($formFieldsValue) && is_numeric($formFieldsValue)) {{ $formFieldsValue }}
-           @else {!! $formFieldsValue !!}
+           @elseif(isset($formFieldsValue))
+                @if (is_array($formFieldsValue))
+                    @php
+                        $formFieldsValue = array_first($formFieldsValue, null, '');
+                    @endphp
+                @endif
+                @if (is_numeric($formFieldsValue)) {{ $formFieldsValue }}
+                @else {!! $formFieldsValue !!}
+                @endif
+           @else
+            ''
            @endif
 })
