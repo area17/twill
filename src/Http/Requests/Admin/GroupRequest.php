747787e08f34a47080dc10d21203b22bc3ffc36c
Yanhao Li <hi@yanhaoli.com>
2019-01-28 18:17:57 -0500

    added GroupRequest

---
+++ src/Http/Requests/Admin/GroupRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace A17\Twill\Http\Requests\Admin;
+
+class GroupRequest extends Request
+{
+    public function rulesForCreate()
+    {
+        return [];
+    }
+
+    public function rulesForUpdate()
+    {
+        return [];
+    }
+}
