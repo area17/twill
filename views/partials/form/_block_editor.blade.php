8b1fa5946562d6aba5fc2cc5ac4f805f3fd7dc6e
marcriepen <klever.marc@gmail.com>
2019-03-29 12:17:02 +0100

    The block editor now accepts a title, to customize the label on the button. If left out it falls back to 'Add Content'

--- views/partials/form/_block_editor.blade.php
+++ views/partials/form/_block_editor.blade.php
@@ -1,7 +1,10 @@
+@php
+    $title = $title ?? 'Add content'
+@endphp
 @unless ($withoutSeparator ?? false)
     <hr/>
 @endif
-<a17-content title="Add content"></a17-content>
+<a17-content title="{{ $title }}"></a17-content>
 
 @php
     $availableBlocks = isset($blocks) ? collect($blocks)->map(function ($block) {
@@ -12,19 +15,19 @@
 @push('vuexStore')
     window.STORE.form.content = {!! json_encode(array_values($availableBlocks)) !!}
     window.STORE.form.blocks = {!! json_encode($form_fields['blocks'] ?? []) !!}
-
+    
     @foreach($form_fields['blocksFields'] ?? [] as $field)
         window.STORE.form.fields.push({!! json_encode($field) !!})
     @endforeach
-
+    
     @foreach($form_fields['blocksMedias'] ?? [] as $name => $medias)
         window.STORE.medias.selected["{{ $name }}"] = {!! json_encode($medias) !!}
     @endforeach
-
+    
     @foreach($form_fields['blocksFiles'] ?? [] as $name => $files)
         window.STORE.medias.selected["{{ $name }}"] = {!! json_encode($files) !!}
     @endforeach
-
+    
     @foreach($form_fields['blocksBrowsers'] ?? [] as $name => $browser)
         window.STORE.browser.selected["{{ $name }}"] = {!! json_encode($browser) !!}
     @endforeach
