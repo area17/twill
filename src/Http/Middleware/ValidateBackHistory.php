2ee1080d84f7f6c0b254947f3c45829008736c8a
Žiga Pavlin <ziga.pavle@gmail.com>
2019-04-17 05:45:54 +0200

    Fix missing header method exception in ValidateBackHistory middleware (#234)

--- src/Http/Middleware/ValidateBackHistory.php
+++ src/Http/Middleware/ValidateBackHistory.php
@@ -11,10 +11,9 @@ class ValidateBackHistory
     {
         $response = $next($request);
 
-        return $response
+        return !method_exists($response, 'header') ? $response : $response
             ->header('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate')
             ->header('Pragma', 'no-cache')
             ->header('Expires', 'Sat, 01 Jan 1990 00:00:00 GMT');
-
     }
 }
