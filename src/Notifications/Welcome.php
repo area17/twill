cde9da3dcd5de56c2843c3d6e7d5737dc63178f9
Yanhao LI <hi@yanhaoli.com>
2019-06-26 12:38:13 -0400

    Updated welcome email scheme from http to https (#311)

--- src/Notifications/Welcome.php
+++ src/Notifications/Welcome.php
@@ -10,7 +10,7 @@ class Welcome extends ResetPassword
     public function toMail($notifiable)
     {
         return (new MailMessage)->markdown('twill::emails.html.email', [
-            'url' => url('http://' . config('twill.admin_app_url') . route('admin.password.reset.welcome.form', $this->token, false)),
+            'url' => url(request()->getScheme() . '://' . config('twill.admin_app_url') . route('admin.password.reset.welcome.form', $this->token, false)),
             'actionText' => 'Choose your own password',
             'title' => 'Welcome',
             'copy' => 'You are receiving this email because an account was created for you on ' . config('app.name') . '.',
