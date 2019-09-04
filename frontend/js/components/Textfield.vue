dd755d88024ca5ef5ce417ed5541726ea3fbcfc4
Antoine Vinial <avinial@area17.com>
2019-09-04 18:09:07 +0200

    Adding validity indicator (only email input for the moment) that shows a green/red circle on top of the field based on the value.

--- frontend/js/components/Textfield.vue
+++ frontend/js/components/Textfield.vue
@@ -88,6 +88,7 @@
         @input="onInput"
       />
       <span class="input__limit f--tiny" :class="limitClasses" v-if="hasMaxlength">{{ counter }}</span>
+      <span :class="validityClasses" v-if="type === 'email'"></span>
     </div>
   </a17-inputframe>
 </template>
@@ -155,6 +156,13 @@
         return {
           'input__limit--red': this.counter < 10
         }
+      },
+      validityClasses: function () {
+        return [
+          'input__validity',
+          this.isFieldValid === true ? 'input__validity--valid' : '',
+          this.isFieldValid === false ? 'input__validity--error' : ''
+        ]
       }
     },
     data: function () {
@@ -162,6 +170,7 @@
         value: this.initialValue,
         lastSavedValue: this.initialValue,
         focused: false,
+        isFieldValid: null,
         counter: 0
       }
     },
@@ -208,6 +217,7 @@
       onInput: debounce(function (event) {
         let newValue = event.target.value
         this.updateAndSaveValue(newValue)
+        this.checkFieldValidity(event.target)
 
         this.$emit('change', newValue)
       }, 250),
@@ -220,6 +230,23 @@
         if (clone) {
           let h = clone.scrollHeight
           this.$refs.input.style.minHeight = `${h + minH}px`
+        }
+      },
+      checkFieldValidity: function (el) {
+        // Switch based on the type of the field.
+        switch (el.type) {
+          case 'email':
+            // If user didn't type any character, return.
+            if (el.value.length < 1) {
+              this.isFieldValid = null
+              return
+            }
+
+            // Get pattern and test validity with regex.
+            const pattern = el.pattern
+            const re = RegExp(pattern)
+            this.isFieldValid = re.test(this.value)
+            break
         }
       }
     },
@@ -333,6 +360,26 @@
 
   .input__limit--red {
     color:red;
+  }
+
+  .input__validity {
+    position: absolute;
+    top: 17px;
+    right: 15px;
+    width: 10px;
+    height: 10px;
+    background-color: $color__tag--disabled;
+    border-radius: 50%;
+    user-select: none;
+    pointer-events: none;
+
+    &--valid {
+      background-color: $color__ok;
+    }
+
+    &--error {
+      background-color: $color__error;
+    }
   }
 
   .input__field--textarea {
