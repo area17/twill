c39ca8e93b3935d46c24a9c2490fbf816c05df42
Yanhao Li <hi@yanhaoli.com>
2019-09-12 17:42:42 -0400

    Refactored AuthServiceProvider to use the authorize callback function

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -11,16 +11,19 @@ class AuthServiceProvider extends ServiceProvider
 
     protected function authorize($user, $callback)
     {
-        Gate::before(function ($user, $ability) {
-            if ($user->is_superadmin) {
-                return true;
-            }
+        if ($user->is_superadmin) {
+            return true;
+        }
 
-            if (!$user->published) {
-                return false;
-            }
-        });
+        if (!$user->published) {
+            return false;
+        }
 
+        return $callback($user);
+    }
+
+    public function boot()
+    {
         /***
          *
          *    Global permissions
@@ -28,35 +31,51 @@ class AuthServiceProvider extends ServiceProvider
          ***/
 
         Gate::define('edit-settings', function ($user) {
-            return $user->role->permissions()->global()->where('name', 'edit-settings')->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->role->permissions()->global()->where('name', 'edit-settings')->exists();
+            });
         });
 
         Gate::define('edit-users', function ($user) {
-            return $user->role->permissions()->global()->where('name', 'edit-users')->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->role->permissions()->global()->where('name', 'edit-users')->exists();
+            });
         });
 
         Gate::define('edit-user-role', function ($user) {
-            return $user->role->permissions()->global()->where('name', 'edit-user-role')->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->role->permissions()->global()->where('name', 'edit-user-role')->exists();
+            });
         });
 
         Gate::define('edit-user-groups', function ($user) {
-            return $user->role->permissions()->global()->where('name', 'edit-user-groups')->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->role->permissions()->global()->where('name', 'edit-user-groups')->exists();
+            });
         });
 
         Gate::define('access-user-management', function ($user) {
-            return $user->can('edit-users') || $user->can('edit-user-role') || $user->can('edit-user-groups');
+            return $this->authorize($user, function ($user) {
+                return $user->can('edit-users') || $user->can('edit-user-role') || $user->can('edit-user-groups');
+            });
         });
 
         Gate::define('manage-modules', function ($user) {
-            return $user->role->permissions()->global()->where('name', 'manage-modules')->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->role->permissions()->global()->where('name', 'manage-modules')->exists();
+            });
         });
 
         Gate::define('access-media-library', function ($user) {
-            return $user->role->permissions()->global()->where('name', 'access-media-library')->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->role->permissions()->global()->where('name', 'access-media-library')->exists();
+            });
         });
 
         Gate::define('impersonate', function ($user) {
-            return $user->is_superadmin;
+            return $this->authorize($user, function ($user) {
+                return $user->is_superadmin;
+            });
         });
 
         /***
@@ -66,27 +85,35 @@ class AuthServiceProvider extends ServiceProvider
          ***/
 
         Gate::define('access-module-list', function ($user, $moduleName) {
-            return $user->can('view-module', $moduleName)
-            || $user->permissions()->ofModuleName($moduleName)->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->can('view-module', $moduleName)
+                || $user->permissions()->ofModuleName($moduleName)->exists();
+            });
         });
 
         // The gate of accessing module list page,
         Gate::define('view-module', function ($user, $moduleName) {
-            return $user->can('edit-module', $moduleName)
-            || $user->role->permissions()->ofModuleName($moduleName)->where('name', 'view-module')->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->can('edit-module', $moduleName)
+                || $user->role->permissions()->ofModuleName($moduleName)->where('name', 'view-module')->exists();
+            });
         });
 
         Gate::define('edit-module', function ($user, $moduleName) {
-            return $user->can('manage-module', $moduleName)
-            || $user->role->permissions()->module()->ofModuleName($moduleName)->where('name', 'manage-module')->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->can('manage-module', $moduleName)
+                || $user->role->permissions()->module()->ofModuleName($moduleName)->where('name', 'manage-module')->exists();
+            });
         });
 
         Gate::define('manage-module', function ($user, $moduleName) {
-            if (!isPermissionableModule($moduleName)) {
-                return true;
-            }
-            return $user->can('manage-modules')
-            || $user->role->permissions()->module()->ofModuleName($moduleName)->where('name', 'manage-module')->exists();
+            return $this->authorize($user, function ($user) {
+                if (!isPermissionableModule($moduleName)) {
+                    return true;
+                }
+                return $user->can('manage-modules')
+                || $user->role->permissions()->module()->ofModuleName($moduleName)->where('name', 'manage-module')->exists();
+            });
         });
 
         /***
@@ -96,18 +123,24 @@ class AuthServiceProvider extends ServiceProvider
          ***/
 
         Gate::define('view-item', function ($user, $item) {
-            return $user->can('edit-item', $item)
-            || $user->permissions()->ofItem($item)->where('name', 'view-item')->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->can('edit-item', $item)
+                || $user->permissions()->ofItem($item)->where('name', 'view-item')->exists();
+            });
         });
 
         Gate::define('edit-item', function ($user, $item) {
-            return $user->can('manage-item', $item)
-            || $user->permissions()->ofItem($item)->where('name', 'edit-item')->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->can('manage-item', $item)
+                || $user->permissions()->ofItem($item)->where('name', 'edit-item')->exists();
+            });
         });
 
         Gate::define('manage-item', function ($user, $item) {
-            return $user->can('manage-module', getModuleNameByModel(get_class($item)))
-            || $user->permissions()->ofItem($item)->where('name', 'manage-item')->exists();
+            return $this->authorize($user, function ($user) {
+                return $user->can('manage-module', getModuleNameByModel(get_class($item)))
+                || $user->permissions()->ofItem($item)->where('name', 'manage-item')->exists();
+            });
         });
     }
 }
