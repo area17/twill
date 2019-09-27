78cdea2ae23b251154d19c9ac1e21c8c4043b595
Salva Camarasa Sola <salvacms@gmail.com>
2019-09-27 11:12:40 +0200

    translated email template to catalan language

--- views/emails/html/email.blade.php
+++ views/emails/html/email.blade.php
@@ -2,17 +2,17 @@
 {{-- Header --}}
 @slot('header')
 @component('twill::emails.html.header', ['url' => config('app.url')])
-# {{ config('app.name') }}&nbsp;<span class="envlabel envlabel--{{ app()->environment() }}">{{ app()->environment() }}</span>
+# {{ config('app.name') }}
 ## {{ $title ?? $actionText }}
 @endcomponent
 @endslot
 
 {{-- Body --}}
-Hello!
+Hola!
 
 {{ $copy }}
 
-Regards,<br>
+Cordialment,<br>
 {{ config('app.name') }}
 
 {{-- Button --}}
@@ -25,14 +25,14 @@ Regards,<br>
 {{-- Subcopy --}}
 @slot('subcopy')
 @component('twill::emails.html.subcopy')
-If you are having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below into your web browser: [{{ $url }}]({{ $url }})
+Si tens cap problema a l'hora d'apretar el botó de "{{ $actionText }}" , copia i enganxa aquesta URL al teu navegador: [{{ $url }}]({{ $url }})
 @endcomponent
 @endslot
 
 {{-- Footer --}}
 @slot('footer')
 @component('twill::emails.html.footer')
-&copy; {{ date('Y') }}  &mdash; CMS powered by AREA 17. All rights reserved.
+&copy; {{ date('Y') }}  &mdash; recomana.cat - CMS powered by AREA 17. All rights reserved.
 @endcomponent
 @endslot
 @endcomponent
