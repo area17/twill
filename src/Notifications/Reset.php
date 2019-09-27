3d67e6d9b2ecae5b1a1b24ce35a2e8aeb3a83309
Salva Camarasa Sola <salvacms@gmail.com>
2019-09-27 11:32:33 +0200

    now mail notificator has translatable fields

--- src/Notifications/Reset.php
+++ src/Notifications/Reset.php
@@ -17,8 +17,10 @@ class Reset extends ResetPassword
     {
         return (new MailMessage)->markdown('twill::emails.html.email', [
             'url' => url(request()->getScheme() . '://' . config('twill.admin_app_url') . route('admin.password.reset.form', $this->token, false)),
-            'actionText' => 'Reset password',
-            'copy' => 'You are receiving this email because we received a password reset. If you did not request a password reset, no further action is required.',
+            'actionText' => Lang::getFromJson('Reset Password Notification'),
+            'copy' => Lang::getFromJson('You are receiving this email because we received a password reset request for your account.') .
+                ' ' .
+                Lang::getFromJson('If you did not request a password reset, no further action is required.')
         ]);
     }
 }
