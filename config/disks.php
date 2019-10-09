0d92373742853d4e9d649fad06ba842428204bc9
Knight Tan <knight@reworklabs.com>
2019-10-09 16:01:09 +0800

    APP_URL by default include http/https scheme

--- config/disks.php
+++ config/disks.php
@@ -1,7 +1,7 @@
 <?php
 
 $localRootPrefix = storage_path('app/public/');
-$localUrlPrefix = request()->getScheme() . '://' . env('APP_URL') . '/storage/';
+$localUrlPrefix = env('APP_URL') . '/storage/';
 
 $mediaLocalConfig = [
     'driver' => 'local',
