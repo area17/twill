70523ae5028a85db125ec6e0cf7a2274cf018404
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-10-11 12:59:38 -0300

    Use Composer to load helpers

--- src/TwillServiceProvider.php
+++ src/TwillServiceProvider.php
@@ -65,8 +65,6 @@ class TwillServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->requireHelpers();
-
         $this->publishConfigs();
         $this->publishMigrations();
         $this->publishAssets();
@@ -328,19 +326,6 @@ class TwillServiceProvider extends ServiceProvider
             Build::class,
             Update::class,
         ]);
-    }
-
-    /**
-     * @return void
-     */
-    private function requireHelpers()
-    {
-        require_once __DIR__ . '/Helpers/routes_helpers.php';
-        require_once __DIR__ . '/Helpers/i18n_helpers.php';
-        require_once __DIR__ . '/Helpers/media_library_helpers.php';
-        require_once __DIR__ . '/Helpers/frontend_helpers.php';
-        require_once __DIR__ . '/Helpers/migrations_helpers.php';
-        require_once __DIR__ . '/Helpers/helpers.php';
     }
 
     /**
