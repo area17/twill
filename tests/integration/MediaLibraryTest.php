32ff9147900a813b415cf73b40c64bfe95f0b5a8
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-10-16 18:40:32 -0300

    Fix assertion

--- tests/integration/MediaLibraryTest.php
+++ tests/integration/MediaLibraryTest.php
@@ -55,7 +55,7 @@ class MediaLibraryTest extends TestCase
 
         $this->assertEquals($fileName, $media->filename);
 
-        $this->files->exists(
+        $this->assertFileExists(
             storage_path("app/public/media-library/{$folder}/{$fileName}")
         );
     }
