e0ff0e1f48164ab2aa8d459c8fb665659339af3b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-10-18 12:23:37 -0300

    Add edit and update user tests

--- tests/integration/UsersTest.php
+++ tests/integration/UsersTest.php
@@ -6,6 +6,26 @@ use A17\Twill\Models\User;
 
 class UsersTest extends TestCase
 {
+    private function createUser($name, $email)
+    {
+        $payload = [
+            'name' => $name,
+            'email' => $email,
+            'role' => 'PUBLISHER',
+            'languages' => [
+                [
+                    'shortlabel' => 'EN',
+                    'label' => 'English',
+                    'value' => 'en',
+                    'disabled' => false,
+                    'published' => true,
+                ],
+            ],
+        ];
+
+        return $this->ajax('/twill/users', 'POST', $payload);
+    }
+
     public function setUp(): void
     {
         parent::setUp();
@@ -24,29 +44,60 @@ class UsersTest extends TestCase
         $this->assertJson($crawler->getContent());
     }
 
-    public function testCanCreateUser()
+    public function testCanUpdateUser()
     {
-        $data = [
-            'name' => ($name = $this->faker->name),
-            'email' => ($email = $this->faker->email),
-            'role' => 'PUBLISHER',
-            'languages' => [
-                [
-                    'shortlabel' => 'EN',
-                    'label' => 'English',
-                    'value' => 'en',
-                    'disabled' => false,
-                    'published' => true,
-                ],
-            ],
+        $this->createUser(
+            $name = $this->faker->name,
+            $email = $this->faker->email
+        );
+
+        $user = User::where('email', $email)->first();
+
+        $payload = [
+            'email' => $email,
+            'name' => ($newName = $this->faker->name),
+            'cmsSaveType' => 'update',
         ];
 
-        $crawler = $this->ajax('/twill/users', 'POST', $data);
+        $crawler = $this->ajax("/twill/users/{$user->id}", 'PUT', $payload);
+
+        $crawler->assertStatus(200);
+
+        $this->assertEquals($name, $user->name);
+
+        $user->refresh();
+
+        $this->assertEquals($newName, $user->name);
+    }
+
+    public function testCanCreateUser()
+    {
+        $crawler = $this->createUser(
+            $name = $this->faker->name,
+            $email = $this->faker->email
+        );
 
         $crawler->assertStatus(200);
 
         $user = User::where('email', $email)->first();
 
         $this->assertEquals($name, $user->name);
+    }
+
+    public function testCanEditUser()
+    {
+        $user = User::where(
+            'email',
+            $email = $this->getSuperAdmin()->email
+        )->first();
+
+        $user->google_2fa_enabled = true;
+        $user->save();
+
+        $crawler = $this->request("/twill/users/{$user->id}/edit");
+
+        $crawler->assertStatus(200);
+
+        $this->assertStringContainsString($email, $crawler->getContent());
     }
 }
