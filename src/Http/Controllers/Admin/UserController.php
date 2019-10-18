60cdc7f4dcae93a7913deca7b499d8a87539ba4a
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-10-18 12:22:59 -0300

    Fix double semicolon

--- src/Http/Controllers/Admin/UserController.php
+++ src/Http/Controllers/Admin/UserController.php
@@ -154,7 +154,7 @@ class UserController extends ModuleController
     protected function formData($request)
     {
         $user = $this->authFactory->guard('twill_users')->user();
-        $with2faSettings = $this->config->get('twill.enabled.users-2fa') && $user->id == $request->route('user');;
+        $with2faSettings = $this->config->get('twill.enabled.users-2fa') && $user->id == $request->route('user');
 
         if ($with2faSettings) {
             $user->generate2faSecretKey();
