c4b3bd7c9b72d6270568a61bc6751846bae110db
Fernando Petrelli <fer.petrelli@gmail.com>
2019-10-23 17:53:58 -0400

    Present Oauth routes only if it has been enabled

--- routes/auth.php
+++ routes/auth.php
@@ -22,5 +22,7 @@ if (config('twill.enabled.users-2fa')) {
     Route::post('login-2fa', 'LoginController@login2Fa')->name('login-2fa');
 }
 
-Route::get('login/oauth/redirect/{provider}', 'LoginController@redirectToProvider')->name('login.redirect');
-Route::get('login/oauth/callback/{provider}', 'LoginController@handleProviderCallback')->name('login.callback');
+if (config('twill.enabled.oauth-login')) {
+    Route::get('login/oauth/redirect/{provider}', 'LoginController@redirectToProvider')->name('login.redirect');
+    Route::get('login/oauth/callback/{provider}', 'LoginController@handleProviderCallback')->name('login.callback');
+}
