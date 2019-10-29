f6d2e72034857f6744266219bbd806efa27f0a3c
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-10-29 19:41:09 -0300

    Skip tests not running on Travis

--- tests/integration/DashboardTest.php
+++ tests/integration/DashboardTest.php
@@ -10,54 +10,69 @@ class DashboardTest extends ModulesTestBase
 
         // TODO -- disabled to fix error in Travis
         //
-        // $this->loadConfig('{$stubs}/modules/dashboard/twill.php'); /// commenting this line it start working
+
+        $this->skipOnTravis();
+
+        $this->loadConfig('{$stubs}/modules/dashboard/twill.php'); /// commenting this line it start working
     }
 
-    /**
-     * TODO -- moved to ModulesAuthorsTest
-     */
-    //    public function testCanDisplayDashboard()
-    //    {
-    //        $this->request('/twill')->assertStatus(200);
-    //
-    //        $this->assertSee('Personnel');
-    //        $this->assertSee('Categories');
-    //
-    //        $this->request('/twill/personnel/authors')->assertStatus(200);
-    //
-    //        $this->assertSee('Name');
-    //        $this->assertSee('Languages');
-    //        $this->assertSee('Mine');
-    //        $this->assertSee('Add new');
-    //
-    //        $this->request('/twill/categories')->assertStatus(200);
-    //    }
+    private function skipOnTravis()
+    {
+        if (!is_null(env('TRAVIS_PHP_VERSION'))) {
+            $this->markTestSkipped('This test cannot be execute on Travis');
+        }
+    }
 
     /**
      * This test produce an unknown failure which happens only on Travis.
      *
      * Please check: https://travis-ci.org/antonioribeiro/twill/jobs/604677960
      *
-     * TODO
+     * moved to ModulesAuthorsTest
      */
+    public function testCanDisplayDashboard()
+    {
+        $this->request('/twill')->assertStatus(200);
 
-    //    public function testCanSearchString()
-    //    {
-    //        $this->createAuthor(3);
-    //
-    //        $this->ajax("/twill/search?search={$this->name_en}")->assertStatus(200);
-    //
-    //        $this->assertJson($this->content());
-    //
-    //        $result = json_decode($this->content(), true);
-    //
-    //        $this->assertGreaterThan(0, count($result));
-    //
-    //        $this->assertEquals(
-    //            $this->now->format('Y-m-d\TH:i:s+00:00'),
-    //            $result[0]['date']
-    //        );
-    //    }
+        $this->assertSee('Personnel');
+        $this->assertSee('Categories');
+
+        $this->request('/twill/personnel/authors')->assertStatus(200);
+
+        $this->assertSee('Name');
+        $this->assertSee('Languages');
+        $this->assertSee('Mine');
+        $this->assertSee('Add new');
+
+        $this->request('/twill/categories')->assertStatus(200);
+    }
+
+    /**
+     * This test produce an unknown failure which happens only on Travis.
+     *
+     * Please check: https://travis-ci.org/antonioribeiro/twill/jobs/604677960
+     */
+    public function testCanSearchString()
+    {
+        $this->createAuthor(3);
+
+        $this->ajax("/twill/search?search={$this->name_en}")->assertStatus(200);
+
+        $this->assertJson($this->content());
+
+        $result = json_decode($this->content(), true);
+
+        $this->assertGreaterThan(0, count($result));
+
+        $this->assertEquals(
+            $this->now->format('Y-m-d\TH:i:s+00:00'),
+            $result[0]['date']
+        );
+    }
+
+    /**
+     * Trying to debug those tests
+     */
 
     //    public function testCanDisplayDashboard()
     //    {
