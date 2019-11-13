caccfc5534c98cf246ce8ec89080888b824bafc8
Antonin Caudron <antonin@area17.com>
2019-11-13 10:42:24 +0100

    Show twill version from value retrieve into twill object

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -23,5 +23,5 @@ window[process.env.VUE_APP_NAME].vheader = new Vue({ el: '#headerUser' })
 /* eslint-disable no-new */
 /* eslint no-unused-vars: "off" */
 window[process.env.VUE_APP_NAME].vsearch = search
-console.log('\x1b[32m', `Made with ${process.env.VUE_APP_NAME} - v${process.env.VUE_APP_VERSION}`)
+console.log('\x1b[32m', `Made with ${process.env.VUE_APP_NAME} - v${window[process.env.VUE_APP_NAME].version}`)
 export default A17Init
