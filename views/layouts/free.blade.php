ef4e748d35435abacd793b232b864179460a18c3
Quentin Renard <quentin@area17.com>
2019-11-18 02:46:43 -0500

    Add support for Twill form fields and buttons in free layout

--- views/layouts/free.blade.php
+++ views/layouts/free.blade.php
@@ -8,6 +8,17 @@
       @yield('contentHTML')
     </div>
   </div>
+  <a17-modal class="modal--browser" ref="browser" mode="medium" :force-close="true">
+      <a17-browser></a17-browser>
+  </a17-modal>
+@stop
+
+@section('initialStore')
+    window.STORE.medias.crops = {!! json_encode(config('twill.settings.crops') ?? []) !!}
+    window.STORE.medias.selected = {}
+
+    window.STORE.browser = {}
+    window.STORE.browser.selected = {}
 @stop
 
 @push('extra_js')
