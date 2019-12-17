94af00ce8560af2790a4cb31880349625e0b591e
Antoine Doury <antoine@area17.com>
2019-12-17 11:56:58 +0100

    Field Rows : Add 2 or 3 columns variants

--- views/partials/form/utils/_field_rows.blade.php
+++ views/partials/form/utils/_field_rows.blade.php
@@ -1,4 +1,4 @@
-<div class="field-rows">
+<div class="field-rows @if(isset($columns) && $columns > 1) field-rows--{{ $columns }}cols @endif">
     @if(isset($title))
         <h3 class="field-rows__title">{{ $title }}</h3>
     @endif
