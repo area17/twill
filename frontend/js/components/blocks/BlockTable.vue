150eb3b1eae2160b914d8f1d783cc504c0c06dce
Antoine Doury <antoine@area17.com>
2019-12-20 15:38:29 +0100

    Add default table component in the codebase

---
+++ frontend/js/components/blocks/BlockTable.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="block__body">
+    <a17-wysiwyg-tiptap :name="fieldName('html')" label="Body" :options='{"modules":{"toolbar":["table"],"syntax":false}}' placeholder='Insert your table' editSource="true" inStore="value"></a17-wysiwyg-tiptap>
+  </div>
+</template>
+
+<script>
+  import BlockMixin from '@/mixins/block'
+
+  export default {
+    name: 'A17Bodytable',
+    mixins: [BlockMixin]
+  }
+</script>
