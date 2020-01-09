32e82e0ef5285fd96dfc02c2a716b84adedd7ea7
Andrei Ioniță <hi@andrei.io>
2020-01-09 18:19:06 +0000

    Refactor slugify filter to match HasSlug behavior

--- frontend/js/utils/filters.js
+++ frontend/js/utils/filters.js
@@ -2,21 +2,15 @@ import dateFormat from 'date-fns/format'
 
 const filters = {
   slugify: function (value) {
-    const a = 'àáäâèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;' + 'ąàáäâãåæćęęèéëêìíïîłńòóöôõøśùúüûñçżź'
-    const b = 'aaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------' + 'aaaaaaaaceeeeeeiiiilnoooooosuuuunczz'
-    const ea = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖŐØÙÚÛÜŰÝÞßàáâãäåæçèéêëìíîïðñòóôõöőøùúûüűýþÿ©ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩΆΈΊΌΎΉΏΪΫαβγδεζηθικλμνξοπρστυφχψωάέίόύήώςϊΰϋΐŞİĞşığАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяЄІЇҐєіїґČĎĚŇŘŠŤŮŽčďěňřšťůžĄĆĘŁŃŚŹŻąćęłńśźżĀĒĢĪĶĻŅŪāēģīķļņū'
-    const eb = ['A', 'A', 'A', 'A', 'A', 'A', 'AE', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'D', 'N', 'O', 'o', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'U', 'Y', 'TH', 'ss', 'a', 'a', 'a', 'a', 'a', 'a', 'ae', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'd', 'n', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'u', 'y', 'th', 'y', '(c)', 'A', 'B', 'G', 'D', 'E', 'Z', 'H', '8', 'I', 'K', 'L', 'M', 'N', '3', 'O', 'P', 'R', 'S', 'T', 'Y', 'F', 'X', 'PS', 'W', 'A', 'E', 'I', 'O', 'Y', 'H', 'W', 'I', 'Y', 'a', 'b', 'g', 'd', 'e', 'z', 'h', '8', 'i', 'k', 'l', 'm', 'n', '3', 'o', 'p', 'r', 's', 't', 'y', 'f', 'x', 'ps', 'w', 'a', 'e', 'i', 'o', 'y', 'h', 'w', 's', 'i', 'y', 'y', 'i', 'S', 'I', 'G', 's', 'i', 'g', 'A', 'B', 'V', 'G', 'D', 'E', 'Yo', 'Zh', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sh', '', 'Y', '', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'yo', 'zh', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sh', '', 'y', '', 'e', 'yu', 'ya', 'Ye', 'I', 'Yi', 'G', 'ye', 'i', 'yi', 'g', 'C', 'D', 'E', 'N', 'R', 'S', 'T', 'U', 'Z', 'c', 'd', 'e', 'n', 'r', 's', 't', 'u', 'z', 'A', 'C', 'e', 'L', 'N', 'S', 'Z', 'Z', 'a', 'c', 'e', 'l', 'n', 's', 'z', 'z', 'A', 'E', 'G', 'i', 'k', 'L', 'N', 'u', 'a', 'e', 'g', 'i', 'k', 'l', 'n', 'u']
-    const p = new RegExp(a.split('').join('|'), 'g')
-    const ep = new RegExp(ea.split('').join('|'), 'g')
+    const charMap = { ',': '-', '/': '-', ':': '-', ';': '-', '_': '-', '©': '(c)', '·': '-', 'ß': 'ss', 'à': 'a', 'á': 'a', 'â': 'a', 'ã': 'a', 'ä': 'a', 'å': 'a', 'æ': 'ae', 'ç': 'c', 'è': 'e', 'é': 'e', 'ê': 'e', 'ë': 'e', 'ì': 'i', 'í': 'i', 'î': 'i', 'ï': 'i', 'ð': 'd', 'ñ': 'n', 'ò': 'o', 'ó': 'o', 'ô': 'o', 'õ': 'o', 'ö': 'o', 'ø': 'o', 'ù': 'u', 'ú': 'u', 'û': 'u', 'ü': 'u', 'ý': 'y', 'þ': 'th', 'ÿ': 'y', 'ā': 'a', 'ă': 'a', 'ą': 'a', 'ć': 'c', 'č': 'c', 'ď': 'd', 'ē': 'e', 'ę': 'e', 'ě': 'e', 'ğ': 'g', 'ģ': 'g', 'ī': 'i', 'ı': 'i', 'ķ': 'k', 'ļ': 'l', 'ł': 'l', 'ń': 'n', 'ņ': 'n', 'ň': 'n', 'ő': 'o', 'œ': 'oe', 'ŕ': 'r', 'ř': 'r', 'ś': 's', 'ş': 's', 'š': 's', 'ť': 't', 'ū': 'u', 'ů': 'u', 'ű': 'u', 'ź': 'z', 'ż': 'z', 'ž': 'z', 'ǘ': 'u', 'ǵ': 'g', 'ǹ': 'n', 'ș': 's', 'ț': 't', 'ΐ': 'i', 'ά': 'a', 'έ': 'e', 'ή': 'h', 'ί': 'i', 'ΰ': 'y', 'α': 'a', 'β': 'b', 'γ': 'g', 'δ': 'd', 'ε': 'e', 'ζ': 'z', 'η': 'h', 'θ': '8', 'ι': 'i', 'κ': 'k', 'λ': 'l', 'μ': 'm', 'ν': 'n', 'ξ': '3', 'ο': 'o', 'π': 'p', 'ρ': 'r', 'ς': 's', 'σ': 's', 'τ': 't', 'υ': 'y', 'φ': 'f', 'χ': 'x', 'ψ': 'ps', 'ω': 'w', 'ϊ': 'i', 'ϋ': 'y', 'ό': 'o', 'ύ': 'y', 'ώ': 'w', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh', 'щ': 'sh', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya', 'ё': 'yo', 'є': 'ye', 'і': 'i', 'ї': 'yi', 'ґ': 'g', 'ḧ': 'h', 'ḿ': 'm', 'ṕ': 'p', 'ẃ': 'w', 'ẍ': 'x' }
+    const p = new RegExp(Object.keys(charMap).join('|'), 'g')
     return value.toString().toLowerCase().trim()
       .replace(/\s+/g, '-') // Replace spaces with -
-      .replace(p, c =>
-        b.charAt(a.indexOf(c))) // Replace special chars
-      .replace(ep, c =>
-        eb[ea.indexOf(c)]) // Replace special chars extended
+      .replace(p, c => charMap[c]) // Replace special chars
       .replace(/&/g, '-and-') // Replace & with 'and'
-      .replace(/[^\w-]+/g, '') // Remove all non-word chars
+      .replace(/[^\w-]+/g, '-') // Replace all non-word chars with -
       .replace(/--+/g, '-') // Replace multiple - with single -
+      .replace(/(^-+)|(-+$)/, '') // Remove leading and traling -
   },
   prettierUrl: function (value) {
     return value.replace(/^\/\/|^.*?:(\/\/)?/, '')
