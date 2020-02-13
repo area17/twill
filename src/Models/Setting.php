58c1437a73d2f9b5e639251fd69f315c4b68abbb
Quentin Renard <quentin@area17.com>
2020-02-13 03:17:26 -0500

    Fix settings translation foreign key when using custom table name

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -4,6 +4,7 @@ namespace A17\Twill\Models;
 
 use A17\Twill\Models\Behaviors\HasMedias;
 use A17\Twill\Models\Behaviors\HasTranslation;
+use Illuminate\Support\Str;
 
 class Setting extends Model
 {
@@ -30,5 +31,10 @@ class Setting extends Model
     public function getTable()
     {
         return config('twill.settings_table', 'twill_settings');
+    }
+
+    protected function getTranslationRelationKey(): string
+    {
+        return Str::singular(config('twill.settings_table', 'twill_settings')) . '_id';
     }
 }
