74173acd0b750243c2d3bab382ed842f36efdce9
Quentin Renard <quentin@area17.com>
2020-02-19 01:21:00 -0500

    Rename default migrations to prevent conflicts in existing codebases

--- migrations/default/2020_02_09_000011_add_locale_column_to_twill_default-mediables.php
+++ migrations/default/2020_02_09_000011_add_locale_column_to_twill_default-mediables.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class AddLocaleColumnToTwillMediables extends Migration
+class AddLocaleColumnToTwillDefaultMediables extends Migration
 {
     public function up()
     {
--- migrations/optional/users-oauth/2020_02_09_000014_create_twill_default_users_oauth_table.php
+++ migrations/optional/users-oauth/2020_02_09_000014_create_twill_default_users_oauth_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTwillUsersOauthTable extends Migration
+class CreateTwillDefaultUsersOauthTable extends Migration
 {
     /**
      * Run the migrations.
