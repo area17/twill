2cc4fdb9fd57c7a2947d16d3207b4a8cea52eb29
Bekzat Samatov <bekzatsmov@gmail.com>
2020-02-22 23:05:12 +0600

    Russian locale

---
+++ views/lang/ru/lang.php
@@ -0,0 +1,137 @@
+<?php
+
+return [
+    'auth' => [
+        'email' => 'Email',
+        'password' => 'Пароль',
+        'forget-password' => 'Восстановить пароль',
+        'login' => 'Войти',
+        'login-title' => 'Войти'
+    ],
+    'dashboard' => [
+        'search-placeholder' => 'Поиск всего...',
+        'empty-message' => 'У вас еще нет активности.',
+        'all-activity' => 'Все активность',
+        'my-activity' => 'Моя активность'
+    ],
+    'footer' => [
+        'version' => 'Версия'
+    ],
+    'form' => [
+        'content' => 'Содержимое'
+    ],
+    'listing' => [
+        'filter' => [
+            'all-items' => 'Все',
+            'mine' => 'Мои',
+            'published' => 'Опубликованные',
+            'draft' => 'Черновики',
+            'trash' => 'Удаленные',
+        ],
+        'add-new-button' => 'Добавить',
+        'listing-empty-message' => 'Ничего нет.',
+        'languages' => 'Языки'
+    ],
+    'main' => [
+        'published' => 'Опубликован',
+        'draft' => 'Черновик',
+        'create' => 'Создать',
+        'update' => 'Обновить',
+        'title' => 'Заголовок'
+    ],
+    'modal' => [
+        'title-field' => 'Заголовок',
+        'permalink-field' => 'Постоянная ссылка',
+        'create' => [
+            'title' => 'Добавить новый',
+            'button' => 'Создать',
+            'create-another' => 'Создать и добавить другую',
+        ]
+    ],
+    'nav' => [
+        'media-library' => 'Медиа библиотека',
+        'cms-users' => 'Пользователи CMS',
+        'settings' => 'Настройки',
+        'logout' => 'Выйти'
+    ],
+    'media-library' => [
+        'title' => 'Медиа библиотека',
+        'images' => 'Изображения',
+        'files' => 'Файлы',
+        'filter-select-label' => 'Фильтровать по тегу',
+        'sidebar' => [
+            'empty-text' => 'Файлы не выделены',
+            'files-selected' => 'файлов выделено',
+            'clear' => 'Снять выделение',
+            'dimensions' => 'Уровни'
+        ]
+    ],
+    'filter' => [
+        'search-placeholder'=> 'Поиск',
+        'toggle-label' => 'Фильтр',
+        'apply-btn' => 'Применить',
+        'clear-btn' => 'Очистить'
+    ],
+    'select' => [
+        'empty-text' => 'Нет соответствующих параметров.'
+    ],
+    'uploader' => [
+        'dropzone-text' => 'или притащите файлы сюда',
+        'upload-btn-label' => 'Загрузить'
+    ],
+    'fields' => [
+        'medias' => [
+            'btn-label' => 'Прикрепить изображение'
+        ],
+        'block-editor' => [
+            'collapse-all' => 'Раскрыть все',
+            'expend-all' => 'Скрыть',
+            'open-in-editor' => 'Открыть в редакторе',
+            'create-another' => 'Создать другой',
+            'delete' => 'Удалить'
+        ],
+        'browser' => [
+            'attach' => 'Прикрепить',
+            'add-label' => 'Добавить'
+        ],
+        'files' => [
+            'add-label' => 'Добавить'
+        ]
+    ],
+    'user-management' => [
+        'users' => 'Пользователи',
+        'active' => 'Активный',
+        'disabled' => 'Отключенный',
+        'enabled' => 'Включенный',
+        'trash' => 'Корзина',
+        'user-image' => 'Изображение',
+        'name' => 'Имя',
+        'email' => 'Email',
+        'role' => 'Роль',
+        'content-fieldset-label' => 'Настройки пользователя',
+        'edit-modal-title' => 'Изменить имя',
+        'update-disabled-user' => 'Обновить отключенного пользователя',
+        'update-disabled-and-close' => 'Обновить отключенного пользователя и закрыть',
+        'update-disabled-user-and-create-new' => 'Обновить отключенного пользователя и создать еще',
+        'enable-user' => 'Включить пользователя',
+        'enable-user-and-close' => 'Включить пользователя и закрыть',
+        'enable-user-and-create-new' => 'Включить пользователя и создать еще',
+        'update' => 'Обновить',
+        'update-and-close' => 'Обновить и закрыть',
+        'update-and-create-new' => 'Обновить и создать еще',
+        'cancel' => 'Отмена'
+    ],
+    'publisher' => [
+        'switcher-title' => 'Статус',
+        'save' => 'Сохранить как черновик',
+        'save-close' => 'Сохранить как черновик и закрыть',
+        'save-new' => 'Сохранить как черновик и создать еще',
+        'publish' => 'Опубликовать',
+        'publish-close' => 'Опубликовать и закрыть',
+        'publish-new' => 'Опубликовать и создать еще',
+        'update' => 'Обновить',
+        'update-close' => 'Обновить и закрыть',
+        'update-new' => 'Обновить и создать еще',
+        'cancel' => 'Отмена'
+    ]
+];
