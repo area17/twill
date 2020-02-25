cb67c7b9dce462a74adf7ceef5189c723b0acbe9
Philipp Daun <post@philippdaun.net>
2020-02-25 22:59:13 +0100

    Extract creation of filter options array

--- views/layouts/listing.blade.php
+++ views/layouts/listing.blade.php
@@ -24,14 +24,19 @@
                         @endif
 
                         @if (isset(${$filter.'List'}))
+                            @php
+                                $list = ${$filter.'List'};
+                                $options = method_exists($list, 'map') ?
+                                    $list->map(function($label, $value) {
+                                        return [
+                                            'value' => $value,
+                                            'label' => $label,
+                                        ];
+                                    })->values()->toArray() : $list;
+                            @endphp
                             <a17-vselect
                             name="{{ $filter }}"
-                            :options="{{ json_encode(method_exists(${$filter.'List'}, 'map') ? ${$filter.'List'}->map(function($label, $value) {
-                                return [
-                                    'value' => $value,
-                                    'label' => $label
-                                ];
-                            })->values()->toArray() : ${$filter.'List'}) }}"
+                            :options="{{ json_encode($options) }}"
                             placeholder="All {{ strtolower(\Illuminate\Support\Str::plural($filter)) }}"
                             ref="filterDropdown[{{ $loop->index }}]"
                             ></a17-vselect>
