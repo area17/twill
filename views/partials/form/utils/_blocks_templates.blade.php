0aa4ea68205df8002c5fca75c605b1058e9d22f6
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2020-03-09 12:37:33 +0100

    Fix missing block list

--- views/partials/form/utils/_blocks_templates.blade.php
+++ views/partials/form/utils/_blocks_templates.blade.php
@@ -1,16 +1,14 @@
 @php
-    $names = app(\A17\Twill\Services\Blocks\BlockCollection::class)
-                ->collect()
-                ->pluck('name')
-                ->values()
-                ->toJson();
+    $blocks = app(\A17\Twill\Services\Blocks\BlockCollection::class)->collect();
+
+    $names = $blocks->pluck('name')->values()->toJson();
 @endphp
 
 <script>
     window['{{ config('twill.js_namespace') }}'].TWILL_BLOCKS_COMPONENTS = {!! $names !!}
 </script>
 
-@foreach($newBlocks as $block)
+@foreach($blocks as $block)
     <script type="text/x-template" id="{{ $block->component }}">
         <div class="block__body">
             {!! $block->render() !!}
