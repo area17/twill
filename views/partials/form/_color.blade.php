e848d1e47772c70e63a6a9dd583be481fc67c00c
Joyce Verheije <joyce.verheije@gmail.com>
2020-03-10 23:11:20 -0400

    Support color form field in settings

--- views/partials/form/_color.blade.php
+++ views/partials/form/_color.blade.php
@@ -4,11 +4,11 @@
     in-store="value"
 ></a17-colorfield>
 
-@unless($renderForBlocks || $renderForModal || !isset($item->$name))
+@unless($renderForBlocks || $renderForModal || (!isset($item->$name) && null == $formFieldsValue = getFormFieldsValue($form_fields, $name)))
 @push('vuexStore')
     window['{{ config('twill.js_namespace') }}'].STORE.form.fields.push({
         name: '{{ $name }}',
-        value: {!! json_encode($item->$name) !!}
+        value: {!! json_encode($item->$name ?? $formFieldsValue) !!}
     })
 @endpush
 @endunless
