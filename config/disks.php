area17/twill

commit 876c93a22b660c14a52577019374a5cb3d569c77

Fix local libraries regressions
- New endpoint root option should only after S3 driver
- Add support for prefix_uuid_with_local_path in media library as well following support in file library (for backward compatibility with the way we've been using the local driver)
- Default local path to "uploads" instead of "libraries"

config/disks.php
@@ -1,7 +1,7 @@
 <?php
 
 $localRootPrefix = storage_path('app/public/');
-$localUrlPrefix = env('APP_URL') . '/storage/';
+$localUrlPrefix = request()->getScheme() . '://' . env('APP_URL') . '/storage/';
 
 $mediaLocalConfig = [
     'driver' => 'local',

config/file-library.php
@@ -21,9 +21,10 @@ return [
     'disk' => 'file_library',
     'endpoint_type' => env('FILE_LIBRARY_ENDPOINT_TYPE', 's3'),
     'cascade_delete' => env('FILE_LIBRARY_CASCADE_DELETE', false),
-    'local_path' => env('FILE_LIBRARY_LOCAL_PATH', 'libraries'),
+    'local_path' => env('FILE_LIBRARY_LOCAL_PATH', 'uploads'),
     'file_service' => env('FILE_LIBRARY_FILE_SERVICE', 'A17\Twill\Services\FileLibrary\Disk'),
     'acl' => env('FILE_LIBRARY_ACL', 'public-read'),
     'filesize_limit' => env('FILE_LIBRARY_FILESIZE_LIMIT', 50),
     'allowed_extensions' => [],
+    'prefix_uuid_with_local_path' => false,
 ];

config/media-library.php
@@ -22,10 +22,11 @@ return [
     'disk' => 'media_library',
     'endpoint_type' => env('MEDIA_LIBRARY_ENDPOINT_TYPE', 's3'),
     'cascade_delete' => env('MEDIA_LIBRARY_CASCADE_DELETE', false),
-    'local_path' => env('MEDIA_LIBRARY_LOCAL_PATH', 'libraries'),
+    'local_path' => env('MEDIA_LIBRARY_LOCAL_PATH', 'uploads'),
     'image_service' => env('MEDIA_LIBRARY_IMAGE_SERVICE', 'A17\Twill\Services\MediaLibrary\Imgix'),
     'acl' => env('MEDIA_LIBRARY_ACL', 'private'),
     'filesize_limit' => env('MEDIA_LIBRARY_FILESIZE_LIMIT', 50),
     'allowed_extensions' => ['svg', 'jpg', 'gif', 'png', 'jpeg'],
     'init_alt_text_from_filename' => true,
+    'prefix_uuid_with_local_path' => config('twill.file_library.prefix_uuid_with_local_path', false),
 ];

src/Http/Controllers/Admin/FileLibraryController.php
@@ -154,18 +154,22 @@ class FileLibraryController extends ModuleController implements SignS3UploadList
     public function storeFile($request)
     {
         $filename = $request->input('qqfilename');
+
         $cleanFilename = preg_replace("/\s+/i", "-", $filename);
 
         $fileDirectory = $request->input('unique_folder_name');
-        $disk = $this->config->get('twill.file_library.disk');
-
-        $request->file('qqfile')->storeAs($fileDirectory, $cleanFilename, $disk);
 
         $uuid = $request->input('unique_folder_name') . '/' . $cleanFilename;
 
         if ($this->config->get('twill.file_library.prefix_uuid_with_local_path', false)) {
-            $uuid = $this->config->get('twill.file_library.local_path') . $uuid;
+            $prefix = trim($this->config->get('twill.file_library.local_path'), '/ ') . '/';
+            $fileDirectory = $prefix . $fileDirectory;
+            $uuid = $prefix . $uuid;
         }
+
+        $disk = $this->config->get('twill.file_library.disk');
+
+        $request->file('qqfile')->storeAs($fileDirectory, $cleanFilename, $disk);
 
         $fields = [
             'uuid' => $uuid,

src/Http/Controllers/Admin/MediaLibraryController.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\ResponseFactory;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
 
 class MediaLibraryController extends ModuleController implements SignS3UploadListener
 {
@@ -147,6 +148,14 @@ class MediaLibraryController extends ModuleController implements SignS3UploadLis
 
         $fileDirectory = $request->input('unique_folder_name');
 
+        $uuid = $request->input('unique_folder_name') . '/' . $filename;
+
+        if ($this->config->get('twill.media_library.prefix_uuid_with_local_path', false)) {
+            $prefix = trim($this->config->get('twill.media_library.local_path'), '/ ') . '/';
+            $fileDirectory = $prefix . $fileDirectory;
+            $uuid = $prefix . $uuid;
+        }
+
         $disk = $this->config->get('twill.media_library.disk');
 
         $request->file('qqfile')->storeAs($fileDirectory, $filename, $disk);
@@ -156,7 +165,7 @@ class MediaLibraryController extends ModuleController implements SignS3UploadLis
         list($w, $h) = getimagesize($filePath);
 
         $fields = [
-            'uuid' => $request->input('unique_folder_name') . '/' . $filename,
+            'uuid' => $uuid,
             'filename' => $originalFilename,
             'width' => $w,
             'height' => $h,

src/Http/ViewComposers/FilesUploaderConfig.php
@@ -55,7 +55,7 @@ class FilesUploaderConfig
             'signatureEndpoint' => $this->urlGenerator->route('admin.file-library.sign-s3-upload'),
             'endpointBucket' => $this->config->get('filesystems.disks.' . $libraryDisk . '.bucket', 'none'),
             'endpointRegion' => $this->config->get('filesystems.disks.' . $libraryDisk . '.region', 'none'),
-            'endpointRoot' => $this->config->get('filesystems.disks.' . $libraryDisk . '.root', ''),
+            'endpointRoot' => $endpointType === 'local' ? '' : $this->config->get('filesystems.disks.' . $libraryDisk . '.root', ''),
             'accessKey' => $this->config->get('filesystems.disks.' . $libraryDisk . '.key', 'none'),
             'csrfToken' => $this->sessionStore->token(),
             'acl' => $this->config->get('twill.file_library.acl'),

src/Http/ViewComposers/MediasUploaderConfig.php
@@ -55,7 +55,7 @@ class MediasUploaderConfig
             'signatureEndpoint' => $this->urlGenerator->route('admin.media-library.sign-s3-upload'),
             'endpointBucket' => $this->config->get('filesystems.disks.' . $libraryDisk . '.bucket', 'none'),
             'endpointRegion' => $this->config->get('filesystems.disks.' . $libraryDisk . '.region', 'none'),
-            'endpointRoot' => $this->config->get('filesystems.disks.' . $libraryDisk . '.root', ''),
+            'endpointRoot' => $endpointType === 'local' ? '' : $this->config->get('filesystems.disks.' . $libraryDisk . '.root', ''),
             'accessKey' => $this->config->get('filesystems.disks.' . $libraryDisk . '.key', 'none'),
             'csrfToken' => $this->sessionStore->token(),
             'acl' => $this->config->get('twill.media_library.acl'),

src/Services/MediaLibrary/Local.php
@@ -4,7 +4,6 @@ namespace A17\Twill\Services\MediaLibrary;
 
 use Illuminate\Support\Facades\Storage;
 
-
 class Local implements ImageServiceInterface
 {
     use ImageServiceDefaults;

commit 15a2dbea5bbbfb3e77e23e821233d3b3e69d28bc

Fix local disk and Glide base url request scheme resolution
https://github.com/area17/twill/commit/876c93a22b660c14a52577019374a5cb3d569c77 introduced an issue by using request() in config. When using php artisan config:cache that request is not coming externally with the appropriate headers.

config/disks.php
@@ -1,20 +1,17 @@
 <?php
 
 $localRootPrefix = storage_path('app/public/');
-$localUrlPrefix = request()->getScheme() . '://' . str_replace(['http://', 'https://'], '', env('APP_URL')) . '/storage/';
 
 $mediaLocalConfig = [
     'driver' => 'local',
     'visibility' => 'public',
     'root' => $localRootPrefix . trim(config('twill.media_library.local_path'), '/ '),
-    'url' => $localUrlPrefix . trim(config('twill.media_library.local_path'), '/ '),
 ];
 
 $fileLocalConfig = [
     'driver' => 'local',
     'visibility' => 'public',
     'root' => $localRootPrefix . trim(config('twill.file_library.local_path'), '/ '),
-    'url' => $localUrlPrefix . trim(config('twill.file_library.local_path'), '/ '),
 ];
 
 $s3Config = [

config/glide.php
@@ -14,7 +14,7 @@ return [
     'source' => env('GLIDE_SOURCE', storage_path('app/public/' . config('twill.media_library.local_path'))),
     'cache' => env('GLIDE_CACHE', storage_path('app')),
     'cache_path_prefix' => env('GLIDE_CACHE_PATH_PREFIX', 'glide_cache'),
-    'base_url' => env('GLIDE_BASE_URL', request()->getScheme() . '://' . str_replace(['http://', 'https://'], '', config('app.url'))),
+    'base_url' => env('GLIDE_BASE_URL', null),
     'base_path' => env('GLIDE_BASE_PATH', 'img'),
     'use_signed_urls' => env('GLIDE_USE_SIGNED_URLS', false),
     'sign_key' => env('GLIDE_SIGN_KEY'),

src/Services/MediaLibrary/Glide.php
@@ -52,8 +52,17 @@ class Glide implements ImageServiceInterface
         $this->app = $app;
         $this->request = $request;
 
+        $baseUrlHost = $this->config->get(
+            'twill.glide.base_url',
+            $this->request->getScheme() . '://' . str_replace(
+                ['http://', 'https://'],
+                '',
+                $this->config->get('app.url')
+            )
+        );
+
         $baseUrl = join('/', [
-            rtrim($this->config->get('twill.glide.base_url'), '/'),
+            rtrim($baseUrlHost, '/'),
             ltrim($this->config->get('twill.glide.base_path'), '/'),
         ]);
 

src/TwillServiceProvider.php
@@ -211,7 +211,29 @@ class TwillServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/../config/dashboard.php', 'twill.dashboard');
         $this->mergeConfigFrom(__DIR__ . '/../config/oauth.php', 'twill.oauth');
         $this->mergeConfigFrom(__DIR__ . '/../config/disks.php', 'filesystems.disks');
+
+        if (config('twill.media_library.endpoint_type') === 'local'
+            && config('twill.media_library.disk') === 'twill_media_library') {
+            $this->setLocalDiskUrl('media');
+        }
+
+        if (config('twill.file_library.endpoint_type') === 'local'
+            && config('twill.file_library.disk') === 'twill_file_library') {
+            $this->setLocalDiskUrl('file');
+        }
+
         $this->mergeConfigFrom(__DIR__ . '/../config/services.php', 'services');
+    }
+
+    private function setLocalDiskUrl($type)
+    {
+        config([
+            'filesystems.disks.twill_' . $type . '_library.url' => request()->getScheme()
+            . '://'
+            . str_replace(['http://', 'https://'], '', config('app.url'))
+            . '/storage/'
+            . trim(config('twill.' . $type . '_library.local_path'), '/ '),
+        ]);
     }
 
     private function publishMigrations()