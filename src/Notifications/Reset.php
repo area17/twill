f742cc7ab2004e7661ec607c1df5a69b6e69094c
Andrei Ioniță <hi@andrei.io>
2020-06-12 12:48:55 +0100

    Fix notifications url

--- src/Notifications/Reset.php
+++ src/Notifications/Reset.php
@@ -16,7 +16,7 @@ class Reset extends ResetPassword
     public function toMail($notifiable)
     {
         return (new MailMessage)->markdown('twill::emails.html.email', [
-            'url' => url(request()->getScheme() . '://' . config('twill.admin_app_url') . route('admin.password.reset.form', $this->token, false)),
+            'url' => route('admin.password.reset.form', $this->token),
             'actionText' => 'Reset password',
             'copy' => 'You are receiving this email because we received a password reset. If you did not request a password reset, no further action is required.',
         ]);
--- src/Notifications/Welcome.php
+++ src/Notifications/Welcome.php
@@ -16,11 +16,10 @@ class Welcome extends ResetPassword
     public function toMail($notifiable)
     {
         return (new MailMessage)->markdown('twill::emails.html.email', [
-            'url' => url(request()->getScheme() . '://' . config('twill.admin_app_url') . route('admin.password.reset.welcome.form', $this->token, false)),
+            'url' => route('admin.password.reset.welcome.form', $this->token),
             'actionText' => 'Choose your own password',
             'title' => 'Welcome',
             'copy' => 'You are receiving this email because an account was created for you on ' . config('app.name') . '.',
         ]);
-
     }
 }
