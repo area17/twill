1155d33e0d71b15cff6be3242457c7fb6473be1c
Quentin Renard <quentin@area17.com>
2020-07-15 14:35:37 -0400

    Closes #683: update Italian translations file

--- lang/it/lang.php
+++ lang/it/lang.php
@@ -1,31 +1,60 @@
 <?php
-/*
-|--------------------------------------------------------------------------
-| 5 Steps to Contribute a New Twill Localization at Ease
-|--------------------------------------------------------------------------
-| 1. Find the "lang.csv" under "lang" directory.
-| 2. Import the csv file into a blank Google Sheet.
-| 3. Each column is a language, enter the translation for a column. (tips: feel free to freeze rows and columns).
-| 4. Download the Google Sheet as CSV, replace the original "lang/lang.csv" with the new one.
-| 5. Run the command "php artisan twill:lang" to sync all lang files.
- */
+    /*
+    |--------------------------------------------------------------------------
+    | 5 Steps to Contribute a New Twill Localization at Ease
+    |--------------------------------------------------------------------------
+    | 1. Find the "lang.csv" under "lang" directory.
+    | 2. Import the csv file into a blank Google Sheet.
+    | 3. Each column is a language, enter the translation for a column. (tips: feel free to freeze rows and columns).
+    | 4. Download the Google Sheet as CSV, replace the original "lang/lang.csv" with the new one.
+    | 5. Run the command "php artisan twill:lang" to sync all lang files.
+    */
+
 
 return [
     'auth' => [
         'email' => 'Email',
-        'password' => 'Password',
         'forgot-password' => 'Password dimenticata?',
         'login' => 'Login',
         'login-title' => 'Login',
+        'password' => 'Password',
     ],
     'dashboard' => [
-        'search-placeholder' => 'Ricerca in tutto il database...',
-        'empty-message' => 'Non hai ancora nessuna attività',
         'all-activity' => 'Tutte le attività',
-        'my-activity' => 'Le mie attività',
         'create-new' => 'Crea nuovo',
+        'empty-message' => 'Non hai ancora nessuna attività',
+        'my-activity' => 'Le mie attività',
         'my-drafts' => 'Le mie bozze',
+        'search-placeholder' => 'Ricerca in tutto il database...',
         'statitics' => 'Statistiche',
+    ],
+    'fields' => [
+        'block-editor' => [
+            'add-content' => 'Aggiungi contenuto',
+            'collapse-all' => 'Racchiudi tutto',
+            'create-another' => 'Crea e aggiungi ancora',
+            'delete' => 'Elimina',
+            'expand-all' => 'Espandi tutto',
+            'loading' => 'Caricamento',
+            'open-in-editor' => 'Apri in modifica',
+            'preview' => 'Anteprima',
+        ],
+        'browser' => [
+            'add-label' => 'Aggiungi',
+            'attach' => 'Allega',
+        ],
+        'files' => [
+            'add-label' => 'Aggiungi',
+        ],
+        'medias' => [
+            'btn-label' => 'Allega immagine',
+        ],
+    ],
+    'filter' => [
+        'apply-btn' => 'Applica',
+        'clear-btn' => 'Cancella',
+        'search-placeholder' => 'Cerca',
+        'toggle-label' => 'Filtro',
     ],
     'footer' => [
         'version' => 'Versione',
@@ -35,71 +64,78 @@ return [
         'editor' => 'Editor',
     ],
     'listing' => [
+        'add-new-button' => 'Nuovo',
+        'dropdown' => [
+            'delete' => 'Elimina',
+            'destroy' => 'Distruggi',
+            'duplicate' => 'Duplica',
+            'edit' => 'Modifica',
+            'feature' => 'Promuovi',
+            'publish' => 'Pubblica',
+            'restore' => 'Ripristina',
+            'unfeature' => 'Non promuovere',
+            'unpublish' => 'Spubblica',
+        ],
         'filter' => [
             'all-items' => 'Tutti',
+            'draft' => 'Bozza',
             'mine' => 'Miei',
             'published' => 'Pubblicati',
-            'draft' => 'Bozza',
             'trash' => 'Cestino',
         ],
-        'dropdown' => [
-            'edit' => 'Modifica',
-            'unpublish' => 'Spubblica',
-            'publish' => 'Pubblica',
-            'unfeature' => 'Non promuovere',
-            'feature' => 'Promuovi',
-            'restore' => 'Ripristina',
-            'destroy' => 'Distruggi',
-            'delete' => 'Elimina',
-            'duplicate' => 'Duplica',
-        ],
-        'add-new-button' => 'Nuovo',
-        'listing-empty-message' => 'Non è presente alcun elemento.',
         'languages' => 'Lingue',
+        'listing-empty-message' => 'Non è presente alcun elemento.',
     ],
     'main' => [
-        'published' => 'Pubblicato',
-        'draft' => 'Bozza',
         'create' => 'Crea',
-        'update' => 'Aggiorna',
+        'draft' => 'Bozza',
+        'published' => 'Pubblicato',
         'title' => 'Titolo',
+        'update' => 'Aggiorna',
+    ],
+    'media-library' => [
+        'files' => 'Documenti',
+        'filter-select-label' => 'Filtra per Tag',
+        'images' => 'Immagini',
+        'sidebar' => [
+            'clear' => 'Cancella',
+            'dimensions' => 'Dimensioni',
+            'empty-text' => 'Nessun file selezionato',
+            'files-selected' => 'file selezionati',
+        ],
+        'title' => 'Libreria',
     ],
     'modal' => [
-        'title-field' => 'Titolo',
-        'permalink-field' => 'Permalink',
         'create' => [
-            'title' => 'Nuovo',
             'button' => 'Crea',
             'create-another' => 'Crea e aggiungi ancora',
+            'title' => 'Nuovo',
         ],
+        'permalink-field' => 'Permalink',
+        'title-field' => 'Titolo',
         'update' => [
             'title' => 'Aggiurna',
         ],
     ],
     'nav' => [
-        'media-library' => 'Libreria',
-        'cms-users' => 'CMS Utenti',
-        'settings' => 'Impostazioni',
-        'logout' => 'Esci',
         'admin' => 'Admin',
+        'cms-users' => 'CMS Utenti',
+        'logout' => 'Esci',
+        'media-library' => 'Libreria',
+        'settings' => 'Impostazioni',
     ],
-    'media-library' => [
-        'title' => 'Libreria',
-        'images' => 'Immagini',
-        'files' => 'Documenti',
-        'filter-select-label' => 'Filtra per Tag',
-        'sidebar' => [
-            'empty-text' => 'Nessun file selezionato',
-            'files-selected' => 'file selezionati',
-            'clear' => 'Cancella',
-            'dimensions' => 'Dimensioni',
-        ],
-    ],
-    'filter' => [
-        'search-placeholder' => 'Cerca',
-        'toggle-label' => 'Filtro',
-        'apply-btn' => 'Applica',
-        'clear-btn' => 'Cancella',
+    'publisher' => [
+        'cancel' => 'Annulla',
+        'publish' => 'Pubblica',
+        'publish-close' => 'Pubblica e chiudi',
+        'publish-new' => 'Pubblica e crea nuovo',
+        'save' => 'Salva come Bozza',
+        'save-close' => 'Salva come Bozza e chiudi',
+        'save-new' => 'Salva come bozza e crea nuovo',
+        'switcher-title' => 'Stato',
+        'update' => 'Aggiorna',
+        'update-close' => 'Aggiorna e chiudi',
+        'update-new' => 'Aggiorna e crea nuovo',
     ],
     'select' => [
         'empty-text' => '...nessuna opzione',
@@ -108,62 +144,27 @@ return [
         'dropzone-text' => 'o trascina i file qui',
         'upload-btn-label' => 'Aggiungi',
     ],
-    'fields' => [
-        'medias' => [
-            'btn-label' => 'Allega immagine',
-        ],
-        'block-editor' => [
-            'collapse-all' => 'Racchiudi tutto',
-            'expand-all' => 'Espandi tutto',
-            'open-in-editor' => 'Apri in modifica',
-            'create-another' => 'Crea e aggiungi ancora',
-            'delete' => 'Elimina',
-            'add-content' => 'Aggiungi contenuto',
-            'preview' => 'Anteprima',
-            'loading' => 'Caricamento',
-        ],
-        'browser' => [
-            'attach' => 'Allega',
-            'add-label' => 'Aggiungi',
-        ],
-        'files' => [
-            'add-label' => 'Aggiungi',
-        ],
-    ],
     'user-management' => [
-        'users' => 'Utenti',
         'active' => 'Attivi',
-        'disabled' => 'Disabilitati',
-        'enabled' => 'Abilitati',
-        'trash' => 'Cestino',
-        'user-image' => 'Immagine utente',
-        'name' => 'Nominativo',
-        'email' => 'Email',
-        'role' => 'Ruolo',
+        'cancel' => 'Annulla',
         'content-fieldset-label' => 'Impostazioni utente',
+        'disabled' => 'Disabilitati',
         'edit-modal-title' => 'Modifica il nominativo',
-        'update-disabled-user' => 'Disabilita',
-        'update-disabled-and-close' => 'Disabilita e chiudi',
-        'update-disabled-user-and-create-new' => 'Disabilita e crea nuovo',
+        'email' => 'Email',
         'enable-user' => 'Abilita',
         'enable-user-and-close' => 'Abilita e chiudi',
         'enable-user-and-create-new' => 'Abilita e crea nuovo',
+        'enabled' => 'Abilitati',
+        'name' => 'Nominativo',
+        'role' => 'Ruolo',
+        'trash' => 'Cestino',
         'update' => 'Aggiorna',
         'update-and-close' => 'Aggiorna e chiudi',
         'update-and-create-new' => 'Aggiorna e crea nuovo',
-        'cancel' => 'Annulla',
-    ],
-    'publisher' => [
-        'switcher-title' => 'Stato',
-        'save' => 'Salva come Bozza',
-        'save-close' => 'Salva come Bozza e chiudi',
-        'save-new' => 'Salva come bozza e crea nuovo',
-        'publish' => 'Pubblica',
-        'publish-close' => 'Pubblica e chiudi',
-        'publish-new' => 'Pubblica e crea nuovo',
-        'update' => 'Aggiorna',
-        'update-close' => 'Aggiorna e chiudi',
-        'update-new' => 'Aggiorna e crea nuovo',
-        'cancel' => 'Annulla',
+        'update-disabled-and-close' => 'Disabilita e chiudi',
+        'update-disabled-user' => 'Disabilita',
+        'update-disabled-user-and-create-new' => 'Disabilita e crea nuovo',
+        'user-image' => 'Immagine utente',
+        'users' => 'Utenti',
     ],
 ];
