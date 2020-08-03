865bf176d39627ee1b1bd9554e91e91564b50b27
Pablo Barrios <ingpablobarrios@gmail.com>
2020-08-03 15:12:25 -0300

    Add ability to store more data from the Google Maps API for the Map form field.
    - Add documentation for the saveExtendedData parameter.
    - Add examples of the returned and stored data.

--- views/partials/form/_map.blade.php
+++ views/partials/form/_map.blade.php
@@ -2,6 +2,7 @@
     $showMap = $showMap ?? true;
     $openMap = $openMap ?? false;
     $inModal = $fieldsInModal ?? false;
+    $saveExtendedData = $saveExtendedData ?? false;
 @endphp
 
 <a17-locationfield
@@ -10,6 +11,7 @@
     @if ($showMap) show-map @else :show-map="false" @endif
     @if ($openMap) open-map @endif
     @if ($inModal) :in-modal="true" @endif
+    @if ($saveExtendedData) :save-extended-data="true" @endif
     in-store="value"
 ></a17-locationfield>
 
