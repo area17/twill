9396bd6365f7268b0f65186e5362229c13965522
Antonio Ribeiro <antonio@area17.com>
2020-08-24 14:03:38 +0200

    Add Glide cloud sources to allow Glide and S3 integration

---
+++ src/Services/Cloud/Azure.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace A17\Twill\Services\Cloud;
+
+use Exception;
+
+class Azure
+{
+    public function filesystemFactory($prefix)
+    {
+        throw new Exception('Azure is not implemented yey.');
+    }
+}
