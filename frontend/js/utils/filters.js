57ae376e14e9e128c02b5d1f56bc1a375866b05f
td.nguyen <68363016+tdnguyen-PACE@users.noreply.github.com>
2020-09-18 03:33:48 +0700

    add sanitizeTitle for slugify. Ex: "Tp. Hồ Chí Minh" => "tp-ho-chi-minh"

--- frontend/js/utils/filters.js
+++ frontend/js/utils/filters.js
@@ -10,11 +10,38 @@ function dateFormatLocale (date, format) {
   })
 }
 
+function sanitizeTitle (title) {
+  var slug = ''
+  // Change to lower case
+  var titleLower = title.toLowerCase()
+  // Letter 'e'
+  slug = titleLower.replace(/e|é|è|ẽ|ẻ|ẹ|ê|ế|ề|ễ|ể|ệ/gi, 'e')
+  // Letter 'a'
+  slug = slug.replace(/a|á|à|ã|ả|ạ|ă|ắ|ằ|ẵ|ẳ|ặ|â|ấ|ầ|ẫ|ẩ|ậ/gi, 'a')
+  // Letter 'o'
+  slug = slug.replace(/o|ó|ò|õ|ỏ|ọ|ô|ố|ồ|ỗ|ổ|ộ|ơ|ớ|ờ|ỡ|ở|ợ/gi, 'o')
+  // Letter 'u'
+  slug = slug.replace(/u|ú|ù|ũ|ủ|ụ|ư|ứ|ừ|ữ|ử|ự/gi, 'u')
+  slug = slug.replace(/ì|í|ị|ỉ|ĩ/g, 'i')
+  slug = slug.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y')
+  slug = slug.replace(/đ/g, 'd')
+  // Some system encode vietnamese combining accent as individual utf-8 characters
+  slug = slug.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, '') // Huyền sắc hỏi ngã nặng
+  slug = slug.replace(/\u02C6|\u0306|\u031B/g, '') // Â, Ê, Ă, Ơ, Ư
+  // Letter 'd'
+  // Trim the last whitespace
+  slug = slug.replace(/\s*$/g, '')
+  // Change whitespace to '-'
+  slug = slug.replace(/\s+/g, '-')
+
+  return slug
+}
+
 const filters = {
   slugify: function (value) {
     const charMap = { ',': '-', '/': '-', ':': '-', ';': '-', _: '-', '©': '(c)', '·': '-', ß: 'ss', à: 'a', á: 'a', â: 'a', ã: 'a', ä: 'a', å: 'a', æ: 'ae', ç: 'c', è: 'e', é: 'e', ê: 'e', ë: 'e', ì: 'i', í: 'i', î: 'i', ï: 'i', ð: 'd', ñ: 'n', ò: 'o', ó: 'o', ô: 'o', õ: 'o', ö: 'o', ø: 'o', ù: 'u', ú: 'u', û: 'u', ü: 'u', ý: 'y', þ: 'th', ÿ: 'y', ā: 'a', ă: 'a', ą: 'a', ć: 'c', č: 'c', ď: 'd', ē: 'e', ę: 'e', ě: 'e', ğ: 'g', ģ: 'g', ī: 'i', ı: 'i', ķ: 'k', ļ: 'l', ł: 'l', ń: 'n', ņ: 'n', ň: 'n', ő: 'o', œ: 'oe', ŕ: 'r', ř: 'r', ś: 's', ş: 's', š: 's', ť: 't', ū: 'u', ů: 'u', ű: 'u', ź: 'z', ż: 'z', ž: 'z', ǘ: 'u', ǵ: 'g', ǹ: 'n', ș: 's', ț: 't', ΐ: 'i', ά: 'a', έ: 'e', ή: 'h', ί: 'i', ΰ: 'y', α: 'a', β: 'b', γ: 'g', δ: 'd', ε: 'e', ζ: 'z', η: 'h', θ: '8', ι: 'i', κ: 'k', λ: 'l', μ: 'm', ν: 'n', ξ: '3', ο: 'o', π: 'p', ρ: 'r', ς: 's', σ: 's', τ: 't', υ: 'y', φ: 'f', χ: 'x', ψ: 'ps', ω: 'w', ϊ: 'i', ϋ: 'y', ό: 'o', ύ: 'y', ώ: 'w', а: 'a', б: 'b', в: 'v', г: 'g', д: 'd', е: 'e', ж: 'zh', з: 'z', и: 'i', й: 'j', к: 'k', л: 'l', м: 'm', н: 'n', о: 'o', п: 'p', р: 'r', с: 's', т: 't', у: 'u', ф: 'f', х: 'h', ц: 'c', ч: 'ch', ш: 'sh', щ: 'sh', ъ: '', ы: 'y', ь: '', э: 'e', ю: 'yu', я: 'ya', ё: 'yo', є: 'ye', і: 'i', ї: 'yi', ґ: 'g', ḧ: 'h', ḿ: 'm', ṕ: 'p', ẃ: 'w', ẍ: 'x', ә: 'a', ғ: 'g', қ: 'q', ң: 'n', ө: 'o', ұ: 'u' }
     const p = new RegExp(Object.keys(charMap).join('|'), 'g')
-    return value.toString().toLowerCase().trim()
+    return sanitizeTitle(value).toString().toLowerCase().trim()
       .replace(/\s+/g, '-') // Replace spaces with -
       .replace(p, c => charMap[c]) // Replace special chars
       .replace(/&/g, '-and-') // Replace & with 'and'
