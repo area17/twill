30fe3593839376623919f90db204283e1b0ed7e8
Quentin Renard <quentin@area17.com>
2021-04-26 17:47:54 +0200

    Add some missing Polish language strings. Closes #752

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -1,22 +1,23 @@
 <?php
-    /*
-    |--------------------------------------------------------------------------
-    | 5 Steps to Contribute a New Twill Localization at Ease
-    |--------------------------------------------------------------------------
-    | 1. Find the "lang.csv" under "lang" directory.
-    | 2. Import the csv file into a blank Google Sheet.
-    | 3. Each column is a language, enter the translation for a column. (tips: feel free to freeze rows and columns).
-    | 4. Download the Google Sheet as CSV, replace the original "lang/lang.csv" with the new one.
-    | 5. Run the command "php artisan twill:lang" to sync all lang files.
-    */
-
+/*
+|--------------------------------------------------------------------------
+| 5 Steps to Contribute a New Twill Localization at Ease
+|--------------------------------------------------------------------------
+| 1. Find the "lang.csv" under "lang" directory.
+| 2. Import the csv file into a blank Google Sheet.
+| 3. Each column is a language, enter the translation for a column. (tips: feel free to freeze rows and columns).
+| 4. Download the Google Sheet as CSV, replace the original "lang/lang.csv" with the new one.
+| 5. Run the command "php artisan twill:lang" to sync all lang files.
+ */
 
 return [
     'auth' => [
         'back-to-login' => 'Powrót do logowania',
         'choose-password' => 'Wybierz hasło',
+        'email' => 'Adres e-mail',
         'forgot-password' => 'Zapomniałem hasła',
         'login' => 'Zaloguj się',
+        'login-title' => 'Logowanie',
         'oauth-link-title' => 'Wprowadź hasło ponownie aby połączyć swoje konto z :provider',
         'otp' => 'Kod jednorazowy',
         'password' => 'Hasło',
