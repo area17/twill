5d9c99535fa5109b3990c268738f0effd5863cf5
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2021-05-23 00:09:02 +0200

    Supports multi-country locales on Translatable

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -245,7 +245,6 @@ if (!function_exists('capsule_namespace')) {
     }
 }
 
-
 if (!function_exists('capsule_namespace_to_path')) {
     function capsule_namespace_to_path($namespace, $capsuleNamespace, $rootPath) {
         return capsules()->capsuleNamespaceToPath($namespace, $capsuleNamespace, $rootPath);
@@ -261,5 +260,17 @@ if (!function_exists('capsules')) {
         }
 
         return $manager;
+    }
+}
+
+if (!function_exists('twill_locales')) {
+    function twill_locales() {
+        return $allLanguages = collect(config('translatable.locales'))->map(function ($locale, $index) {
+            return collect($locale)->map(function ($country) use ($locale, $index) {
+                return is_numeric($index)
+                    ? $locale
+                    : "$index-$country";
+            });
+        })->flatten()->toArray();
     }
 }
