3c45b3ff8c45dee2efaa0dc3afa1ee4de2063137
Patrick Boivin <pboivin@gmail.com>
2021-06-05 12:11:48 -0400

    Revert code reformatting

--- src/Models/Behaviors/HasSlug.php
+++ src/Models/Behaviors/HasSlug.php
@@ -524,20 +524,7 @@ trait HasSlug
      */
     public function urlSlugShorter($string)
     {
-        return strtolower(
-                trim(
-                    preg_replace('~[^0-9a-z]+~i', '-',
-                        html_entity_decode(
-                            preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1',
-                                         htmlentities($string, ENT_QUOTES, 'UTF-8')
-                            ),
-                            ENT_QUOTES,
-                            'UTF-8'
-                        )
-                    ),
-                    '-'
-                )
-            );
+        return strtolower(trim(preg_replace('~[^0-9a-z]+~i', '-', html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8')), '-'));
     }
 
     /**
