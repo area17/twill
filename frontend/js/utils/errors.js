95feee8c686c3f566d3ff5565af30f90c1a7bd10
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2021-06-08 01:51:52 +0200

    Add an axios global error handler to warn on session expired

--- frontend/js/utils/errors.js
+++ frontend/js/utils/errors.js
@@ -7,13 +7,15 @@ export function globalError (component = null, error = { message: '', value: nul
 
   const errorMessage = prefix + error.message
 
+  const statusCode = error?.value?.response?.status ?? error?.response?.status ?? null
+
   console.error(errorMessage)
 
-  if (error.value && error.value.response) {
-    console.error(error.value.response.data)
+  if (error?.value && error.value?.response) {
+    console.error(error.value.response?.data)
   }
 
-  if ('response' in error.value && 'status' in error.value.response && error.value.response.status === 401) {
+  if (statusCode === 401) {
     window[process.env.VUE_APP_NAME].vm.notif({
       message: 'Your session has expired, please <a href="' + document.location + '" target="_blank">login in another tab</a>. You can then continue working here.',
       variant: 'warning'
