7e78f10eeaf1ab95196837a437cc5ef62797bb35
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2021-06-08 02:13:28 +0200

    Also handle 419 errors

--- frontend/js/utils/errors.js
+++ frontend/js/utils/errors.js
@@ -15,7 +15,9 @@ export function globalError (component = null, error = { message: '', value: nul
     console.error(error.value.response?.data)
   }
 
-  if (statusCode === 401) {
+  // Error 401 = session expired / not authenticated
+  // Error 419 = CSRF token mismatched
+  if (statusCode === 401 || statusCode === 419) {
     window[process.env.VUE_APP_NAME].vm.notif({
       message: 'Your session has expired, please <a href="' + document.location + '" target="_blank">login in another tab</a>. You can then continue working here.',
       variant: 'warning'
