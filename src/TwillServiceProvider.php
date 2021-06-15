c465f1481294b082cbd40e7e0927fbceddc0a074
Quentin Renard <quentin@area17.com>
2021-06-15 23:48:56 +0200

    Update version

--- src/TwillServiceProvider.php
+++ src/TwillServiceProvider.php
@@ -47,7 +47,7 @@ class TwillServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '2.3.1';
+    const VERSION = '2.4.0';
 
     /**
      * Service providers to be registered.
