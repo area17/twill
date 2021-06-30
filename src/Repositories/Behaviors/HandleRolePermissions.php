528f535aad05156f08e1dbcf168b6aa78b97864c
Patrick Vézina <patrick@croustille.io>
2021-06-30 18:07:21 -0400

    Role doc blocks

--- src/Repositories/Behaviors/HandleRolePermissions.php
+++ src/Repositories/Behaviors/HandleRolePermissions.php
@@ -2,67 +2,29 @@
 
 namespace A17\Twill\Repositories\Behaviors;
 
+use A17\Twill\Models\Role;
 use Illuminate\Support\Str;
 use A17\Twill\Models\Permission;
 
 trait HandleRolePermissions
 {
+    /**
+     * Retrieve role permissions fields
+     *
+     * @param Model|Role $object
+     * @param array $fields
+     * @return array
+     */
     public function getFormFieldsHandleRolePermissions($object, $fields)
     {
-        $fields = $this->renderRolePermissions($object, $fields);
+        $object->permissions()->get();
 
-        return $fields;
-    }
-
-    public function afterSaveHandleRolePermissions($object, $fields)
-    {
-        $this->handleRolePermissions($object, $fields);
-    }
-
-    // After save handle permissions form fields on role form
-    protected function handleRolePermissions($role, $fields)
-    {
-        foreach (Permission::available(Permission::SCOPE_GLOBAL) as $permissionName) {
-            if (isset($fields[$permissionName]) && $fields[$permissionName] === true) {
-                $role->grantGlobalPermission($permissionName);
-            } else {
-                $role->revokeGlobalPermission($permissionName);
-            }
-        }
-
-        foreach ($fields as $key => $permissionName) {
-            if (Str::startsWith($key, 'module_') && Str::endsWith($key, '_permissions')) {
-                $modulePermissions = Permission::available(Permission::SCOPE_MODULE);
-                $model = getModelByModuleName($moduleName = explode('_', $key)[1]);
-
-                $currentPermission = $role->permissions()
-                    ->where('permissionable_type', $model)
-                    ->whereIn('name', $modulePermissions)
-                    ->first()
-                ;
-
-                if (!$currentPermission || $permissionName != $currentPermission->name) {
-                    $role->revokeAllModulePermission($model);
-                    if (in_array($permissionName, $modulePermissions)) {
-                        $role->grantModulePermission($permissionName, $model);
-                    }
-                }
-            }
-        }
-
-        $role->save();
-    }
-
-    protected function renderRolePermissions($role, $fields)
-    {
-        $role->permissions()->get();
-
-        foreach ($role->permissions()->global()->pluck('name')->toArray() as $permissionName) {
+        foreach ($object->permissions()->global()->pluck('name')->toArray() as $permissionName) {
             $fields[$permissionName] = true;
         }
 
         foreach (Permission::permissionableModules() as $moduleName) {
-            $modulePermission = $role->permissions()->module()->ofModuleName($moduleName)->first();
+            $modulePermission = $object->permissions()->module()->ofModuleName($moduleName)->first();
             if ($modulePermission) {
                 $fields['module_' . $moduleName . '_permissions'] = $modulePermission->name;
             } else {
@@ -71,5 +33,44 @@ trait HandleRolePermissions
         }
 
         return $fields;
+    }
+
+    /**
+     * Function executed after save on role form
+     *
+     * @param Model|Role $object
+     * @param array $fields
+     */
+    public function afterSaveHandleRolePermissions($object, $fields)
+    {
+        foreach (Permission::available(Permission::SCOPE_GLOBAL) as $permissionName) {
+            if (isset($fields[$permissionName]) && $fields[$permissionName] === true) {
+                $object->grantGlobalPermission($permissionName);
+            } else {
+                $object->revokeGlobalPermission($permissionName);
+            }
+        }
+
+        foreach ($fields as $key => $permissionName) {
+            if (Str::startsWith($key, 'module_') && Str::endsWith($key, '_permissions')) {
+                $modulePermissions = Permission::available(Permission::SCOPE_MODULE);
+                $model = getModelByModuleName($moduleName = explode('_', $key)[1]);
+
+                $currentPermission = $object->permissions()
+                    ->where('permissionable_type', $model)
+                    ->whereIn('name', $modulePermissions)
+                    ->first()
+                ;
+
+                if (!$currentPermission || $permissionName != $currentPermission->name) {
+                    $object->revokeAllModulePermission($model);
+                    if (in_array($permissionName, $modulePermissions)) {
+                        $object->grantModulePermission($permissionName, $model);
+                    }
+                }
+            }
+        }
+
+        $object->save();
     }
 }
