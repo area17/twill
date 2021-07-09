a5e224efec800ed1e551e75c9fe75626efcce622
Patrick Boivin <pboivin@gmail.com>
2021-07-09 16:51:23 -0400

    Update User model

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,11 +3,16 @@
 namespace A17\Twill\Models;
 
 use A17\Twill\Models\Behaviors\HasMedias;
-use A17\Twill\Models\Behaviors\HasPresenter;
 use A17\Twill\Models\Behaviors\HasOauth;
+use A17\Twill\Models\Behaviors\HasPermissions;
+use A17\Twill\Models\Behaviors\HasPresenter;
 use A17\Twill\Models\Enums\UserRole;
 use A17\Twill\Models\Behaviors\IsTranslatable;
+use A17\Twill\Models\Group;
+use A17\Twill\Models\Role;
 use A17\Twill\Notifications\Reset as ResetNotification;
+use A17\Twill\Notifications\TemporaryPassword as TemporaryPasswordNotification;
+use A17\Twill\Notifications\PasswordResetByAdmin as PasswordResetByAdminNotification;
 use A17\Twill\Notifications\Welcome as WelcomeNotification;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -19,9 +24,15 @@ use PragmaRX\Google2FAQRCode\Google2FA;
 
 class User extends AuthenticatableContract
 {
-    use Authenticatable, Authorizable, HasMedias, Notifiable, HasPresenter, HasOauth, SoftDeletes, IsTranslatable;
+
+    use Authenticatable, Authorizable, HasMedias, Notifiable, HasPresenter, HasOauth, HasPermissions, SoftDeletes, IsTranslatable;
 
     public $timestamps = true;
+
+    protected $casts = [
+        'is_superadmin' => 'boolean',
+        'published' => 'boolean',
+    ];
 
     protected $fillable = [
         'email',
@@ -30,6 +41,7 @@ class User extends AuthenticatableContract
         'published',
         'title',
         'description',
+        'role_id',
         'google_2fa_enabled',
         'google_2fa_secret',
         'language'
@@ -37,6 +49,8 @@ class User extends AuthenticatableContract
 
     protected $dates = [
         'deleted_at',
+        'registered_at',
+        'last_login_at',
     ];
 
     protected $hidden = ['password', 'remember_token', 'google_2fa_secret'];
@@ -53,8 +67,6 @@ class User extends AuthenticatableContract
         ],
     ];
 
-    protected $casts = ['published' => 'boolean'];
-
     public function __construct(array $attributes = [])
     {
         $this->table = config('twill.users_table', 'twill_users');
@@ -69,11 +81,15 @@ class User extends AuthenticatableContract
 
     public function getRoleValueAttribute()
     {
-        if (!empty($this->role)) {
-            if ($this->role == 'SUPERADMIN') {
-                return "SUPERADMIN";
-            }
+        if ($this->is_superadmin || $this->role == 'SUPERADMIN') {
+            return 'SUPERADMIN';
+        }
 
+        if (config('twill.enabled.permissions-management')) {
+            return $this->role ? $this->role->name : null;
+        }
+
+        if (!empty($this->role)) {
             return UserRole::{$this->role}()->getValue();
         }
 
@@ -83,6 +99,16 @@ class User extends AuthenticatableContract
     public function getCanDeleteAttribute()
     {
         return auth('twill_users')->user()->id !== $this->id;
+    }
+
+    public function scopeActivated($query)
+    {
+        return $query->whereActivated(true);
+    }
+
+    public function scopePending($query)
+    {
+        return $query->whereActivated(false)->published();
     }
 
     public function scopePublished($query)
@@ -152,6 +178,52 @@ class User extends AuthenticatableContract
     public function isPublished()
     {
         return (bool) $this->published;
+    }
+
+    public function sendTemporaryPasswordNotification($password)
+    {
+        $this->notify(new TemporaryPasswordNotification($password));
+    }
+
+    public function sendPasswordResetByAdminNotification($password)
+    {
+        $this->notify(new PasswordResetByAdminNotification($password));
+    }
+
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class, 'group_twill_user', 'twill_user_id', 'group_id');
+    }
+
+    public function publishedGroups()
+    {
+        return $this->groups()->published();
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function allPermissions()
+    {
+        $permissions = Permission::whereHas('users', function ($query) {
+            $query->where('id', $this->id);
+        })->orWhereHas('roles', function ($query) {
+            $query->where('id', $this->role->id);
+        })->orWhereHas('groups', function ($query) {
+            $query
+                ->join('group_twill_user', 'groups.id', '=', 'group_twill_user.group_id')
+                ->where('group_twill_user.twill_user_id', $this->id)
+                ->where('published', 1)
+            ;
+        });
+        return $permissions;
+    }
+
+    public function getLastLoginColumnValueAttribute()
+    {
+        return $this->last_login_at ? $this->last_login_at->format('d M Y, H:i') : ($this->activated ? '&mdash;' : twillTrans('twill::lang.user-management.activation-pending'));
     }
 
     public function setGoogle2faSecretAttribute($secret)
