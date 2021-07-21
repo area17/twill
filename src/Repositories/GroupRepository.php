ec6af15a3d20a90a5c71520abffbed97862659fa
Patrick Boivin <pboivin@gmail.com>
2021-07-21 16:31:37 -0400

    Prevent delete and bulkDelete actions on Everyone group

--- src/Repositories/GroupRepository.php
+++ src/Repositories/GroupRepository.php
@@ -29,4 +29,26 @@ class GroupRepository extends ModuleRepository
 
         parent::afterSave($group, $fields);
     }
+
+    public function delete($id)
+    {
+        if ($this->model->find($id)->is_everyone_group) {
+            return false;
+        }
+
+        return parent::delete($id);
+    }
+
+    public function bulkDelete($ids)
+    {
+        $includes_everyone_group = $this->model->whereIn('id', $ids)
+            ->where('is_everyone_group', true)
+            ->first();
+
+        if ($includes_everyone_group) {
+            return false;
+        }
+
+        return parent::bulkDelete($ids);
+    }
 }
