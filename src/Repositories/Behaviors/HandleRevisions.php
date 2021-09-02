d563474f148fcb8293df40dee012066b07cb7289
Patrick Boivin <pboivin@gmail.com>
2021-09-02 17:18:33 -0400

    Cleanup

--- src/Repositories/Behaviors/HandleRevisions.php
+++ src/Repositories/Behaviors/HandleRevisions.php
@@ -4,7 +4,6 @@ namespace A17\Twill\Repositories\Behaviors;
 
 use A17\Twill\Models\Behaviors\HasRelated;
 use A17\Twill\Models\RelatedItem;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
@@ -18,12 +17,10 @@ trait HandleRevisions
      */
     public function hydrateHandleRevisions($object, $fields)
     {
-        // HandleRepeaters trait => getRepeaters
         foreach($this->getRepeaters() as $repeater) {
             $this->hydrateRepeater($object, $fields, $repeater['relation'], $repeater['model']);
         }
 
-        // HandleBrowers trait => getBrowsers
         foreach($this->getBrowsers() as $browser) {
             $this->hydrateBrowser($object, $fields, $browser['relation'], $browser['positionAttribute'], $browser['model']);
         }
@@ -133,7 +130,7 @@ trait HandleRevisions
      */
     public function hydrateBrowser($object, $fields, $relationship, $positionAttribute = 'position', $model = null)
     {
-        return $this->hydrateOrderedBelongsTomany($object, $fields, $relationship, $positionAttribute, $model);
+        return $this->hydrateOrderedBelongsToMany($object, $fields, $relationship, $positionAttribute, $model);
     }
 
     /**
@@ -144,7 +141,7 @@ trait HandleRevisions
      * @param \A17\Twill\Models\Model|null $model
      * @return void
      */
-    public function hydrateOrderedBelongsTomany($object, $fields, $relationship, $positionAttribute = 'position', $model = null)
+    public function hydrateOrderedBelongsToMany($object, $fields, $relationship, $positionAttribute = 'position', $model = null)
     {
         $fieldsHasElements = isset($fields['browsers'][$relationship]) && !empty($fields['browsers'][$relationship]);
         $relatedElements = $fieldsHasElements ? $fields['browsers'][$relationship] : [];
