38007f0fe2020839675812ff5b598d36cf4683e3
Patrick Boivin <pboivin@gmail.com>
2021-09-28 18:24:30 -0400

    Add SingletonModuleController

---
+++ src/Http/Controllers/Admin/SingletonModuleController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace A17\Twill\Http\Controllers\Admin;
+
+use Illuminate\Support\Facades\Session;
+
+abstract class SingletonModuleController extends ModuleController
+{
+    protected $permalinkBase = '';
+
+    public function index($parentModuleId = null)
+    {
+        throw new \Exception("{$this->getModelName()} has no index");
+    }
+
+    public function editSingleton()
+    {
+        $model = "App\\Models\\{$this->getModelName()}";
+
+        $item = app($model)->first();
+
+        if (!$item) {
+            throw new \Exception("{$this->getModelName()} is missing");
+        }
+
+        Session::put('pages_back_link', url()->current());
+
+        return view("admin.{$this->moduleName}.form", $this->form($item->id));
+    }
+}
