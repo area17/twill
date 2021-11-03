44d78bb8344eab17b5d7df0927ef0c710282c5e4
Harings Rob <haringsrob@gmail.com>
2021-11-03 20:25:31 +0100

    Remove unused include.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,7 +15,6 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/helpers.php';
 require __DIR__ . '/../src/Helpers/helpers.php';
-include_once __DIR__ . '/Providers/ConfigInjectingProvider.php';
 
 use Carbon\Carbon;
 
