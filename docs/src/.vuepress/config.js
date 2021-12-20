f68d67d5b4d7f50b56b0ab4305f4aa74cf87b14f
Patrick Boivin <pboivin@gmail.com>
2021-12-20 18:48:55 -0500

    docs: Add pagination (#1131)
    Co-authored-by: Patrick Boivin <pboivin@gmail.com>
    Co-authored-by: Alexander <avramch@users.noreply.github.com>
    Co-authored-by: afatmustafa <mustafa@afat.me>
    Co-authored-by: Markel <MarcelWeidum@users.noreply.github.com>
    Co-authored-by: Freshleaf Media <10062339+freshleafmedia@users.noreply.github.com>
    Co-authored-by: Quentin Renard <quentin@area17.com>

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  title: 'Twill',
+  description: 'Twill — An open source CMS toolkit for Laravel',
+  base: "/docs/",
+  dest: "docs",
+  head: [
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', href: '/favicon-192.png' }],
+    ['meta', { name: 'theme-color', content: '#000000' }],
+    ['meta', { property: 'og:url', content: 'https://twill.io/' }],
+    ['meta', { name: 'twitter:url', content: 'https://twill.io/' }],
+    ['meta', { property: 'og:image', content: '/social_share.png' }],
+    ['meta', { property: 'og:image:width', content: '1200' }],
+    ['meta', { property: 'og:image:height', content: '630' }],
+    ['meta', { name: 'twitter:image', content: '/social_share.png' }],
+    ['meta', { itemprop: 'image', content: '/social_share.png' }],
+    ['meta', { property: 'og:site_name', content: 'Twill' }],
+    ['meta', { property: 'og:author', content: 'https://www.facebook.com/twillcms/' }],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:site', content: '@twillcms' }],
+    ['meta', { name: 'twitter:domain', content: 'twill.io' }],
+    ['meta', { name: 'twitter:creator', content: '@twillcms' }]
+  ],
+  themeConfig: {
+    docsRepo: 'area17/twill',
+    docsDir: 'docs/src',
+    docsBranch: '2.x',
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    algolia: {
+      apiKey: '9360cb12e45076d95f77b63549021a6d',
+      indexName: 'twill',
+      appId: '89HNJPXALF',
+    },
+    nav: [
+      { text: 'GitHub', link: 'https://github.com/area17/twill' },
+    ],
+    sidebar: require('./sidebar.js'),
+  },
+  plugins: [
+    ['@vuepress/google-analytics', {
+      ga: 'UA-117094786-1'
+    }]
+  ]
+}
---
+++ docs/src/form-fields/checkbox.md
@@ -0,0 +1,26 @@
+---
+pageClass: twill-doc
+---
+
+# Checkbox
+
+![screenshot](/docs/_media/checkbox.png)
+
+```php
+@formField('checkbox', [
+    'name' => 'featured',
+    'label' => 'Featured'
+])
+```
+
+| Option              | Description                                             | Type            | Default value |
+| :------------------ | :------------------------------------------------------ | :-------------- | :------------ |
+| name                | Name of the field                                       | string          |               |
+| label               | Label of the field                                      | string          |               |
+| note                | Hint message displayed above the field                  | string          |               |
+| default             | Sets a default value                                    | boolean         | false         |
+| disabled            | Disables the field                                      | boolean         | false         | 
+| requireConfirmation | Displays a confirmation dialog when modifying the field | boolean         | false         |
+| confirmTitleText    | The title of the confirmation dialog                    | string          | 'Confirm selection' |
+| confirmMessageText  | The text of the confirmation dialog                     | string          | 'Are you sure you want to change this option ?' |
+| border              | Draws a border around the field                         | boolean         | false         |
