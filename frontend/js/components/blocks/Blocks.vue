26981f17e94f3e90a30edf417a8ea729d517905b
Harings Rob <haringsrob@gmail.com>
2022-01-12 16:58:07 +0100

    #1332: Expand all blocks if they are less then 3.

--- frontend/js/components/blocks/Blocks.vue
+++ frontend/js/components/blocks/Blocks.vue
@@ -265,7 +265,9 @@
     mounted () {
       // if there are blocks, these should be all collapse by default
       this.$nextTick(function () {
-        if (this.blocks(this.editorName) && this.blocks(this.editorName).length > 3) this.collapseAllBlocks()
+        if (this.blocks(this.editorName) && this.blocks(this.editorName).length < 4) {
+          this.$refs.blockList.forEach((block) => block.toggleExpand())
+        }
       })
     }
   }
