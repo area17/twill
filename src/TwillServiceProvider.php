4c7369cb12c57acfee38d5652e74b2ab1e2f8778
Harings Rob <haringsrob@gmail.com>
2022-02-02 11:30:29 +0100

    Register new command.

--- src/TwillServiceProvider.php
+++ src/TwillServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace A17\Twill;
 
+use A17\Twill\Commands\UpdateExampleCommand;
 use Exception;
 use A17\Twill\Commands\BlockMake;
 use A17\Twill\Commands\Build;
@@ -325,6 +326,7 @@ class TwillServiceProvider extends ServiceProvider
             Dev::class,
             SyncLang::class,
             CapsuleInstall::class,
+            UpdateExampleCommand::class
         ]);
     }
 
