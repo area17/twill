7476891c61be6aa34d1edb7b7d8c237a70e32e40
Harings Rob <haringsrob@gmail.com>
2022-02-18 13:41:06 +0100

    Implement max.

--- src/View/Components/Repeater.php
+++ src/View/Components/Repeater.php
@@ -6,19 +6,22 @@ class Repeater extends TwillFormComponent
 {
     public $buttonAsLink;
     public $type;
+    public $max;
 
     public function __construct(
         $type,
         $name = null,
         $buttonAsLink = false,
         $renderForBlocks = false,
-        $renderForModal = false
+        $renderForModal = false,
+        $max = null
     )
     {
         parent::__construct($name ?? $type, $name, $renderForBlocks, $renderForModal);
 
         $this->buttonAsLink = $buttonAsLink;
         $this->type = $type;
+        $this->max = $max;
     }
 
     public function render()
