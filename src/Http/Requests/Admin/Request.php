51e3a7f63f3abc629fe8351bfaba32e2a35b5f94
Olayinka Azeez <44491331+zeezo887@users.noreply.github.com>
2022-04-22 20:05:22 +0100

    fixed error in rulesForTranslatedFields helper

--- src/Http/Requests/Admin/Request.php
+++ src/Http/Requests/Admin/Request.php
@@ -46,7 +46,7 @@ abstract class Request extends FormRequest
 
         if ($this->request->has('languages')) {
             foreach ($locales as $locale) {
-                $language = Collection::make($this->request->get('languages'))->where('value', $locale)->first();
+                $language = Collection::make($this->request->all('languages'))->where('value', $locale)->first();
                 $currentLocaleActive = $language['published'] ?? false;
                 $rules = $this->updateRules($rules, $fields, $locale, $currentLocaleActive);
 
