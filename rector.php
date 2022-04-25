2f3e80ab7bcdb60a4ced61fab248524e02c73e20
Harings Rob <haringsrob@gmail.com>
2022-04-25 10:09:54 +0200

    Config update.

--- rector.php
+++ rector.php
@@ -8,6 +8,12 @@ use Rector\Set\ValueObject\SetList;
  * This rector file is the one used by Twill internally to handle automated upgrades of code.
  */
 return static function (\Rector\Config\RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([
+        SetList::DEAD_CODE,
+    ]);
+    $rectorConfig->import(SetList::TYPE_DECLARATION);
+    $rectorConfig->import(SetList::CODE_QUALITY);
+    $rectorConfig->import(SetList::CODING_STYLE);
     $rectorConfig->import(SetList::PHP_80);
     $rectorConfig->import(SetList::PHP_81);
 };
