d667842983b7fd3132987de48c7f75a11ccc7e5c
Olayinka Azeez <44491331+zeezo887@users.noreply.github.com>
2022-05-09 10:45:51 +0100

    Added dashboard config

--- examples/agency/config/twill.php
+++ examples/agency/config/twill.php
@@ -30,4 +30,42 @@ return [
             ],
         ],
     ],
+    'dashboard' => [
+        'modules' => [
+            'works' => [
+                'name' => 'works',
+                'label' => 'works',
+                'label_singular' => 'work',
+                'routePrefix' => 'work',
+                'count' => true,
+                'create' => true,
+                'activity' => true,
+                'draft' => true,
+                'search' => true,
+            ],
+            'people' => [
+                'name' => 'people',
+                'label' => 'people',
+                'label_singular' => 'person',
+                'routePrefix' => 'about',
+                'count' => true,
+                'create' => true,
+                'activity' => true,
+                'draft' => true,
+                'search' => true,
+                'search_fields' => ['full_name']
+            ],
+            'offices' => [
+                'name' => 'offices',
+                'label' => 'offices',
+                'label_singular' => 'office',
+                'routePrefix' => 'contact',
+                'count' => true,
+                'create' => true,
+                'activity' => true,
+                'draft' => true,
+                'search' => true,
+            ],
+        ],
+    ],
 ];
