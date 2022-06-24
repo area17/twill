ed47ddb4769498f852ae24963247c4afee70ebff
Harings Rob <haringsrob@gmail.com>
2022-06-24 11:29:10 +0200

    Remove ray.

--- tests/integration/Controllers/Tables/PresenterColumnTest.php
+++ tests/integration/Controllers/Tables/PresenterColumnTest.php
@@ -4,27 +4,17 @@ namespace A17\Twill\Tests\Integration\Controllers\Tables;
 
 use A17\Twill\Services\Listings\Columns\Presenter;
 use A17\Twill\Tests\Integration\ModulesTestBase;
-use Illuminate\Support\Facades\Artisan;
 
 class PresenterColumnTest extends ModulesTestBase
 {
     public function setUp(): void
     {
         parent::setUp();
-        ray(1);
-        ray($this->getAllUris());
-        ray(
-            file_get_contents(
-                '/Users/rob/Sites/twill-dev/twill/vendor/orchestra/testbench-core/laravel/routes/twill.php'
-            )
-        );
-
         $this->author = $this->createAuthor();
     }
 
     public function testColumn(): void
     {
-        ray(2);
         $column = Presenter::make()->field('createdAt');
 
         $this->assertEquals('PresenterValueFromTestPresenter', $column->renderCell($this->author));
