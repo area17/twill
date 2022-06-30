2da22002da848f3ff660954b882b321b927ccd4e
Patrick Boivin <pboivin@gmail.com>
2022-06-30 13:11:05 -0400

    feat(revisions): Allow draft revisions on top of published content

--- src/Http/Controllers/Admin/Concerns/FormSubmitOptions.php
+++ src/Http/Controllers/Admin/Concerns/FormSubmitOptions.php
@@ -8,6 +8,10 @@ trait FormSubmitOptions
 {
     public function getSubmitOptions(Model $item): ?array
     {
+        if ($this->moduleHas('revisions') && $this->enableDraftRevisions) {
+            return $this->getSubmitOptionsForDraftRevisions($item);
+        }
+
         if ($item->cmsRestoring ?? false) {
             return [
                 'draft' => [
@@ -116,6 +120,129 @@ trait FormSubmitOptions
                 [
                     'name' => 'update-new',
                     'text' => twillTrans('twill::lang.publisher.update-new'),
+                ],
+                [
+                    'name' => 'cancel',
+                    'text' => twillTrans('twill::lang.publisher.cancel'),
+                ],
+            ],
+        ];
+    }
+
+    protected function getSubmitOptionsForDraftRevisions(Model $item): ?array
+    {
+        if ($item->cmsRestoring ?? false) {
+            return [
+                'draft' => [
+                    [
+                        'name' => 'draft-revision',
+                        'text' => twillTrans('twill::lang.publisher.draft-revision'),
+                    ],
+                    [
+                        'name' => 'draft-revision-close',
+                        'text' => twillTrans('twill::lang.publisher.draft-revision-close'),
+                    ],
+                    [
+                        'name' => 'draft-revision-new',
+                        'text' => twillTrans('twill::lang.publisher.draft-revision-new'),
+                    ],
+                    [
+                        'name' => 'cancel',
+                        'text' => twillTrans('twill::lang.publisher.cancel'),
+                    ],
+                ],
+                'live' => [
+                    [
+                        'name' => 'restore',
+                        'text' => twillTrans('twill::lang.publisher.publish'),
+                    ],
+                    [
+                        'name' => 'restore-close',
+                        'text' => twillTrans('twill::lang.publisher.publish-close'),
+                    ],
+                    [
+                        'name' => 'restore-new',
+                        'text' => twillTrans('twill::lang.publisher.publish-new'),
+                    ],
+                    [
+                        'name' => 'cancel',
+                        'text' => twillTrans('twill::lang.publisher.cancel'),
+                    ],
+                ],
+                'update' => [
+                    [
+                        'name' => 'restore',
+                        'text' => twillTrans('twill::lang.publisher.publish'),
+                    ],
+                    [
+                        'name' => 'restore-close',
+                        'text' => twillTrans('twill::lang.publisher.publish-close'),
+                    ],
+                    [
+                        'name' => 'restore-new',
+                        'text' => twillTrans('twill::lang.publisher.publish-new'),
+                    ],
+                    [
+                        'name' => 'cancel',
+                        'text' => twillTrans('twill::lang.publisher.cancel'),
+                    ],
+                ],
+            ];
+        }
+
+        return [
+            'draft' => [
+                [
+                    'name' => 'save',
+                    'text' => twillTrans('twill::lang.publisher.save'),
+                ],
+                [
+                    'name' => 'save-close',
+                    'text' => twillTrans('twill::lang.publisher.save-close'),
+                ],
+                [
+                    'name' => 'save-new',
+                    'text' => twillTrans('twill::lang.publisher.save-new'),
+                ],
+                [
+                    'name' => 'cancel',
+                    'text' => twillTrans('twill::lang.publisher.cancel'),
+                ],
+            ],
+            'live' => [
+                [
+                    'name' => 'publish',
+                    'text' => twillTrans('twill::lang.publisher.publish'),
+                ],
+                [
+                    'name' => 'publish-close',
+                    'text' => twillTrans('twill::lang.publisher.publish-close'),
+                ],
+                [
+                    'name' => 'publish-new',
+                    'text' => twillTrans('twill::lang.publisher.publish-new'),
+                ],
+                [
+                    'name' => 'cancel',
+                    'text' => twillTrans('twill::lang.publisher.cancel'),
+                ],
+            ],
+            'update' => [
+                [
+                    'name' => 'draft-revision',
+                    'text' => twillTrans('twill::lang.publisher.draft-revision'),
+                ],
+                [
+                    'name' => 'publish',
+                    'text' => twillTrans('twill::lang.publisher.publish'),
+                ],
+                [
+                    'name' => 'publish-close',
+                    'text' => twillTrans('twill::lang.publisher.publish-close'),
+                ],
+                [
+                    'name' => 'publish-new',
+                    'text' => twillTrans('twill::lang.publisher.publish-new'),
                 ],
                 [
                     'name' => 'cancel',
