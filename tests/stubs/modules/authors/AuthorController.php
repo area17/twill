d8b4f559c5ef20019b17fdf4f2dbf2c1a85d0c54
Patrick Boivin <pboivin@gmail.com>
2022-06-30 13:11:44 -0400

    feat(revisions): Add tests

--- tests/stubs/modules/authors/AuthorController.php
+++ tests/stubs/modules/authors/AuthorController.php
@@ -64,6 +64,8 @@ class AuthorController extends ModuleController
         parent::__construct($app, $request);
 
         $this->indexOptions['editInModal'] = env('EDIT_IN_MODAL', false);
+
+        $this->enableDraftRevisions = env('ENABLE_DRAFT_REVISIONS', false);
     }
 
     public function getIndexOptions()
