2f466b70244489df1df9e4c0c980300ae6d05c90
Harings Rob <haringsrob@gmail.com>
2022-07-12 10:19:10 +0200

    Show media library on clean install.

--- views/partials/navigation/_global_navigation.blade.php
+++ views/partials/navigation/_global_navigation.blade.php
@@ -1,5 +1,5 @@
-@if ($nav_items !== [])
-    <nav class="header__nav">
+<nav class="header__nav">
+    @if ($nav_items !== [])
         <ul class="header__items">
             @foreach($nav_items as $nav_item)
                 <li class=" header__item @if($nav_item['is_active']) s--on @endif">
@@ -10,21 +10,21 @@
                 </li>
             @endforeach
         </ul>
-        @if (config('twill.enabled.media-library') || config('twill.enabled.file-library') || config('twill.enabled.site-link'))
-            <ul class="header__items">
-                @can('access-media-library')
-                    @if (config('twill.enabled.media-library') || config('twill.enabled.file-library'))
-                        <li class="header__item"><a href="#"
-                                                    data-medialib-btn>{{ twillTrans('twill::lang.nav.media-library') }}</a>
-                        </li>
-                    @endif
-                @endcan
-                @if (config('twill.enabled.site-link'))
-                    <li class="header__item"><a href="{{ config('app.url') }}"
-                                                target="_blank">{{ twillTrans('twill::lang.nav.open-live-site') }} &#8599;</a>
+    @endif
+    @if (config('twill.enabled.media-library') || config('twill.enabled.file-library') || config('twill.enabled.site-link'))
+        <ul class="header__items">
+            @can('access-media-library')
+                @if (config('twill.enabled.media-library') || config('twill.enabled.file-library'))
+                    <li class="header__item"><a href="#"
+                                                data-medialib-btn>{{ twillTrans('twill::lang.nav.media-library') }}</a>
                     </li>
                 @endif
-            </ul>
-        @endif
-    </nav>
-@endif
+            @endcan
+            @if (config('twill.enabled.site-link'))
+                <li class="header__item"><a href="{{ config('app.url') }}"
+                                            target="_blank">{{ twillTrans('twill::lang.nav.open-live-site') }} &#8599;</a>
+                </li>
+            @endif
+        </ul>
+    @endif
+</nav>
