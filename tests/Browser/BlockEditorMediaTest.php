087b7a16520e8910eaac285e8e92c32f05d7a992
Harings Rob <haringsrob@gmail.com>
2022-09-14 10:42:21 +0200

    Force screenshots.

--- tests/Browser/BlockEditorMediaTest.php
+++ tests/Browser/BlockEditorMediaTest.php
@@ -77,25 +77,38 @@ HTML;
         $this->browse(function (Browser $browser) {
             $browser->loginAs($this->superAdmin, 'twill_users');
             $browser->visitTwill();
+            $browser->screenshot('1');
             $browser->createModuleEntryWithTitle('Command', 'Build');
+            $browser->screenshot('2');
 
             $browser->clickLink('Open in editor');
+            $browser->screenshot('3');
             // @note: Make sure that the cursor pointer is in the correct location.
             //
             // This is only required in a local environment when the browser is being displayed.
             $browser->drag('.editorSidebar__button', '.editorPreview__content');
+            $browser->screenshot('4');
             $browser->waitForText('Attach image', 3);
+            $browser->screenshot('5');
             $browser->press('Attach image');
+            $browser->screenshot('6');
 
             $browser->waitFor('.mediagrid__button', 3);
+            $browser->screenshot('7');
             $browser->click('.mediagrid__button');
+            $browser->screenshot('8');
             $browser->waitForText('Insert image', 3);
+            $browser->screenshot('9');
             $browser->press('Insert image');
+            $browser->screenshot('10');
 
             $browser->waitFor('.editorPreview__frame iframe', 3);
+            $browser->screenshot('11');
 
             $browser->withinFrame('.editorPreview__frame iframe', function (Browser $browser) {
+                $browser->screenshot('12');
                 $browser->waitForText('http://127.0.0.1:8001/img/uuid/area17.png', 3);
+                $browser->screenshot('13');
                 $browser->assertSee('http://127.0.0.1:8001/img/uuid/area17.png');
             });
         });
