35887ae9eb252bf98e6d782ed6820fc66e12c6b5
Harings Rob <haringsrob@gmail.com>
2022-09-20 17:53:43 +0200

    translation: Fix string object to json.

--- src/Helpers/TwillTransString.php
+++ src/Helpers/TwillTransString.php
@@ -8,7 +8,7 @@ namespace A17\Twill\Helpers;
  * This is made so that translations are resolved at render time rather than call time. This ensures
  * the correct language is being used regardless of where the string is introduced.
  */
-class TwillTransString implements \Stringable
+class TwillTransString implements \Stringable, \JsonSerializable
 {
     public function __construct(
         public string $key,
@@ -20,5 +20,10 @@ class TwillTransString implements \Stringable
     {
         $locale = config('twill.locale', config('twill.fallback_locale', 'en'));
         return (string)trans($this->key, $this->replace, $locale);
+    }
+
+    public function jsonSerialize(): string
+    {
+        return (string)$this;
     }
 }
