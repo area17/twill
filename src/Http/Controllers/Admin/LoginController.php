09f214b6d033807fa405a2ce081f26bb4c2bbe75
Harings Rob <haringsrob@gmail.com>
2022-09-21 10:16:56 +0200

    login: Remove no longer required check.

--- src/Http/Controllers/Admin/LoginController.php
+++ src/Http/Controllers/Admin/LoginController.php
@@ -282,16 +282,4 @@ class LoginController extends Controller
             return $this->sendFailedLoginResponse($request);
         }
     }
-
-    /**
-     * Get the needed authorization credentials from the request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    protected function credentials(Request $request)
-    {
-        // Only active user can login
-        return array_merge($request->only($this->username(), 'password'), ['published' => 1]);
-    }
 }
