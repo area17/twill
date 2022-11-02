4168cbb38d769242853237420bd25be74d773d83
Harings Rob <haringsrob@gmail.com>
2022-11-02 10:06:01 +0100

    Files for frontend in example.

---
+++ examples/basic-page-builder/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require('@tailwindcss/typography'),
+  ],
+}
