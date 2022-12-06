8cd8303ce67db875950bbcd3b9ce0dd6e6f7a20e
Deck Chan <deckchan@users.noreply.github.com>
2022-12-06 11:19:29 +0800

    Fix restoreRevision not showing any fields in Content

--- src/Http/Controllers/Admin/ModuleController.php
+++ src/Http/Controllers/Admin/ModuleController.php
@@ -1345,6 +1345,8 @@ abstract class ModuleController extends Controller
 
         $this->setBackLink();
 
+        $controllerForm = $this->getForm($this->repository->getById($id));
+
         $view = Collection::make([
             "$this->viewPrefix.form",
             "twill::$this->moduleName.form",
@@ -1367,7 +1369,10 @@ abstract class ModuleController extends Controller
 
         View::share('form', $this->form($id, $item));
 
-        return View::make($view, $this->form($id, $item));
+        return View::make($view, $this->form($id, $item))->with(
+            'renderFields',
+            $controllerForm
+        );
     }
 
     public function publish(): JsonResponse
