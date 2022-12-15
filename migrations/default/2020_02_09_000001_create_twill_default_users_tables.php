9e9abe5344906cbc42570e9a08978b95c85d6820
Andrea Vannucchi <andrea.vannucchi@hotmail.it>
2022-12-15 11:03:39 +0100

    Added foreign key to email in password_resets table

--- migrations/default/2020_02_09_000001_create_twill_default_users_tables.php
+++ migrations/default/2020_02_09_000001_create_twill_default_users_tables.php
@@ -31,10 +31,14 @@ class CreateTwillDefaultUsersTables extends Migration
         $twillPasswordResetsTable = config('twill.password_resets_table', 'twill_password_resets');
 
         if (!Schema::hasTable($twillPasswordResetsTable)) {
-            Schema::create($twillPasswordResetsTable, function (Blueprint $table) {
+            Schema::create($twillPasswordResetsTable, function (Blueprint $table) use ($twillUsersTable) {
                 $table->string('email')->index();
                 $table->string('token')->index();
                 $table->timestamp('created_at')->nullable();
+                $table->foreign('email')
+                    ->references('email')->on($twillUsersTable)
+                    ->cascadeOnUpdate()
+                    ->cascadeOnDelete();
             });
         }
     }
