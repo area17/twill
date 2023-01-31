8f57dda3e7cf8b5b9fcb2ae13c6c4ffe91f1af23
Quentin Renard <quentin@area17.com>
2023-01-31 17:53:26 +0100

    Fix new docs on mobile

--- docs/_templates/layout.blade.php
+++ docs/_templates/layout.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ $title }}</title>
   <link href="/style.css" rel="stylesheet">
   <script src="//unpkg.com/alpinejs" defer></script>
