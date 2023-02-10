b3bd7b1283dbf79a6cb9960b05104466444a5d7b
Matteo VG7 <57224795+matteovg7@users.noreply.github.com>
2023-02-10 08:59:40 +0100

    update glide config array

--- config/glide.php
+++ config/glide.php
@@ -12,6 +12,7 @@ return [
      */
 
     'source' => env('GLIDE_SOURCE', storage_path('app/public/' . config('twill.media_library.local_path'))),
+    'source_path_prefix' => env('GLIDE_SOURCE_PATH_PREFIX', ''),
     'cache' => env('GLIDE_CACHE', storage_path('app')),
     'cache_path_prefix' => env('GLIDE_CACHE_PATH_PREFIX', 'glide_cache'),
     'base_url' => env('GLIDE_BASE_URL', config('app.url')),
