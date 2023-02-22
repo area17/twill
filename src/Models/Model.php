66b518bef2cacf843a1f2a155f7d0f85c047e23e
Antonio Ribeiro <antonio@area17.com>
2023-02-22 01:15:40 +0100

    Add some comments

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -131,22 +131,30 @@ abstract class Model extends BaseModel implements TaggableInterface
     {
         $dates = parent::getDates();
 
+        // If it's not Laravel 10, stick to the current implementation
         if(app()->version() < '10') {
             return $dates;
         }
 
+        // If we have a $dates property, get the dates from it
         if (property_exists($this, 'dates')) {
             $dates = array_merge($dates, $this->dates);
         }
 
-        if (property_exists($this, 'casts')) {
-            $casts = array_filter($this->casts, function($k) {
-                return $k === 'date' || $k === 'datetime';
-            });
-
-            $dates = array_merge($dates, array_keys($casts));
+        // If we don't have a $casts or it's empty, return the dates we have now
+        if (!property_exists($this, 'casts') || $this->casts === []) {
+            return $dates;
         }
 
+        // Filter the ones that are casted as dates
+        $casts = array_filter($this->casts, function($k) {
+            return $k === 'date' || $k === 'datetime';
+        });
+
+        // Merge the key with our dates
+        $dates = array_merge($dates, array_keys($casts));
+
+        // Don't return duplicates
         return array_unique(array_values($dates));
     }
 }
