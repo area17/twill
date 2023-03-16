16a8e5f1b2e78af592ded1939152fb5a568d220d
Mike Byrne <mike@area17.com>
2023-03-16 09:47:55 +0000

    Revert "refactor out directly referencing colour tokens"
    This reverts commit 2d8acd54c820c232d5898007a05574c97c56d84f.

--- docs/_templates/components/alert.blade.php
+++ docs/_templates/components/alert.blade.php
@@ -1,22 +1,22 @@
 @props(['type' => 'tip', 'title' => null])
 @if ($type === 'tip')
-    <div class="bg-tip p-30 rounded-[4px] mt-30">
+    <div class="bg-purple-light p-30 rounded-[4px] mt-30">
         @if($title != null)
-            <h4 class="text-tip f-h4 font-bold !mt-0 uppercase">{{$title}}</h4>
+            <h4 class="text-purple f-h4 font-bold !mt-0 uppercase">{{$title}}</h4>
         @else
-            <h4 class="text-tip f-h4 font-bold !mt-0 uppercase">Tip</h4>
+            <h4 class="text-purple f-h4 font-bold !mt-0 uppercase">Tip</h4>
         @endif
         <div class="ml-3">
             {{$slot}}
         </div>
     </div>
 @elseif($type === 'warning')
-    <div class="bg-warn p-30 rounded-[4px] mt-30">
+    <div class="bg-yellow-light p-30 rounded-[4px] mt-30">
 
         @if($title != null)
-            <h4 class="text-warn f-h4 font-bold !mt-0 uppercase">{{$title}}</h4>
+            <h4 class="text-yellow f-h4 font-bold !mt-0 uppercase">{{$title}}</h4>
         @else
-            <h4 class="text-warn f-h4 font-bold !mt-0 uppercase">Warning</h4>
+            <h4 class="text-yellow f-h4 font-bold !mt-0 uppercase">Warning</h4>
         @endif
         <div class="ml-3 text-sm font-medium">
             {{$slot}}
@@ -32,11 +32,11 @@
         </div>
     </div>
 @elseif($type === 'danger')
-    <div class="bg-danger p-30 rounded-[4px] mt-30">
+    <div class="bg-red-light p-30 rounded-[4px] mt-30">
         @if($title != null)
-            <h4 class="text-danger f-h4 font-bold !mt-0 uppercase">{{$title}}</h4>
+            <h4 class="text-red f-h4 font-bold !mt-0 uppercase">{{$title}}</h4>
         @else
-            <h4 class="text-danger f-h4 font-bold !mt-0 uppercase">Danger</h4>
+            <h4 class="text-red f-h4 font-bold !mt-0 uppercase">Danger</h4>
         @endif
         <div class="ml-3 text-sm font-medium">
             {{$slot}}
