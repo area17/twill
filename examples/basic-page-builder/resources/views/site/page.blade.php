60078334bca7e731c35355a47c6456dd8ec1c82f
Mike Byrne <mike@area17.com>
2023-03-27 14:36:51 +0100

    updates styles of basic page builder example

--- examples/basic-page-builder/resources/views/site/page.blade.php
+++ examples/basic-page-builder/resources/views/site/page.blade.php
@@ -6,7 +6,10 @@
 </head>
 <body>
 <x-menu/>
-<div class="mx-auto max-w-2xl">
+<div class="mx-auto max-w-2xl px-5 md:px-0">
+    <div class="prose md:prose-lg lg:prose-xl prose-a:font-normal mt-16 first:mt-0">
+        <h1>{{ $item->title }}</h1>
+    </div>
     {!! $item->renderBlocks() !!}
 </div>
 </body>
