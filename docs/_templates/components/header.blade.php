667a9c8939698eb40394b7a0dae689385aa5b51a
Quentin Renard <quentin@area17.com>
2023-03-27 03:07:29 +0200

    Add header links

--- docs/_templates/components/header.blade.php
+++ docs/_templates/components/header.blade.php
@@ -3,7 +3,12 @@
         <a href="/index.html">
             <x-twilldocs::logo />
         </a>
-        <div class="flex items-center gap-x-20">
+        <div class="flex items-center gap-x-32">
+            <x-twilldocs::navLink url="https://twill.io/made" label="#MadeWithTwill" branded />
+            <x-twilldocs::navLink url="https://demo.twill.io/" label="Demo" />
+            <x-twilldocs::navLink url="https://discord.gg/cnWk7EFv8R" label="Chat" />
+            <x-twilldocs::navLink url="https://twill.io/blog" label="Blog" />
+            <x-twilldocs::navLink url="https://twill.io/docs" label="Docs" />
             <x-twilldocs::githubBtn />
             <x-twilldocs::menuBtn />
         </div>
