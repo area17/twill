501cdbabdc96c2530a442216500edcd93455f0f8
Harings Rob <haringsrob@gmail.com>
2023-03-30 09:18:02 +0200

    Fix migrations.

--- migrations/default/2023_03_24_125122_add_id_to_related.php
+++ migrations/default/2023_03_24_125122_add_id_to_related.php
@@ -8,7 +8,18 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('related', function (Blueprint $table) {
-            $table->increments('id')->first();
+            // We cannot add id's to existing table with sqlite.
+            // For our tests to continue to work, we added this migration in the source file for the original
+            // database creation.
+            // If your project is running on sqlite you will have to write a custom migration to:
+            // - store all the related items in a temporary table
+            // - drop the related table
+            // - recreate it with the id increments
+            // - copy back the data from the temporary table
+            // - drop the temporary table.
+            if (config('database.default') !== 'sqlite') {
+                $table->increments('id')->first();
+            }
         });
     }
 
