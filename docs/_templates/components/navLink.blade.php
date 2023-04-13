3e886ff6e30ed9545d1071ef38573727e9323c92
Mike Byrne <mike@area17.com>
2023-04-13 14:21:01 +0100

    add mobile nav to non docs pages

--- docs/_templates/components/navLink.blade.php
+++ docs/_templates/components/navLink.blade.php
@@ -2,10 +2,13 @@
     'branded' => false,
     'url' => '#',
     'label' => '',
+    'mobileNav' => false
 ])
 
 <a @class([
-    'f-h4 no-underline xs:hidden lg:inline',
+    'f-h4 no-underline',
+    'xs:hidden lg:inline' => !$mobileNav,
+    'block' => $mobileNav,
     'hover:text-link' => !$branded,
     'text-link hover:text-primary' => $branded
 ]) href="{{ $url }}">{{ $label }}</a>
