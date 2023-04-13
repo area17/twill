61d23b9a9737f4251a16fafb39888e6a14ee9df6
Mike Byrne <mike@area17.com>
2023-04-13 14:40:14 +0100

    add missing nav options to mobile nav, unify styles

--- docs/_templates/components/sidebarSimple.blade.php
+++ docs/_templates/components/sidebarSimple.blade.php
@@ -24,11 +24,11 @@
         <svg class="h-18 w-18" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10"><defs><style>.close-icon-line{fill:none;stroke-miterlimit:10;stroke-width:1.5px;}</style></defs><title>close_icon</title><line class="close-icon-line" x1="1" y1="1" x2="9" y2="9" stroke="currentColor"/><line class="close-icon-line" x1="9" y1="1" x2="1" y2="9" stroke="currentColor"/>
         </svg>
     </button>
-    <ul role="list" class="flex flex-col flex-nowrap min-h-full py-28">
+    <ul role="list" class="flex flex-col flex-nowrap min-h-full py-32">
         <li><x-twilldocs::navLink url="/blog/" label="Blog" mobileNav="true" /></li>
         <li class="mt-12"><x-twilldocs::navLink url="/guides/" label="Guides" mobileNav="true" /></li>
         <li class="mt-12 mb-auto"><x-twilldocs::navLink url="/docs/" label="Docs" mobileNav="true" /></li>
-        <li class="mt-12 pt-12 border-t border-t-primary"><x-twilldocs::navLink url="/made" label="#MadeWithTwill" mobileNav="true" branded /></li>
+        <li class="mt-20 pt-20 border-t border-t-primary"><x-twilldocs::navLink url="/made" label="#MadeWithTwill" mobileNav="true" branded /></li>
         <li class="mt-12"><x-twilldocs::navLink url="https://demo.twill.io/" label="Demo" mobileNav="true" /></li>
         <li class="mt-12"><x-twilldocs::navLink url="https://discord.gg/cnWk7EFv8R" label="Chat" mobileNav="true" /></li>
     </ul>
