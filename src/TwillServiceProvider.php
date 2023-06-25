9dfbd7e1df50d326f5056b439514c057254d5170
Mike Rockétt <mike@rockett.pw>
2023-06-25 21:12:21 +0200

    use pushOnce builtin, remove custom directive

--- src/TwillServiceProvider.php
+++ src/TwillServiceProvider.php
@@ -507,17 +507,6 @@ class TwillServiceProvider extends ServiceProvider
             ?>";
         });
 
-        $blade->directive('pushonce', function ($expression) {
-            [$pushName, $pushSub] = explode(':', trim(substr($expression, 1, -1)));
-            $key = '__pushonce_' . $pushName . '_' . str_replace('-', '_', $pushSub);
-
-            return "<?php if(! isset(\$__env->{$key})): \$__env->{$key} = 1; \$__env->startPush('{$pushName}'); ?>";
-        });
-
-        $blade->directive('endpushonce', function () {
-            return '<?php $__env->stopPush(); endif; ?>';
-        });
-
         $blade->component('twill::partials.form.utils._fieldset', 'formFieldset');
         $blade->component('twill::partials.form.utils._columns', 'formColumns');
         $blade->component('twill::partials.form.utils._collapsed_fields', 'formCollapsedFields');
--- views/partials/form/_block_editor.blade.php
+++ views/partials/form/_block_editor.blade.php
@@ -13,6 +13,6 @@
     window['{{ config('twill.js_namespace') }}'].STORE.form.editorNames.push({!! json_encode($editorName) !!})
 @endpush
 
-@pushonce('vuexStore:block_editor')
+@pushOnce('vuexStore', 'store:block_editor')
     @include('twill::partials.form.utils._block_editor_store')
-@endpushonce
+@endPushOnce
