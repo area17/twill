ab8635fe519ba80b5ec77b24d62ec3d8f7f60149
Joyce Verheije <joyce@area17.com>
2023-08-31 12:16:29 +0200

    Add support for titleInBrowser accessor in browsers

--- src/Services/Listings/TableColumns.php
+++ src/Services/Listings/TableColumns.php
@@ -31,7 +31,7 @@ class TableColumns extends Collection
         }
 
         $data['id'] = $model->{$tableDataContext->identifierColumn};
-        $data['name'] = $model->{$tableDataContext->titleColumnKey};
+        $data['name'] = $model->titleInBrowser ?? $model->{$tableDataContext->titleColumnKey};
         $data['edit'] = $editUrl ?? null;
         $data['endpointType'] = $tableDataContext->endpointType;
         $data['repeaterFields'] = $tableDataContext->repeaterFields;
