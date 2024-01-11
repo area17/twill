a4d2021be52c0e58cff8cb7e13a4e7fe8c1520ea
Quentin Renard <quentin@area17.com>
2024-01-11 23:55:23 +0100

    Code style fix

--- src/Commands/ModuleMake.php
+++ src/Commands/ModuleMake.php
@@ -653,7 +653,7 @@ PHP;
         if ($this->factory && $this->getApplication()->has('make:factory')) {
             $activeModelTraitsImports .= "\nuse Illuminate\Database\Eloquent\Factories\HasFactory;";
             $activeModelTraitsString = Str::of($activeModelTraitsString)->replace(';', ', HasFactory;');
-            Artisan::call('make:factory', ['name' => $modelName.'Factory','--model' => $modelClassName], $this->output);
+            Artisan::call('make:factory', ['name' => $modelName . 'Factory', '--model' => $modelClassName], $this->output);
         }
 
         if ($this->seeder && $this->getApplication()->has('make:seeder')) {
