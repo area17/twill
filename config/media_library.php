fd926d12864fe68648717ed9b6596e76ce02251b
Keania Eric <keaniaeric@gmail.com>
2024-02-27 14:27:18 +0100

    Revert "allow all filetypes in media library"
    This reverts commit e6f2bc0ea45da55cc5379ea0b310aa935f55c103.

--- config/media_library.php
+++ config/media_library.php
@@ -27,7 +27,7 @@ return [
     'image_service' => env('MEDIA_LIBRARY_IMAGE_SERVICE', 'A17\Twill\Services\MediaLibrary\Glide'),
     'acl' => env('MEDIA_LIBRARY_ACL', 'private'),
     'filesize_limit' => env('MEDIA_LIBRARY_FILESIZE_LIMIT', 50),
-    'allowed_extensions' => [],
+    'allowed_extensions' => ['svg', 'jpg', 'gif', 'png', 'jpeg'],
     'init_alt_text_from_filename' => true,
     'prefix_uuid_with_local_path' => config('twill.file_library.prefix_uuid_with_local_path', false),
     'translated_form_fields' => false,
