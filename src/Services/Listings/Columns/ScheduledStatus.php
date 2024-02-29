569a9645639e1d4e530c38f1f970d65e1f8440f3
Olayinka Azeez <olayinka@area17.com>
2024-02-29 18:02:40 +0100

    added scheduled_status to Table cell specific columns

--- src/Services/Listings/Columns/ScheduledStatus.php
+++ src/Services/Listings/Columns/ScheduledStatus.php
@@ -15,33 +15,4 @@ class ScheduledStatus extends TableColumn
 
         return $column;
     }
-
-    protected function getRenderValue(TwillModelContract $model): string
-    {
-        $this->html = true;
-
-        $startDate = $model->publish_start_date ? Carbon::parse($model->publish_start_date) : null;
-        $endDate = $model->publish_end_date ? Carbon::parse($model->publish_end_date) : null;
-
-        $expired = $model->publish_end_date && $endDate->isPast();
-        $scheduled = $model->publish_start_date && $startDate->isFuture();
-
-        $label = null;
-        if ($scheduled) {
-            $label = twillTrans('twill::lang.publisher.scheduled');
-        } elseif ($expired) {
-            $label = twillTrans('twill::lang.publisher.expired');
-        }
-
-        $format = config('twill.publish_date_display_format', 'F d, Y');
-
-        return view('twill::listings.columns.scheduled-data', [
-            'isScheduled' => $scheduled,
-            'isExpired' => $expired,
-            'label' => $label,
-            'hasStartDate' => $model->publish_start_date ?? false,
-            'startDate' => $startDate?->format($format),
-            'endDate' => $endDate?->format($format),
-        ])->render();
-    }
 }
