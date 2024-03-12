cc26b32fe6bc3da79bf81c9f0e16d57bc1a3dff1
Olayinka Azeez <olayinka@area17.com>
2024-03-12 17:11:56 +0100

    load browsers into related cache when previewing blocks

--- src/Models/Behaviors/HasRelated.php
+++ src/Models/Behaviors/HasRelated.php
@@ -125,4 +125,9 @@ trait HasRelated
     {
         $this->relatedItems()->delete();
     }
+
+    public function setRelatedCache($browser, $items): void
+    {
+        $this->relatedCache[$browser] = $items;
+    }
 }
