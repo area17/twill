135ef9b94fbd90b679580a1bcebf51f459879374
Tofandel <adrien.foulon@tukan.hu>
2024-03-18 16:19:49 +0100

    Fix user creation is missing a role input

--- src/Http/Controllers/Admin/ModuleController.php
+++ src/Http/Controllers/Admin/ModuleController.php
@@ -1778,7 +1778,7 @@ abstract class ModuleController extends Controller
         // @todo: use $this->filters instead of indexData.
         $indexDataWithoutFilters = $this->indexData($this->request);
         foreach ($indexDataWithoutFilters as $key => $value) {
-            if (Str::endsWith($key, 'List')) {
+            if ($key !== 'roleList' && Str::endsWith($key, 'List')) {
                 unset($indexDataWithoutFilters[$key]);
             }
         }
@@ -1922,7 +1922,7 @@ abstract class ModuleController extends Controller
         $tableFilters = TableFilters::make();
 
         foreach ($this->indexData($this->request) as $key => $value) {
-            if (Str::endsWith($key, 'List')) {
+            if ($key !== 'roleList' && Str::endsWith($key, 'List')) {
                 $queryString = Str::beforeLast($key, 'List');
 
                 if ($filterKey = ($this->filters[$queryString] ?? false)) {
