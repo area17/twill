359d1c8f3217bca00bd691661a1d0a3c9b4b8897
Olayinka Azeez <olayinka@area17.com>
2024-03-29 18:41:20 +0100

    set vue alias to @vue/compat and enable compat mode

--- vue.config.js
+++ vue.config.js
@@ -137,6 +137,23 @@ const config = {
       modules: resolveModules
     },
     plugins
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
   }
 }
 
