169e7b746f1e31d9b5a1c35eb58a31ed415233b8
Olayinka Azeez <olayinka@area17.com>
2024-04-25 17:12:12 +0100

    disable detailed warnings for hydration mismatches

--- vue.config.js
+++ vue.config.js
@@ -141,7 +141,7 @@ const config = {
   chainWebpack: (config) => {
     config.plugin('define').tap((definitions) => {
       Object.assign(definitions[0], {
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
       })
       return definitions
     })
