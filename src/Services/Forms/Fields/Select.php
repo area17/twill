eeed569597faf1898991593da855f0a5c70c7a83
Olayinka Azeez <olayinka@area17.com>
2024-05-06 20:31:01 +0100

    added method to add an empty option to select form field

--- src/Services/Forms/Fields/Select.php
+++ src/Services/Forms/Fields/Select.php
@@ -6,6 +6,8 @@ use A17\Twill\Services\Forms\Fields\Traits\HasOptions;
 use A17\Twill\Services\Forms\Fields\Traits\HasPlaceholder;
 use A17\Twill\Services\Forms\Fields\Traits\IsTranslatable;
 use A17\Twill\Services\Forms\Fields\Traits\Unpackable;
+use A17\Twill\Services\Forms\Option;
+use A17\Twill\Services\Forms\Options;
 
 class Select extends BaseFormField
 {
@@ -30,6 +32,22 @@ class Select extends BaseFormField
     public function searchable(bool $searchable = true): static
     {
         $this->searchable = $searchable;
+
+        return $this;
+    }
+
+    /**
+     * Adds an empty option as the first option
+     *
+     * @param string $label
+     * @return $this
+     */
+    public function withEmptyOption(string $label = ''): static
+    {
+        if ($this->options === null) {
+            $this->options = Options::make();
+        }
+        $this->options->prepend(Option::make('', $label));
 
         return $this;
     }
