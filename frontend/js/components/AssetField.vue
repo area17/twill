f61cc950a575fa48cb546f4a0c154702291b0b65
Olayinka Azeez <olayinka@area17.com>
2024-09-03 09:54:21 +0100

    linting

--- frontend/js/components/AssetField.vue
+++ frontend/js/components/AssetField.vue
@@ -39,86 +39,86 @@
 </template>
 
 <script>
-import draggable from 'vuedraggable'
-import inputframeMixin from '@/mixins/inputFrame'
-import draggableMixin from "@/mixins/draggable";
-import mediaLibraryMixin from "@/mixins/mediaLibrary/mediaLibrary";
-import mediaFieldMixin from '@/mixins/mediaField.js'
-import localeMixin from "@/mixins/locale";
-import {MEDIA_LIBRARY} from "@/store/mutations";
-import {mapState} from "vuex";
+  import draggable from 'vuedraggable'
+  import inputframeMixin from '@/mixins/inputFrame'
+  import draggableMixin from "@/mixins/draggable";
+  import mediaLibraryMixin from "@/mixins/mediaLibrary/mediaLibrary";
+  import mediaFieldMixin from '@/mixins/mediaField.js'
+  import localeMixin from "@/mixins/locale";
+  import {MEDIA_LIBRARY} from "@/store/mutations";
+  import {mapState} from "vuex";
 
 
-export default {
-  name: "A17AssetField",
-  components: {
-    draggable
-  },
-  mixins: [draggableMixin, mediaLibraryMixin, localeMixin, inputframeMixin, mediaFieldMixin],
-  props: {
-    fieldNote: {
-      type: String,
-      default: ''
+  export default {
+    name: "A17AssetField",
+    components: {
+      draggable
     },
-    name: {
-      type: String,
-      required: true
-    },
-    itemLabel: {
-      type: String,
-      default: 'asset'
-    },
-    max: {
-      type: Number,
-      default: 10
-    },
-    allowFile: {
-      type: Boolean,
-      default: true
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    buttonOnTop: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data: function () {
-    return {
-      handle: '.slide__handle', // Drag handle override
-      hoverable: true
-    }
-  },
-  computed: {
-    addLabel: function () {
-      const itemNames = this.itemLabel + 's'
-      return 'Attach ' + itemNames
-    },
-    items: {
-      get () {
-        if (this.selectedMedias.hasOwnProperty(this.name)) {
-          return this.selectedMedias[this.name] || []
-        } else {
-          return []
-        }
+    mixins: [draggableMixin, mediaLibraryMixin, localeMixin, inputframeMixin, mediaFieldMixin],
+    props: {
+      fieldNote: {
+        type: String,
+        default: ''
       },
-      set (value) {
-        this.$store.commit(MEDIA_LIBRARY.REORDER_MEDIAS, {
-          name: this.name,
-          medias: value
-        })
+      name: {
+        type: String,
+        required: true
+      },
+      itemLabel: {
+        type: String,
+        default: 'asset'
+      },
+      max: {
+        type: Number,
+        default: 10
+      },
+      allowFile: {
+        type: Boolean,
+        default: true
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      buttonOnTop: {
+        type: Boolean,
+        default: false
       }
     },
-    remainingItems: function () {
-      return Math.max(0, this.max - this.items.length)
+    data: function () {
+      return {
+        handle: '.slide__handle', // Drag handle override
+        hoverable: true
+      }
     },
-    ...mapState({
-      selectedMedias: state => state.mediaLibrary.selected
-    })
+    computed: {
+      addLabel: function () {
+        const itemNames = this.itemLabel + 's'
+        return 'Attach ' + itemNames
+      },
+      items: {
+        get () {
+          if (this.selectedMedias.hasOwnProperty(this.name)) {
+            return this.selectedMedias[this.name] || []
+          } else {
+            return []
+          }
+        },
+        set (value) {
+          this.$store.commit(MEDIA_LIBRARY.REORDER_MEDIAS, {
+            name: this.name,
+            medias: value
+          })
+        }
+      },
+      remainingItems: function () {
+        return Math.max(0, this.max - this.items.length)
+      },
+      ...mapState({
+        selectedMedias: state => state.mediaLibrary.selected
+      })
+    }
   }
-}
 </script>
 
 <style lang="scss" scoped>
