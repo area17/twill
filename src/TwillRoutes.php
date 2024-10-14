area17/twill

commit 684970473ae2e92f17d885735b68e8eb62a07273

Support subdomain

src/TwillRoutes.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 
+use function request;
+
 class TwillRoutes
 {
     /**
@@ -405,10 +407,14 @@ class TwillRoutes
 
     public function isTwillRequest(): bool
     {
-        $adminAppUrl = config('twill.admin_app_url', config('app.url'));
+        $adminAppUrl = str_replace(['http://', 'https://'], '', config('twill.admin_app_url', config('app.url')));
+        $requestHost = request()->getHttpHost();
 
-        $matchesDomain = !config('twill.admin_app_strict') || Str::endsWith(\request()->getSchemeAndHttpHost(), $adminAppUrl);
-        $matchesPath = empty(config('twill.admin_app_path')) || \request()->is(config('twill.admin_app_path', '') . '*');
+        $matchesDomain = config('twill.support_subdomain_admin_routing') ?
+                Str::startsWith($requestHost, config('twill.admin_app_subdomain', 'admin') . '.') && Str::endsWith($requestHost, '.' . $adminAppUrl)
+                : !config('twill.admin_app_strict') || $requestHost === $adminAppUrl;
+
+        $matchesPath = empty(config('twill.admin_app_path')) || request()->is(config('twill.admin_app_path', '') . '*');
 
         return $matchesDomain && $matchesPath;
     }

commit 042c87e190760e140a746d0ee005c99408078c3c

Fix regression introduced in #2612
With the default path set to `admin`, `/adminsomething`, eg. `/administration` would trigger the Twill 404 error instead of the frontend 404 due to the start of the path matching.

src/TwillRoutes.php
@@ -414,7 +414,7 @@ class TwillRoutes
                 Str::startsWith($requestHost, config('twill.admin_app_subdomain', 'admin') . '.') && Str::endsWith($requestHost, '.' . $adminAppUrl)
                 : !config('twill.admin_app_strict') || $requestHost === $adminAppUrl;
 
-        $matchesPath = empty(config('twill.admin_app_path')) || request()->is(config('twill.admin_app_path', '') . '*');
+        $matchesPath = empty(config('twill.admin_app_path')) || request()->is(config('twill.admin_app_path', '') . '/*');
 
         return $matchesDomain && $matchesPath;
     }