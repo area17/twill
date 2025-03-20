ba62368519a3587cabac653578e54a7387fcd008
ifox <ifox@users.noreply.github.com>
2025-03-20 04:43:54 +0000

    Update assets and bump version to 3.5.2

--- src/TwillServiceProvider.php
+++ src/TwillServiceProvider.php
@@ -65,7 +65,7 @@ class TwillServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const VERSION = '3.5.1';
+    public const VERSION = '3.5.2';
 
     /**
      * Service providers to be registered.
