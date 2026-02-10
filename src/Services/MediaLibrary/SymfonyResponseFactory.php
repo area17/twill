9b85c093672b18c18801ee66d203264c8372036a
Antonio Ribeiro <acr@antoniocarlosribeiro.com>
2026-02-10 23:13:23 +0100

    Replace SymfonyResponseFactory implementation to support Glide v3

---
+++ src/Services/MediaLibrary/SymfonyResponseFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace A17\Twill\Services\MediaLibrary;
+
+use League\Flysystem\FilesystemOperator;
+use League\Glide\Responses\ResponseFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+class SymfonyResponseFactory implements ResponseFactoryInterface
+{
+    protected ?Request $request;
+
+    public function __construct(?Request $request = null)
+    {
+        $this->request = $request;
+    }
+
+    public function create(FilesystemOperator $cache, string $path): StreamedResponse
+    {
+        $stream = $cache->readStream($path);
+
+        $response = new StreamedResponse();
+        $response->headers->set('Content-Type', $cache->mimeType($path));
+        $response->headers->set('Content-Length', (string) $cache->fileSize($path));
+        $response->setPublic();
+        $response->setMaxAge(31536000);
+        $response->setExpires(date_create()->modify('+1 years'));
+
+        if ($this->request) {
+            $response->setLastModified(date_create()->setTimestamp($cache->lastModified($path)));
+            $response->isNotModified($this->request);
+        }
+
+        $response->setCallback(function () use ($stream) {
+            if (ftell($stream) !== 0) {
+                rewind($stream);
+            }
+            fpassthru($stream);
+            fclose($stream);
+        });
+
+        return $response;
+    }
+}
